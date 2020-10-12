aa438f6edc976aacff31588834c60dac53e2d434
Don Jayamanne <don.jayamanne@yahoo.com>
2020-10-12 13:07:35 -0700

    Trust for native notebooks (#14353)

--- src/test/datascience/notebook/interrupRestart.native.vscode.test.ts
+++ src/test/datascience/notebook/interrupRestart.native.vscode.test.ts
@@ -30,6 +30,7 @@ import {
     insertCodeCell,
     startJupyter,
     trustAllNotebooks,
+    waitForKernelToGetAutoSelected,
     waitForTextOutputInVSCode
 } from './helper';
 
@@ -78,6 +79,7 @@ suite('DataScience - VSCode Notebook - Restart/Interrupt/Cancel/Errors (slow)', 
         await trustAllNotebooks();
         // Open a notebook and use this for all tests in this test suite.
         await editorProvider.createNew();
+        await waitForKernelToGetAutoSelected();
         assert.isOk(vscodeNotebook.activeNotebookEditor, 'No active notebook');
         vscEditor = vscodeNotebook.activeNotebookEditor!;
     });
