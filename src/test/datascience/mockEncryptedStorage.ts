0af6db561bd061cbb4ecd487761ec67a117a6a22
rchiodo <rchiodo@users.noreply.github.com>
2020-10-29 15:06:39 -0700

    Keytar not working on build machines

--- src/test/datascience/mockEncryptedStorage.ts
+++ src/test/datascience/mockEncryptedStorage.ts
@@ -1,25 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { injectable } from 'inversify';
-import * as keytar from 'keytar';
 import { IEncryptedStorage } from '../../client/common/application/types';
 
 /**
- * Class that wraps keytar and authentication to provide a way to write out and save a string
- * This class MUST run outside of VS code
+ * Mock for encrypted storage. Doesn't do anything except hold values in memory (keytar doesn't work without a UI coming up on Mac/Linux)
  */
 @injectable()
 export class MockEncryptedStorage implements IEncryptedStorage {
+    private map = new Map<string, string>();
     public async store(service: string, key: string, value: string | undefined): Promise<void> {
-        // When not in insiders, use keytar
-        if (!value) {
-            await keytar?.deletePassword(service, key);
+        const trueKey = `${service}.${key}`;
+        if (value) {
+            this.map.set(trueKey, value);
         } else {
-            return keytar?.setPassword(service, key, value);
+            this.map.delete(trueKey);
         }
     }
     public async retrieve(service: string, key: string): Promise<string | undefined> {
-        const val = await keytar?.getPassword(service, key);
-        return val ? val : undefined;
+        const trueKey = `${service}.${key}`;
+        return this.map.get(trueKey);
     }
 }
