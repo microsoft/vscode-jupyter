275cfa6ff5007b4b412f6321a49cf279279ad6b8
Don Jayamanne <don.jayamanne@yahoo.com>
2020-10-29 16:17:49 -0700

    Misc

--- src/client/application/serviceRegistry.ts
+++ src/client/application/serviceRegistry.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-'use strict';
-
 import { IExtensionSingleActivationService } from '../activation/types';
 import { IServiceManager } from '../ioc/types';
 import { JoinMailingListPrompt } from './misc/joinMailingListPrompt';
