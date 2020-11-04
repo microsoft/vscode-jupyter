7a7c1586dd63155940347cd0d9b11461285e63c0
Ian Huff <ianhuff@s-pkges.redmond.corp.microsoft.com>
2020-11-04 09:46:36 -0800

    Context and experiment to show native variables

--- src/client/datascience/variablesView/variableViewActivationService.ts
+++ src/client/datascience/variablesView/variableViewActivationService.ts
@@ -1,7 +1,10 @@
 import { inject, injectable } from 'inversify';
 import { window } from 'vscode';
 import { IExtensionSingleActivationService } from '../../activation/types';
-import { IExtensionContext } from '../../common/types';
+import { ICommandManager } from '../../common/application/types';
+import { ContextKey } from '../../common/contextKey';
+import { Experiments } from '../../common/experiments/groups';
+import { IExperimentService, IExtensionContext } from '../../common/types';
 import { IVariableViewProvider } from './types';
 
 // Responsible for registering our Native Notebook variable view
@@ -9,15 +12,23 @@ import { IVariableViewProvider } from './types';
 export class VariableViewActivationService implements IExtensionSingleActivationService {
     constructor(
         @inject(IExtensionContext) private extensionContext: IExtensionContext,
-        @inject(IVariableViewProvider) private variableViewProvider: IVariableViewProvider
+        @inject(IVariableViewProvider) private variableViewProvider: IVariableViewProvider,
+        @inject(IExperimentService) private experimentService: IExperimentService,
+        @inject(ICommandManager) private commandManager: ICommandManager
     ) {}
 
     public async activate() {
-        this.extensionContext.subscriptions.push(
-            // IANHU: Consider not using retainContext here?
-            window.registerWebviewViewProvider(this.variableViewProvider.viewType, this.variableViewProvider, {
-                webviewOptions: { retainContextWhenHidden: true }
-            })
-        );
+        const context = new ContextKey('jupyter.nativevariableview', this.commandManager);
+        if (await this.experimentService.inExperiment(Experiments.NativeVariableView)) {
+            context.set(true).ignoreErrors();
+            this.extensionContext.subscriptions.push(
+                // IANHU: Consider not using retainContext here? This will save the context of our view, but take more memory in VS Code.
+                window.registerWebviewViewProvider(this.variableViewProvider.viewType, this.variableViewProvider, {
+                    webviewOptions: { retainContextWhenHidden: true }
+                })
+            );
+        } else {
+            context.set(false).ignoreErrors();
+        }
     }
 }
