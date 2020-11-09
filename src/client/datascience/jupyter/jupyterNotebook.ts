6b4a006b705848346bf6890eb1e9ca52899ed6f2
Joyce Er <joyceerhuiling@gmail.com>
2020-11-09 11:33:30 -0800

    Handle `window.location` in inline tensorboard iframes (#420)
    * Handle `window.location` in tensorboard iframes
    
    This allows tensorboard to be rendered inline in notebook cells
    
    Also fix an incomplete comment from an earlier PR
    
    * Make condition for replacing more restrictive

--- src/client/datascience/jupyter/jupyterNotebook.ts
+++ src/client/datascience/jupyter/jupyterNotebook.ts
@@ -1377,10 +1377,27 @@ export class JupyterNotebookBase implements INotebook {
         }
     }
 
+    private handleTensorBoardDisplayDataOutput(data: nbformat.IMimeBundle) {
+        // After executing %tensorboard --logdir <log directory> to launch
+        // TensorBoard inline, TensorBoard sends back an IFrame to display as output.
+        // The TensorBoard app hardcodes the source URL of the IFrame to `window.location`.
+        // In the VSCode context this results in the URL taking on the internal
+        // vscode-webview:// scheme which doesn't work. Hence rewrite it to use
+        // http://localhost:<port number>.
+        if (data.hasOwnProperty('text/html')) {
+            const text = data['text/html'];
+            if (typeof text === 'string' && text.includes('<iframe id="tensorboard-frame-')) {
+                data['text/html'] = text.replace(/new URL\((.*), window.location\)/, 'new URL("http://localhost")');
+            }
+        }
+        return data;
+    }
+
     private handleDisplayData(msg: KernelMessage.IDisplayDataMsg, clearState: RefBool, cell: ICell) {
+        const newData = this.handleTensorBoardDisplayDataOutput(msg.content.data);
         const output: nbformat.IDisplayData = {
             output_type: 'display_data',
-            data: msg.content.data,
+            data: newData,
             metadata: msg.content.metadata,
             // tslint:disable-next-line: no-any
             transient: msg.content.transient as any // NOSONAR
