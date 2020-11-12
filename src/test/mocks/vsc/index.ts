81a373e07574abae03ed8ab6578f14446db00d5c
Don Jayamanne <don.jayamanne@yahoo.com>
2020-11-12 12:00:08 -0800

    oops

--- src/test/mocks/vsc/index.ts
+++ src/test/mocks/vsc/index.ts
@@ -320,6 +320,6 @@ export namespace vscMock {
          * The extension is running from an `--extensionTestsPath` and
          * the extension host is running unit tests.
          */
-        Test = 3,
+        Test = 3
     }
 }
