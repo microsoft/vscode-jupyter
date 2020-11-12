29690b23ed3bfbb55d2913c8e808b4b59ebae8f4
rchiodo <rchiodo@users.noreply.github.com>
2020-11-12 10:43:00 -0800

    Fixup reload documents

--- src/client/datascience/ipywidgets/notebookIPyWidgetCoordinator.ts
+++ src/client/datascience/ipywidgets/notebookIPyWidgetCoordinator.ts
@@ -4,6 +4,7 @@ import { inject, injectable } from 'inversify';
 import { Disposable } from 'vscode';
 import { NotebookCommunication, NotebookDocument, NotebookKernel } from '../../../../types/vscode-proposed';
 import { IExtensionSingleActivationService } from '../../activation/types';
+import { IVSCodeNotebook } from '../../common/application/types';
 import { IServiceContainer } from '../../ioc/types';
 import { InteractiveWindowMessages } from '../interactive-common/interactiveWindowTypes';
 import { INotebookKernelProvider } from '../notebook/types';
@@ -12,16 +13,18 @@ import { CommonMessageCoordinator } from './commonMessageCoordinator';
 @injectable()
 export class NotebookIPyWidgetCoordinator implements IExtensionSingleActivationService {
     private messageCoordinators = new Map<string, Promise<CommonMessageCoordinator>>();
-    private attachedWebViews = new Set<string>();
+    private attachedWebViews = new Map<string, { webviews: Set<string>; disposables: Disposable[] }>();
     private disposables: Disposable[] = [];
     constructor(
         @inject(INotebookKernelProvider) notebookKernelProvider: INotebookKernelProvider,
-        @inject(IServiceContainer) private readonly serviceContainer: IServiceContainer
+        @inject(IServiceContainer) private readonly serviceContainer: IServiceContainer,
+        @inject(IVSCodeNotebook) private readonly notebookProvider: IVSCodeNotebook
     ) {
         notebookKernelProvider.onResolvedKernel(this.onResolvedKernel.bind(this));
         if (notebookKernelProvider.onDidChangeKernels) {
             notebookKernelProvider.onDidChangeKernels(this.onDidChangeKernels.bind(this));
         }
+        notebookProvider.onDidCloseNotebookDocument(this.onDidCloseNotebook.bind(this));
     }
     public async activate(): Promise<void> {
         // We don't need to do anything here. We'll signup on resolve kernel events
@@ -42,13 +45,31 @@ export class NotebookIPyWidgetCoordinator implements IExtensionSingleActivationS
             promise = CommonMessageCoordinator.create(arg.document.uri, this.serviceContainer);
             this.messageCoordinators.set(arg.document.uri.toString(), promise);
         }
-        promise.then(this.attachCoordinator.bind(this, arg.webview)).ignoreErrors();
+        promise.then(this.attachCoordinator.bind(this, arg.document, arg.webview)).ignoreErrors();
     }
 
-    private attachCoordinator(webview: NotebookCommunication, c: CommonMessageCoordinator) {
-        if (!this.attachedWebViews.has(webview.editorId)) {
-            this.attachedWebViews.add(webview.editorId);
-            this.disposables.push(
+    private onDidCloseNotebook(e: NotebookDocument) {
+        // See if this is the last copy of this document
+        if (!this.notebookProvider.notebookDocuments.find((d) => d.uri !== e.uri)) {
+            const coordinator = this.messageCoordinators.get(e.uri.toString());
+            coordinator?.then((c) => c.dispose());
+            this.messageCoordinators.delete(e.uri.toString());
+            const attachment = this.attachedWebViews.get(e.uri.toString());
+            attachment?.disposables?.forEach((d) => d.dispose());
+        }
+    }
+
+    private attachCoordinator(document: NotebookDocument, webview: NotebookCommunication, c: CommonMessageCoordinator) {
+        let attachment = this.attachedWebViews.get(document.uri.toString());
+        if (!attachment) {
+            attachment = { webviews: new Set<string>(), disposables: [] };
+            this.attachedWebViews.set(document.uri.toString(), attachment);
+        }
+        if (!attachment.webviews.has(webview.editorId)) {
+            attachment.webviews.add(webview.editorId);
+
+            // Attach message requests to this webview (should dupe to all of them)
+            attachment.disposables.push(
                 c.postMessage((e) => {
                     // Special case for webview URI translation
                     if (e.message === InteractiveWindowMessages.ConvertUriForUseInWebViewRequest) {
@@ -66,8 +87,6 @@ export class NotebookIPyWidgetCoordinator implements IExtensionSingleActivationS
                     c.onMessage(m.type, m.payload);
                 })
             );
-
-            // Might have to dispose on shutdown of the webview?
         }
     }
     private onDidChangeKernels(_document: NotebookDocument | undefined) {
