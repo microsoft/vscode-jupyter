1914133e5796a5ebecac86ed4fa5ca84b0c68cb8
Don Jayamanne <don.jayamanne@yahoo.com>
2020-11-13 09:18:32 -0800

    Disable test

--- src/test/datascience/notebook/interrupRestart.vscode.test.ts
+++ src/test/datascience/notebook/interrupRestart.vscode.test.ts
@@ -115,7 +115,8 @@ suite('DataScience - VSCode Notebook - Restart/Interrupt/Cancel/Errors (slow)', 
             await waitForExecutionCompletedWithErrors(cell);
         }
     });
-    test('Restarting kernel will cancel cell execution & we can re-run a cell', async () => {
+    test('Restarting kernel will cancel cell execution & we can re-run a cell', async function () {
+        return this.skip();
         await insertCodeCell('import time\nfor i in range(10000):\n  print(i)\n  time.sleep(0.1)', { index: 0 });
         const cell = vscEditor.document.cells[0];
         // Ensure we click `Yes` when prompted to restart the kernel.
