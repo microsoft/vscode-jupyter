38a64f5bc06fe1d15c0a0be4e75cbe2854f555eb
Don Jayamanne <don.jayamanne@yahoo.com>
2020-11-19 10:22:33 -0800

    Track the interpreter that owns kernelspec in kernelspec info (#3968)

--- src/client/datascience/jupyter/kernels/jupyterKernelSpec.ts
+++ src/client/datascience/jupyter/kernels/jupyterKernelSpec.ts
@@ -17,19 +17,21 @@ export class JupyterKernelSpec implements IJupyterKernelSpec {
     public name: string;
     public language: string;
     public path: string;
-    public specFile: string | undefined;
     public readonly env: NodeJS.ProcessEnv | undefined;
     public display_name: string;
     public argv: string[];
 
     // tslint:disable-next-line: no-any
     public metadata?: Record<string, any> & { interpreter?: Partial<PythonEnvironment> };
-    constructor(specModel: Kernel.ISpecModel, file?: string) {
+    constructor(
+        specModel: Kernel.ISpecModel,
+        public readonly specFile?: string,
+        public readonly interpreterPath?: string
+    ) {
         this.name = specModel.name;
         this.argv = specModel.argv;
         this.language = specModel.language;
         this.path = specModel.argv && specModel.argv.length > 0 ? specModel.argv[0] : '';
-        this.specFile = file;
         this.display_name = specModel.display_name;
         this.metadata = specModel.metadata;
         // tslint:disable-next-line: no-any
