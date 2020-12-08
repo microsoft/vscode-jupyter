a9bc73599c88496f5453d4a55578eedc907b845e
rchiodo <rchiodo@users.noreply.github.com>
2020-12-08 13:56:04 -0800

    Test action

---
+++ .github/actions/print-coverage/index.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const action = async () => {
+    const githubToken = core.getInput('github_token');
+    const commit = core.getInput('commit');
+
+    const pullRequest = github.context.payload.pull_request;
+    const link = (pullRequest && pullRequest.html_url) || github.context.ref;
+    const conclusion =
+        (foundResults && annotations.length === 0) || (!foundResults && !failIfNoTests) ? 'success' : 'failure';
+    const status = 'completed';
+    const head_sha = commit || (pullRequest && pullRequest.head.sha) || github.context.sha;
+    core.info(`Posting status '${status}' with conclusion '${conclusion}' to ${link} (sha: ${head_sha})`);
+
+    const createCheckRequest = {
+        ...github.context.repo,
+        name: 'Test print-coverage',
+        head_sha,
+        status,
+        conclusion,
+        output: {
+            title,
+            summary: '',
+            annotations: annotations.slice(0, 50)
+        }
+    };
+
+    core.debug(JSON.stringify(createCheckRequest, null, 2));
+
+    // make conclusion consumable by downstream actions
+    core.setOutput('conclusion', conclusion);
+
+    const octokit = new github.GitHub(githubToken);
+    await octokit.checks.create(createCheckRequest);
+};
+
+module.exports = action;
