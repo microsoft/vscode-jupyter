b1b54c4b8cd4e8a95c52f8b54ef4ca155d051d4e
rchiodo <rchiodo@users.noreply.github.com>
2021-01-05 16:09:37 -0800

    Reenable ipyvolume test

--- src/test/datascience/uiTests/ipywidget.ui.functional.test.ts
+++ src/test/datascience/uiTests/ipywidget.ui.functional.test.ts
@@ -132,9 +132,9 @@ use(chaiAsPromised);
         async function openBqplotIpynb() {
             return openNotebookFile('bqplot_widgets.ipynb');
         }
-        // async function openIPyVolumeIpynb() {
-        //     return openNotebookFile('ipyvolume_widgets.ipynb');
-        // }
+        async function openIPyVolumeIpynb() {
+            return openNotebookFile('ipyvolume_widgets.ipynb');
+        }
         async function openPyThreejsIpynb() {
             return openNotebookFile('pythreejs_widgets.ipynb');
         }
@@ -409,31 +409,29 @@ use(chaiAsPromised);
             });
         });
         test('Render ipyvolume', async function () {
-            // See bug https://github.com/microsoft/vscode-jupyter/issues/4152
-            this.skip();
-            // const { notebookUI } = await openIPyVolumeIpynb();
-            // await assert.eventually.isFalse(notebookUI.cellHasOutput(3));
+            const { notebookUI } = await openIPyVolumeIpynb();
+            await assert.eventually.isFalse(notebookUI.cellHasOutput(3));
 
-            // // Confirm sliders and canvas are rendered.
-            // await retryIfFail(async () => {
-            //     await notebookUI.executeCell(1);
-            //     const cellOutputHtml = await notebookUI.getCellOutputHTML(1);
-            //     assert.include(cellOutputHtml, '<canvas ');
+            // Confirm sliders and canvas are rendered.
+            await retryIfFail(async () => {
+                await notebookUI.executeCell(1);
+                const cellOutputHtml = await notebookUI.getCellOutputHTML(1);
+                assert.include(cellOutputHtml, '<canvas ');
 
-            //     const cellOutput = await notebookUI.getCellOutput(1);
-            //     const sliders = await cellOutput.$$('div.ui-slider');
-            //     assert.equal(sliders.length, 2);
-            // });
+                const cellOutput = await notebookUI.getCellOutput(1);
+                const sliders = await cellOutput.$$('div.ui-slider');
+                assert.equal(sliders.length, 2);
+            });
 
-            // // Confirm canvas is rendered.
-            // await retryIfFail(async () => {
-            //     await notebookUI.executeCell(2);
-            //     await notebookUI.executeCell(3);
-            //     await notebookUI.executeCell(4);
+            // Confirm canvas is rendered.
+            await retryIfFail(async () => {
+                await notebookUI.executeCell(2);
+                await notebookUI.executeCell(3);
+                await notebookUI.executeCell(4);
 
-            //     const cellOutputHtml = await notebookUI.getCellOutputHTML(4);
-            //     assert.include(cellOutputHtml, '<canvas ');
-            // });
+                const cellOutputHtml = await notebookUI.getCellOutputHTML(4);
+                assert.include(cellOutputHtml, '<canvas ');
+            });
         });
         test('Render pythreejs', async () => {
             const { notebookUI } = await openPyThreejsIpynb();
