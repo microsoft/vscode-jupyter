6649655263e139f0d0fb0d8954f18d1390f870c4
Ian Huff <ianhuff@lcs2d-Dell-04.northamerica.corp.microsoft.com>
2021-01-13 13:51:25 -0800

    remove variable view experiement and enable along with native notebook

--- src/client/datascience/variablesView/variableViewActivationService.ts
+++ src/client/datascience/variablesView/variableViewActivationService.ts
@@ -1,8 +1,6 @@
 import { inject, injectable } from 'inversify';
 import { window } from 'vscode';
 import { IExtensionSingleActivationService } from '../../activation/types';
-import { ICommandManager } from '../../common/application/types';
-import { ContextKey } from '../../common/contextKey';
 import { Experiments } from '../../common/experiments/groups';
 import { IExperimentService, IExtensionContext } from '../../common/types';
 import { IVariableViewProvider } from './types';
@@ -13,22 +11,18 @@ export class VariableViewActivationService implements IExtensionSingleActivation
     constructor(
         @inject(IExtensionContext) private extensionContext: IExtensionContext,
         @inject(IVariableViewProvider) private variableViewProvider: IVariableViewProvider,
-        @inject(IExperimentService) private experimentService: IExperimentService,
-        @inject(ICommandManager) private commandManager: ICommandManager
+        @inject(IExperimentService) private experimentService: IExperimentService
     ) {}
 
     public async activate() {
-        const context = new ContextKey('jupyter.nativevariableview', this.commandManager);
-        if (await this.experimentService.inExperiment(Experiments.NativeVariableView)) {
-            context.set(true).ignoreErrors();
+        // Only activate this when in the NativeNotebook experiment
+        if (await this.experimentService.inExperiment(Experiments.NativeNotebook)) {
             this.extensionContext.subscriptions.push(
                 // Consider not using retainContext here? This will save the context of our view, but take more memory in VS Code.
                 window.registerWebviewViewProvider(this.variableViewProvider.viewType, this.variableViewProvider, {
                     webviewOptions: { retainContextWhenHidden: true }
                 })
             );
-        } else {
-            context.set(false).ignoreErrors();
         }
     }
 }
