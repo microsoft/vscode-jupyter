d8eae7df3d3d7bdab13d9d2d0c4c40357ad70dcc
Ian Huff <ianhuff@lcs2d-Dell-04.northamerica.corp.microsoft.com>
2021-01-14 14:52:18 -0800

    removing some logging

--- src/test/datascience/variableView/variableView.vscode.test.ts
+++ src/test/datascience/variableView/variableView.vscode.test.ts
@@ -34,7 +34,6 @@ suite('DataScience - VariableView', () => {
     let vscodeNotebook: IVSCodeNotebook;
     suiteSetup(async function () {
         this.timeout(120_000); // IANHU: From other tests? Reduce this?
-        console.log('**** Start variableView suiteSetup ****');
         api = await initialize();
 
         // Don't run if we can't use the native notebook interface
@@ -69,37 +68,17 @@ suite('DataScience - VariableView', () => {
 
     // Test showing the variable view
     test('Can show VariableView', async function () {
-        this.timeout(120_000); // IANHU: Just for testing
-        //commands.executeCommand('workbench.action.togglePanel');
-        console.log('**** Start variableView test ****');
         // Add one simple cell and execute it
         await insertCodeCell('test = "MYTESTVALUE"', { index: 0 });
         const cell = vscodeNotebook.activeNotebookEditor?.document.cells![0]!;
         await executeCell(cell);
         await waitForExecutionCompletedSuccessfully(cell);
 
-        console.log('**** Cell execution done ****');
         // Send the command to open the view
         commandManager.executeCommand(Commands.OpenVariableView);
 
-        // IANHU: Remove, just for testing
-        //await sleep(4_000);
-
-        // Another view open?
-        //commandManager.executeCommand(Commands.OpenVariableView);
-        //await sleep(4_000);
-
-        console.log('**** Sleep finished ****');
-
         // Now check to see if we can actually look at the variable view
         const variableView = await variableViewProvider.activeVariableView;
-
-        if (variableView) {
-            console.log('**** found variableView');
-        }
-
-        // Check our messages for variable view
-        //const variableMessageWrapper = new OnMessageWrapper(variableView as any);
 
         // Add our message listener
         const onMessageListener = new OnMessageListener(variableView);
@@ -108,19 +87,12 @@ suite('DataScience - VariableView', () => {
         await insertCodeCell('test2 = "MYTESTVALUE2"', { index: 1 });
         const cell2 = vscodeNotebook.activeNotebookEditor?.document.cells![1]!;
         await executeCell(cell2);
-        //await waitForExecutionCompletedSuccessfully(cell2);
 
         await onMessageListener.waitForMessage(InteractiveWindowMessages.VariablesComplete);
 
         const htmlResult = await variableView?.getHTMLById('variable-view-main-panel');
-        //const rootHtml = await variableView?.getElementByIdAsync('root');
-
-        //console.log(`**** rootHTML ${rootHtml}`);
-        console.log(`**** htmlResult ${htmlResult} ****`);
 
         expect(htmlResult).to.contain('MYTESTVALUE');
         expect(htmlResult).to.contain('MYTESTVALUE2');
-
-        //await sleep(5_000);
     });
 });
