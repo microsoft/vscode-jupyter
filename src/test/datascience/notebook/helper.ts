3cafafe7506d81284143be2508d5985aa2977ead
Don Jayamanne <don.jayamanne@yahoo.com>
2021-01-15 15:43:51 -0800

    Oops

--- src/test/datascience/notebook/helper.ts
+++ src/test/datascience/notebook/helper.ts
@@ -273,7 +273,6 @@ export async function waitForKernelToGetAutoSelected(expectedLanguage?: string) 
             kernelInfo = '<No specific kernel expected>. Non Jupyter Kernel';
             return true;
         }
-        console.log(kernelInfo);
         return false;
     };
 
