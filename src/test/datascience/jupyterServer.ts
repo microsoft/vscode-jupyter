eb07fa7eb3198478f8242c39342ebd69d2da1e10
Don Jayamanne <don.jayamanne@yahoo.com>
2021-01-19 13:26:27 -0800

    Fixes

--- src/test/datascience/jupyterServer.ts
+++ src/test/datascience/jupyterServer.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { ChildProcess } from 'child_process';
 import * as getFreePort from 'get-port';
 import * as path from 'path';
 import { Uri } from 'vscode';
