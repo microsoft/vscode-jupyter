8290ed30dfcbadc0fe22fa63a66dbb734663d1dd
Don Jayamanne <don.jayamanne@yahoo.com>
2021-01-20 12:24:37 -0800

    Leave status as idle when re-opening notebooks (#4428)

--- src/test/datascience/notebook/saving.vscode.test.ts
+++ src/test/datascience/notebook/saving.vscode.test.ts
@@ -93,7 +93,7 @@ suite('DataScience - VSCode Notebook - (Saving) (slow)', function () {
             'Cells did not finish executing'
         );
 
-        function verifyCelMetadata() {
+        function verifyCelMetadata(reOpened = false) {
             assert.lengthOf(cell1.outputs, 1, 'Incorrect output for cell 1');
             assert.lengthOf(cell2.outputs, 1, 'Incorrect output for cell 2');
             assert.lengthOf(cell3.outputs, 0, 'Incorrect output for cell 3'); // stream and interrupt error.
@@ -101,19 +101,27 @@ suite('DataScience - VSCode Notebook - (Saving) (slow)', function () {
 
             assert.equal(
                 cell1.metadata.runState,
-                vscodeNotebookEnums.NotebookCellRunState.Success,
-                'Incorrect state 1'
+                reOpened
+                    ? vscodeNotebookEnums.NotebookCellRunState.Idle
+                    : vscodeNotebookEnums.NotebookCellRunState.Success,
+                'Incorrect state in cell 1'
             );
-            assert.equal(cell2.metadata.runState, vscodeNotebookEnums.NotebookCellRunState.Error, 'Incorrect state 2');
+            assert.equal(
+                cell2.metadata.runState,
+                reOpened
+                    ? vscodeNotebookEnums.NotebookCellRunState.Idle
+                    : vscodeNotebookEnums.NotebookCellRunState.Error,
+                'Incorrect state in cell 2'
+            );
             assert.equal(
                 cell3.metadata.runState || vscodeNotebookEnums.NotebookCellRunState.Idle,
                 vscodeNotebookEnums.NotebookCellRunState.Idle,
-                'Incorrect state 3'
+                'Incorrect state in cell 3'
             );
             assert.equal(
                 cell4.metadata.runState || vscodeNotebookEnums.NotebookCellRunState.Idle,
                 vscodeNotebookEnums.NotebookCellRunState.Idle,
-                'Incorrect state 4'
+                'Incorrect state in cell 4'
             );
 
             assertHasTextOutputInVSCode(cell1, '1', 0);
@@ -154,6 +162,6 @@ suite('DataScience - VSCode Notebook - (Saving) (slow)', function () {
         // Reopen the notebook & validate the metadata.
         await openNotebook(api.serviceContainer, testEmptyIPynb.fsPath);
         initializeCells();
-        verifyCelMetadata();
+        verifyCelMetadata(true);
     });
 });
