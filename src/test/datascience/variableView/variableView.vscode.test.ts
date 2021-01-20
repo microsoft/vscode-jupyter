8f883c6d7595eb7e48d4f6229abd85de133e1c2a
Ian Huff <ianhuff@ADINATRU-SB01.ntdev.corp.microsoft.com>
2021-01-20 15:58:36 -0800

    increase timeout for slow test

--- src/test/datascience/variableView/variableView.vscode.test.ts
+++ src/test/datascience/variableView/variableView.vscode.test.ts
@@ -72,6 +72,7 @@ suite('DataScience - VariableView', () => {
 
     // Test showing the basic variable view with a value or two
     test('Can show VariableView', async function () {
+        this.timeout(60_000);
         // Add one simple cell and execute it
         await insertCodeCell('test = "MYTESTVALUE"', { index: 0 });
         const cell = vscodeNotebook.activeNotebookEditor?.document.cells![0]!;
