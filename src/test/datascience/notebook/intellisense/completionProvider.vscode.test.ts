a0d1befeef31565ce37f7bb1bdbdb148e04497ae
Don Jayamanne <don.jayamanne@yahoo.com>
2021-01-20 14:52:27 -0800

    Not required

--- src/test/datascience/notebook/intellisense/completionProvider.vscode.test.ts
+++ src/test/datascience/notebook/intellisense/completionProvider.vscode.test.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-'use strict';
-
 /* eslint-disable @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 import { assert } from 'chai';
 import * as sinon from 'sinon';
