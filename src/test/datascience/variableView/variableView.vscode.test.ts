385ee9053b826c149195bc85984f2d5683f97489
Ian Huff <ianhuff@ADINATRU-SB01.ntdev.corp.microsoft.com>
2021-01-20 11:29:26 -0800

    longer timeout

--- src/test/datascience/variableView/variableView.vscode.test.ts
+++ src/test/datascience/variableView/variableView.vscode.test.ts
@@ -73,6 +73,7 @@ suite('DataScience - VariableView', () => {
     // Test showing the basic variable view with a value or two
     test('Can show VariableView', async function () {
         //this.skip(); // Re-enable in CI when #4412 is fixed
+        this.timeout(60_000);
         // Add one simple cell and execute it
         await insertCodeCell('test = "MYTESTVALUE"', { index: 0 });
         const cell = vscodeNotebook.activeNotebookEditor?.document.cells![0]!;
