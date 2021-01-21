0bc9308752b9101361a957ed2416c2fea790f06c
rchiodo <rchiodo@users.noreply.github.com>
2021-01-21 11:24:11 -0800

    Empty test is timing out in teardown

--- src/test/datascience/notebook/notebookEditor.vscode.test.ts
+++ src/test/datascience/notebook/notebookEditor.vscode.test.ts
@@ -115,29 +115,4 @@ suite('Notebook Editor tests', () => {
         // The third cell should have a runState of Success
         assert.strictEqual(thirdCell?.metadata.runState, vscodeNotebookEnums.NotebookCellRunState.Success);
     });
-    test('Switch kernels', async function () {
-        this.skip();
-        // Do this after talking with VS code team
-        // // add a cell
-        // await insertCodeCell('print("0")', { index: 0 });
-
-        // const cell = vscodeNotebook.activeNotebookEditor?.document.cells![0]!;
-
-        // await executeCell(cell);
-
-        // // Wait till execution count changes and status is success.
-        // await waitForExecutionCompletedSuccessfully(cell);
-
-        // // Switch kernels to the other kernel
-        // const kernels = await kernelProvider.provideKernels(
-        //     vscodeNotebook.activeNotebookEditor!.document,
-        //     CancellationToken.None
-        // );
-        // if (kernels?.length && kernels?.length > 0) {
-        //     // We have multiple kernels. Try switching
-        //     await commandManager.executeCommand(
-        //         'notebook.selectKernel',
-        //     );
-        // }
-    });
 });
