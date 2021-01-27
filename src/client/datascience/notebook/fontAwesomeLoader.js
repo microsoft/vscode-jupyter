fa63b3d56b22c301ca3b2ac2a0b832bcd3f94947
David Kutugata <dakutuga@microsoft.com>
2021-01-27 11:07:55 -0800

    add missing license

--- src/client/datascience/notebook/fontAwesomeLoader.js
+++ src/client/datascience/notebook/fontAwesomeLoader.js
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 const getPublicPath = () => {
     return new URL(scriptUrl.replace(/[^/]+$/, '')).toString();
 };
