5a1c6cb74eb4d76c957e6a005956a43073c23d01
Don Jayamanne <don.jayamanne@yahoo.com>
2021-02-02 13:12:24 -0800

    Fixes

--- src/client/common/amlContext.ts
+++ src/client/common/amlContext.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { inject, named } from 'inversify';
+import { inject, injectable, named } from 'inversify';
 import { Memento } from 'vscode';
 import { IExtensionSingleActivationService } from '../activation/types';
 import { setSharedProperty } from '../telemetry';
@@ -11,6 +11,7 @@ import { noop } from './utils/misc';
 
 const amlComputeMementoKey = 'JVSC_IS_AML_COMPUTE_INSTANCE';
 
+@injectable()
 export class AmlComputeContext implements IExtensionSingleActivationService {
     constructor(
         @inject(IMemento) @named(GLOBAL_MEMENTO) private readonly memento: Memento,
