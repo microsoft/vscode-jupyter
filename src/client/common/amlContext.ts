4c048cb24713942dcf701532133e9178576bd325
Don Jayamanne <don.jayamanne@yahoo.com>
2021-02-05 15:30:52 -0800

    Use Environment variable to detect if AML Compute

--- src/client/common/amlContext.ts
+++ src/client/common/amlContext.ts
@@ -5,7 +5,6 @@ import { inject, injectable, named } from 'inversify';
 import { Memento } from 'vscode';
 import { IExtensionSingleActivationService } from '../activation/types';
 import { setSharedProperty } from '../telemetry';
-import { IWorkspaceService } from './application/types';
 import { GLOBAL_MEMENTO, IMemento } from './types';
 import { noop } from './utils/misc';
 
@@ -13,24 +12,13 @@ const amlComputeMementoKey = 'JVSC_IS_AML_COMPUTE_INSTANCE';
 
 @injectable()
 export class AmlComputeContext implements IExtensionSingleActivationService {
-    constructor(
-        @inject(IMemento) @named(GLOBAL_MEMENTO) private readonly memento: Memento,
-        @inject(IWorkspaceService) private readonly workspace: IWorkspaceService
-    ) {}
+    constructor(@inject(IMemento) @named(GLOBAL_MEMENTO) private readonly memento: Memento) {}
 
     public get isAmlCompute() {
         return this.memento.get<boolean>(amlComputeMementoKey, false) || this.isAmlComputeWorkspace();
     }
     private isAmlComputeWorkspace() {
-        let isRemoteConnection: boolean = false;
-        // If there is at least 1 folder, we can check if the scheme and authority match a remote connection
-        if (Array.isArray(this.workspace.workspaceFolders) && this.workspace.workspaceFolders.length > 0) {
-            // We are in a remote connection to a CI if the scheme is vscode-remote and the authority contains amlext
-            isRemoteConnection = this.workspace.workspaceFolders.some(
-                (item) => item.uri.scheme === 'vscode-remote' && item.uri.authority.indexOf('amlext') >= 0
-            );
-        }
-        return isRemoteConnection;
+        return typeof process.env.AZURE_EXTENSION_DIR === 'string';
     }
     public async activate(): Promise<void> {
         if (this.memento.get<boolean>(amlComputeMementoKey, false)) {
