743b19d508f386d8a6b22c9d5b2d1971536aeadb
Joyce Er <joyceerhuiling@gmail.com>
2021-02-10 12:42:43 -0800

    Add improved Tensor tooltips for Python files which have been run in the interactive window (#4693)

--- src/client/common/experiments/groups.ts
+++ src/client/common/experiments/groups.ts
@@ -7,5 +7,7 @@ export enum Experiments {
     // Experiment to turn on custom editor or VS Code Native Notebook API support.
     NativeNotebook = 'NativeNotebookEditor',
     // Experiment to enable an enhanced data viewer.
-    SliceDataViewer = 'jupyterEnhancedDataViewer'
+    SliceDataViewer = 'jupyterEnhancedDataViewer',
+    // Experiment to enable tensor tooltips.
+    EnhancedTooltips = 'jupyterEnhancedTensorTooltips'
 }
