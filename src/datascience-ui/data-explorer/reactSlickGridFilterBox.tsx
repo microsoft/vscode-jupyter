0b0eaada50866adcdeadc2c5ca82bd4b50945b51
Joyce Er <huer@microsoft.com>
2021-02-12 19:18:10 -0800

    New filter box

--- src/datascience-ui/data-explorer/reactSlickGridFilterBox.tsx
+++ src/datascience-ui/data-explorer/reactSlickGridFilterBox.tsx
@@ -3,11 +3,23 @@
 'use strict';
 
 import * as React from 'react';
+import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
+import { IIconProps } from 'office-ui-fabric-react/lib/Icon';
+import { initializeIcons } from '@fluentui/react';
+
+initializeIcons();
 
 import './reactSlickGridFilterBox.css';
 
+const filterIcon: IIconProps = { iconName: 'Filter', styles: { root: {
+    fontSize: 'var(--vscode-font-size)',
+    width: 'var(--vscode-font-size)',
+    color: 'var(--vscode-editor-inactiveSelectionBackground)'
+}}};
+
 interface IFilterProps {
     column: Slick.Column<Slick.SlickData>;
+    fontSize: number;
     onChange(val: string, column: Slick.Column<Slick.SlickData>): void;
 }
 
@@ -17,21 +29,19 @@ export class ReactSlickGridFilterBox extends React.Component<IFilterProps> {
     }
 
     public render() {
+        const placeholder = this.props.column.id === '0' ? 'Filter' : '';
         return (
-            <input
-                type="text"
-                tabIndex={0}
-                aria-label={this.props.column.name}
-                className="filter-box"
-                onChange={this.updateInputValue}
-            />
+            <SearchBox placeholder={placeholder} iconProps={filterIcon} onChange={this.updateInputValue} onClear={this.clearInputValue} tabIndex={0} ariaLabel={this.props.column.name} className="filter-box" />
         );
     }
 
-    private updateInputValue = (evt: React.SyntheticEvent) => {
-        const element = evt.currentTarget as HTMLInputElement;
-        if (element) {
-            this.props.onChange(element.value, this.props.column);
+    private clearInputValue = () => {
+        this.props.onChange('', this.props.column);
+    }
+
+    private updateInputValue = (_event?: React.ChangeEvent<HTMLInputElement> | undefined, newValue?: string | undefined) => {
+        if (newValue) {
+            this.props.onChange(newValue, this.props.column);
         }
     };
 }
