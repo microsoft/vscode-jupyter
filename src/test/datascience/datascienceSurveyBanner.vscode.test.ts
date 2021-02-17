8f093cb28043ea4ea4576b5b703e89cd705f6b8f
David Kutugata <dakutuga@microsoft.com>
2021-02-16 18:35:23 -0800

    Fix surveys (#4838)
    * remove experiment service
    
    * lint

--- src/test/datascience/datascienceSurveyBanner.vscode.test.ts
+++ src/test/datascience/datascienceSurveyBanner.vscode.test.ts
@@ -9,12 +9,7 @@ import * as fakeTimers from '@sinonjs/fake-timers';
 import * as sinon from 'sinon';
 import { anything, instance, mock, when, verify, resetCalls } from 'ts-mockito';
 import { IApplicationEnvironment, IApplicationShell, IVSCodeNotebook } from '../../client/common/application/types';
-import {
-    IBrowserService,
-    IExperimentService,
-    IPersistentState,
-    IPersistentStateFactory
-} from '../../client/common/types';
+import { IBrowserService, IPersistentState, IPersistentStateFactory } from '../../client/common/types';
 import {
     BannerType,
     DataScienceSurveyBanner,
@@ -45,7 +40,6 @@ import { MillisecondsInADay } from '../../client/constants';
         let appEnv: IApplicationEnvironment;
         let vscodeNotebook: IVSCodeNotebook;
         let notebookExtensibility: INotebookExtensibility;
-        let experimentService: IExperimentService;
         let clock: fakeTimers.InstalledClock;
         teardown(() => {
             sinon.restore();
@@ -62,7 +56,6 @@ import { MillisecondsInADay } from '../../client/constants';
             persistentStateFactory = mock<IPersistentStateFactory>();
             vscodeNotebook = mock<IVSCodeNotebook>();
             notebookExtensibility = mock<INotebookExtensibility>();
-            experimentService = mock<IExperimentService>();
 
             when(appEnv.uiKind).thenReturn(UIKind.Desktop);
             when(appEnv.channel).thenReturn(UseVSCodeNotebookEditorApi ? 'insiders' : 'stable');
@@ -131,8 +124,7 @@ import { MillisecondsInADay } from '../../client/constants';
                 instance(vscodeNotebook),
                 instance(notebookExtensibility),
                 [],
-                UseVSCodeNotebookEditorApi,
-                instance(experimentService)
+                UseVSCodeNotebookEditorApi
             );
         }
         test(type + ' - Confirm prompt is displayed & only once per session', async () => {
