6972e283f796e5d273e8ab88b400e1ab0179d247
Don Jayamanne <don.jayamanne@yahoo.com>
2021-02-23 14:37:06 -0800

    Fix formatting

--- src/client/datascience/notebook/helpers/helpers.ts
+++ src/client/datascience/notebook/helpers/helpers.ts
@@ -596,7 +596,7 @@ export type CellOutputMetadata = {
          * We don't know of others properties, but this is definitely used.
          */
         display_id?: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } & any;
     /**
      * Original cell output type
@@ -746,7 +746,7 @@ export function getTextOutputValue(output: NotebookCellOutput): string {
                 opit.mime === CellOutputMimeTypes.textStream ||
                 opit.mime === 'text/plain' ||
                 opit.mime === 'text/markdown'
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
         )?.value as any) || ''
     );
 }
