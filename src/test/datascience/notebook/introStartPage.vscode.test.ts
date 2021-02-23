a33c9a23dd77cbe42817947f872fd6aae46398b8
Don Jayamanne <don.jayamanne@yahoo.com>
2021-02-23 12:54:11 -0800

    Logging

--- src/test/datascience/notebook/introStartPage.vscode.test.ts
+++ src/test/datascience/notebook/introStartPage.vscode.test.ts
@@ -90,6 +90,9 @@ suite('DataScience - VSCode Notebook - Native Notebook Experiment', function () 
         await sleep(1_000);
 
         assert.isUndefined(notebookEditorProvider.activeEditor);
+        notebookEditorProvider.editors.map((item) => {
+            assert.fail(item, undefined, `There should be no document - ${item.file.fsPath}`);
+        });
         assert.equal(notebookEditorProvider.editors.length, 0);
     });
     test('Do not display start page for Stable VS Code Not in experiment', async () => {
