2d842cbe27b578ee54f68539ff340d00a10df651
rchiodo <rchiodo@users.noreply.github.com>
2021-02-24 17:10:29 -0800

    Outline for tests for kernel finders

---
+++ src/test/datascience/kernel-launcher/remoteKernelFinder.unit.test.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+'use strict';
+
+import { assert } from 'chai';
+import * as sinon from 'sinon';
+import { anything, instance, mock, when } from 'ts-mockito';
+
+suite('Remote Kernel Finder', () => {
+    setup(() => {});
+    test('Kernels found', async () => {});
+    test('Active sessions found', async () => {});
+    test('Can match based on notebook metadata', async () => {});
+});
