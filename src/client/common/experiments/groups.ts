120d288094e2c5982794d6b8f1a19ca25d9cbf07
David Kutugata <dakutuga@microsoft.com>
2021-02-26 17:21:32 -0800

    remove custom editor experiment (#4968)
    * remove custom editor experiment
    
    * mutually exclude native notebooks and custom
    editor

--- src/client/common/experiments/groups.ts
+++ src/client/common/experiments/groups.ts
@@ -3,8 +3,6 @@
 
 export enum Experiments {
     // Experiment to turn on custom editor or VS Code Native Notebook API support.
-    CustomEditor = 'CustomEditor',
-    // Experiment to turn on custom editor or VS Code Native Notebook API support.
     NativeNotebook = 'NativeNotebookEditor',
     // Experiment to enable an enhanced data viewer.
     SliceDataViewer = 'jupyterEnhancedDataViewer',
