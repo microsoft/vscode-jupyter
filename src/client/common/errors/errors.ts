8ea84ae60e7933a1f9a2e2ee6034fcb809d627c0
Don Jayamanne <don.jayamanne@yahoo.com>
2021-03-02 08:04:06 -0800

    More telemetry to track kernel failure reasons (#4982)

--- src/client/common/errors/errors.ts
+++ src/client/common/errors/errors.ts
@@ -7,7 +7,8 @@ const taggers = [
     tagWithDllLoadError,
     tagWithOldIPyKernel,
     tagWithOldIPython,
-    tagWithChildProcessExited
+    tagWithChildProcessExited,
+    tagWithKernelRestarterFailed
 ];
 export function getErrorTags(stdErrOrStackTrace: string) {
     const tags: string[] = [];
@@ -50,6 +51,18 @@ function tagWithChildProcessExited(stdErrOrStackTrace: string, tags: string[] = 
     // StackTrace = at ChildProcess.exithandler child_process.js:312:12\n\tat ChildProcess.emit events.js:315:20\n\tat ChildProcess.EventEmitter.emit domain.js:483:12\n\tat maybeClose <REDACTED: user-file-path>:1021:16\n\tat Socket <REDACTED: user-file-path>:443:11\n\tat Socket.emit events.js:315:20\n\tat Socket.EventEmitter.emit domain.js:483:12\n\tat Pipe net.js:674:12"
     if (stdErrOrStackTrace.includes('ChildProcess.exithandler'.toLowerCase())) {
         tags.push('childproc.exit');
+    }
+}
+function tagWithKernelRestarterFailed(stdErrOrStackTrace: string, tags: string[] = []) {
+    /*
+    [I 14:48:13.999 NotebookApp] KernelRestarter: restarting kernel (1/5), new random ports
+    [I 14:48:17.011 NotebookApp] KernelRestarter: restarting kernel (2/5), new random ports
+    [I 14:48:20.023 NotebookApp] KernelRestarter: restarting kernel (3/5), new random ports
+    [I 14:48:23.031 NotebookApp] KernelRestarter: restarting kernel (4/5), new random ports
+    [W 14:48:26.040 NotebookApp] KernelRestarter: restart failed
+    */
+    if (stdErrOrStackTrace.includes('KernelRestarter: restart failed'.toLowerCase())) {
+        tags.push('KernelRestarter.failed');
     }
 }
 function tagWithZmqError(stdErr: string, tags: string[] = []) {
