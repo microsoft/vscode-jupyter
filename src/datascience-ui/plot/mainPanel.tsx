f7cc25a0877324882b888a774893ab74de9b049a
Joyce Er <huer@microsoft.com>
2021-03-08 14:52:28 -0800

    Add initializeIcons comment

--- src/datascience-ui/plot/mainPanel.tsx
+++ src/datascience-ui/plot/mainPanel.tsx
@@ -21,8 +21,6 @@ import { SvgViewer } from '../react-common/svgViewer';
 import { TestSvg } from './testSvg';
 import { Toolbar } from './toolbar';
 
-import { initializeIcons } from '@fluentui/react';
-initializeIcons();
 
 // Our css has to come after in order to override body styles
 export interface IMainPanelProps {
