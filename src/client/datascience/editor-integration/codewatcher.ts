9dd90371ec791ffc25b28f0a14f46d771e347b91
Andrew Craig <andy.peter.craig@gmail.com>
2021-03-23 21:13:25 +0900

    Fix for lint

--- src/client/datascience/editor-integration/codewatcher.ts
+++ src/client/datascience/editor-integration/codewatcher.ts
@@ -358,7 +358,7 @@ export class CodeWatcher implements ICodeWatcher {
         const cellDelineator = this.getDefaultCellMarker(editor.document.uri);
 
         if (editor) {
-            editor.edit((editBuilder) => {
+            void editor.edit((editBuilder) => {
                 let lastCell = true;
 
                 for (let i = editor.selection.end.line + 1; i < editor.document.lineCount; i += 1) {
@@ -449,7 +449,7 @@ export class CodeWatcher implements ICodeWatcher {
             new Position(startLineNumber, startCharacterNumber),
             new Position(endLineNumber, endCharacterNumber)
         );
-        editor.edit((editBuilder) => {
+        void editor.edit((editBuilder) => {
             editBuilder.replace(cellExtendedRange, '');
             this.codeLensUpdatedEvent.fire();
         });
@@ -745,7 +745,7 @@ export class CodeWatcher implements ICodeWatcher {
                 ? `${cellMarker} [markdown]${definitionExtra}` // code -> markdown
                 : `${cellMarker}${definitionExtra}`; // markdown -> code
 
-        editor.edit(async (editBuilder) => {
+        void editor.edit(async (editBuilder) => {
             editBuilder.replace(definitionLine.range, newDefinitionText);
             cell.cell_type = toCellType;
             if (cell.range.start.line < cell.range.end.line) {
@@ -758,9 +758,9 @@ export class CodeWatcher implements ICodeWatcher {
                 // ensure all lines in markdown cell have a comment.
                 // these are not included in the test because it's unclear
                 // how TypeMoq works with them.
-                commands.executeCommand('editor.action.removeCommentLine');
+                void commands.executeCommand('editor.action.removeCommentLine');
                 if (toCellType === 'markdown') {
-                    commands.executeCommand('editor.action.addCommentLine');
+                    void commands.executeCommand('editor.action.addCommentLine');
                 }
             }
         });
@@ -945,7 +945,7 @@ export class CodeWatcher implements ICodeWatcher {
         const cellStartPosition = new Position(line, 0);
         const newCursorPosition = new Position(line + 1, 0);
 
-        editor.edit((editBuilder) => {
+        void editor.edit((editBuilder) => {
             editBuilder.insert(cellStartPosition, newCell);
             this.codeLensUpdatedEvent.fire();
         });
