a7d2d3cbfd554b0626b166684eb4d6af478131e0
Andrew Craig <andy.peter.craig@gmail.com>
2021-03-24 01:12:50 +0900

    Add string argument to jupyter.execSelectionInteractive for extensibility (#5053)
    * Add text parameter to execSelectionInteractive
    
    * Add unit test for RunSelection with text argument
    
    * Remove hard-coded string in RunSelection unit test
    
    Source of runSelectionOrLine string to send can now be
    getSelectedTextToExecute or text argument, so remove hard-coded string
    to make sure result is output of getSelectedTextToExecute.
    
    * Add news entry file
    
    * Fix null to undefined
    
    * Remove redundant undefined defaults
    
    * Remove codewatcher.unit.test.ts from lint ignore
    
    * Remove codewatcher.ts from lint ignore
    
    * Fix for lint
    
    * Fix for Prettier

--- src/client/datascience/commands/commandRegistry.ts
+++ src/client/datascience/commands/commandRegistry.ts
@@ -81,7 +81,9 @@ export class CommandRegistry implements IDisposable {
         this.registerCommand(Commands.RunCell, this.runCell);
         this.registerCommand(Commands.RunCurrentCell, this.runCurrentCell);
         this.registerCommand(Commands.RunCurrentCellAdvance, this.runCurrentCellAndAdvance);
-        this.registerCommand(Commands.ExecSelectionInInteractiveWindow, this.runSelectionOrLine);
+        this.registerCommand(Commands.ExecSelectionInInteractiveWindow, (text: string | undefined) =>
+            this.runSelectionOrLine(text)
+        );
         this.registerCommand(Commands.RunAllCellsAbove, this.runAllCellsAbove);
         this.registerCommand(Commands.RunCellAndAllBelow, this.runCellAndAllBelow);
         this.registerCommand(Commands.InsertCellBelowPosition, this.insertCellBelowPosition);
@@ -299,10 +301,10 @@ export class CommandRegistry implements IDisposable {
         }
     }
 
-    private async runSelectionOrLine(): Promise<void> {
+    private async runSelectionOrLine(text: string | undefined): Promise<void> {
         const activeCodeWatcher = this.getCurrentCodeWatcher();
         if (activeCodeWatcher) {
-            return activeCodeWatcher.runSelectionOrLine(this.documentManager.activeTextEditor);
+            return activeCodeWatcher.runSelectionOrLine(this.documentManager.activeTextEditor, text);
         } else {
             return;
         }
