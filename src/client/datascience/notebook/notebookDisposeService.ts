5335ac8b339d65de2c54680186436dc40f9c4a54
Don Jayamanne <don.jayamanne@yahoo.com>
2021-03-26 08:16:43 -0700

    Logging for kernel shutdown when closing Notebooks (#5320)

--- src/client/datascience/notebook/notebookDisposeService.ts
+++ src/client/datascience/notebook/notebookDisposeService.ts
@@ -8,6 +8,7 @@ import { NotebookDocument } from 'vscode';
 import { IExtensionSingleActivationService } from '../../activation/types';
 import { IVSCodeNotebook } from '../../common/application/types';
 import { UseVSCodeNotebookEditorApi } from '../../common/constants';
+import { traceInfo } from '../../common/logger';
 import { IDisposableRegistry } from '../../common/types';
 import { noop } from '../../common/utils/misc';
 import { IKernelProvider } from '../jupyter/kernels/types';
@@ -30,8 +31,13 @@ export class NotebookDisposeService implements IExtensionSingleActivationService
         this.vscNotebook.onDidCloseNotebookDocument(this.onDidCloseNotebookDocument, this, this.disposables);
     }
     private onDidCloseNotebookDocument(document: NotebookDocument) {
+        traceInfo(`Notebook Closed ${document.uri.toString()}`);
         const kernel = this.kernelProvider.get(document.uri);
         if (kernel) {
+            traceInfo(
+                `Kernel got disposed as a result of closing the notebook ${document.uri.toString()}`,
+                kernel.uri.toString()
+            );
             kernel.dispose().catch(noop);
         }
         this.notebookProvider.disposeAssociatedNotebook({ identity: document.uri });
