a1ddb4cc22ff4e594230c922d5ec06fd0c13213f
Joyce Er <huer@microsoft.com>
2021-04-21 20:15:31 -0700

    Try creating IW another way

--- src/test/smoke/datascience.smoke.test.ts
+++ src/test/smoke/datascience.smoke.test.ts
@@ -19,25 +19,50 @@ import { closeActiveWindows, initialize, initializeTest } from '../initialize';
 const timeoutForCellToRun = 3 * 60 * 1_000;
 suite('Smoke Tests', () => {
     let api: IExtensionTestApi;
+    let provider: IInteractiveWindowProvider;
     suiteSetup(async function () {
         if (!IS_SMOKE_TEST) {
             return this.skip();
         }
         api = await initialize();
+        provider = api.serviceManager.get<IInteractiveWindowProvider>(IInteractiveWindowProvider);
         await setAutoSaveDelayInWorkspaceRoot(1);
     });
     setup(initializeTest);
     suiteTeardown(closeActiveWindows);
     teardown(closeActiveWindows);
 
+    async function runCellInInteractiveWindow() {
+        const file = path.join(
+            EXTENSION_ROOT_DIR_FOR_TESTS,
+            'src',
+            'test',
+            'pythonFiles',
+            'datascience',
+            'simple_note_book.py'
+        );
+        const outputFile = path.join(path.dirname(file), 'ds.log');
+        if (await fs.pathExists(outputFile)) {
+            await fs.unlink(outputFile);
+        }
+        const textDocument = await openFile(file);
+
+        // Wait for code lenses to get detected.
+        await sleep(1_000);
+
+        await vscode.commands.executeCommand<void>('jupyter.runallcells', textDocument.uri);
+        const checkIfFileHasBeenCreated = () => fs.pathExists(outputFile);
+        await waitForCondition(checkIfFileHasBeenCreated, timeoutForCellToRun, `"${outputFile}" file not created`);
+    }
+
     test('Interactive window should always pick up current active interpreter', async function () {
-        this.timeout(60_000);
+        // this.timeout(60_000);
         // Make an interactive window
-        await vscode.commands.executeCommand<void>('jupyter.createnewinteractive');
-        const provider = api.serviceManager.get<IInteractiveWindowProvider>(IInteractiveWindowProvider);
-        assert.ok(provider.windows.length === 1, 'Unexpected number of interactive windows created');
-        const currentWindow = provider.windows[0];
-        const interpreterForCurrentWindow = currentWindow.notebook?.getMatchingInterpreter();
+        await runCellInInteractiveWindow();
+        assert.ok(provider?.windows?.length === 1, 'Unexpected number of interactive windows created');
+        const currentWindow = provider?.windows[0];
+        console.log(currentWindow, currentWindow);
+        const interpreterForCurrentWindow = currentWindow?.notebook?.getMatchingInterpreter();
         assert.ok(interpreterForCurrentWindow !== undefined, 'Unable to get matching interpreter for current window');
 
         // Now change active interpreter
@@ -56,8 +81,8 @@ suite('Smoke Tests', () => {
         await sleep(1_000); // Wait for the Python extension to respond to the setting change
 
         // Now make another interactive window and confirm it's using the newly selected interpreter
-        await vscode.commands.executeCommand<void>('jupyter.createnewinteractive');
-        assert.ok(provider.windows.length === 2, 'Unexpected number of interactive windows created');
+        currentWindow.dispose();
+        await runCellInInteractiveWindow();
         const newWindow = provider.windows.find((window) => window !== currentWindow);
         const interpreterForNewWindow = newWindow?.notebook?.getMatchingInterpreter();
         assert.ok(interpreterForNewWindow !== undefined, 'Unable to get matching interpreter for current window');
@@ -91,26 +116,7 @@ suite('Smoke Tests', () => {
     });
 
     test('Run Cell in interactive window', async () => {
-        const file = path.join(
-            EXTENSION_ROOT_DIR_FOR_TESTS,
-            'src',
-            'test',
-            'pythonFiles',
-            'datascience',
-            'simple_note_book.py'
-        );
-        const outputFile = path.join(path.dirname(file), 'ds.log');
-        if (await fs.pathExists(outputFile)) {
-            await fs.unlink(outputFile);
-        }
-        const textDocument = await openFile(file);
-
-        // Wait for code lenses to get detected.
-        await sleep(1_000);
-
-        await vscode.commands.executeCommand<void>('jupyter.runallcells', textDocument.uri);
-        const checkIfFileHasBeenCreated = () => fs.pathExists(outputFile);
-        await waitForCondition(checkIfFileHasBeenCreated, timeoutForCellToRun, `"${outputFile}" file not created`);
+        await runCellInInteractiveWindow();
     }).timeout(timeoutForCellToRun);
 
     test('Run Cell in native editor', async () => {
