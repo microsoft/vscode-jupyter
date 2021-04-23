d1e309f15180f113f13a40a9ad85f098d5702967
Don Jayamanne <don.jayamanne@yahoo.com>
2021-04-23 13:00:55 -0700

    More cahnges

--- .eslintrc.js
+++ .eslintrc.js
@@ -914,7 +914,6 @@ module.exports = {
         'src/client/datascience/dataScienceFileSystem.ts',
         'src/client/logging/levels.ts',
         'src/client/logging/transports.ts',
-        'src/client/logging/_global.ts',
         'src/client/logging/util.ts',
         'src/client/logging/index.ts',
         'src/client/logging/formatters.ts',
