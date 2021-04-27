0aef70da9bb1dfe193424b0480be052c9da86ffb
Don Jayamanne <don.jayamanne@yahoo.com>
2021-04-27 15:15:23 -0700

    Misc

--- src/client/logging/logger.ts
+++ src/client/logging/logger.ts
@@ -121,7 +121,7 @@ function getLevelName(level: LogLevel, levels: winston.config.AbstractConfigSetL
         return levelName;
     } else if (isConsole) {
         // XXX Hard-coding this is fragile:
-        return 'silly';
+        return levelName || 'log';
     } else {
         return resolveLevelName(LogLevel.Info, levels) || 'info';
     }
