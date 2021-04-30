5d34e330218e3b23f7a35f5057a537480a1eed17
Don Jayamanne <don.jayamanne@yahoo.com>
2021-04-30 09:02:14 -0700

    Better telemetry for languages

---
+++ src/client/telemetry/languageInitializer.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { injectable } from 'inversify';
+import { languages } from 'vscode';
+import { IExtensionSingleActivationService} from '../activation/types';
+import { KnownNotebookLanguages } from '../datascience/constants';
+
+/**
+ * Initializes the list of known languages with whats registered in VS Code.
+ */
+@injectable()
+export class LanguageInitializer implements IExtensionSingleActivationService {
+    public async activate(){
+        languages.getLanguages().then(languages => {
+            languages.map(language => language.toLowerCase()).forEach(language => {
+                if (!KnownNotebookLanguages.includes(language)){
+                    KnownNotebookLanguages.push(language);
+                }
+            })
+        })
+    }
+}
