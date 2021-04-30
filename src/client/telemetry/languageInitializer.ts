f791a2e5ab6ce92eb7d1eabe693d7e69d18488c1
Don Jayamanne <don.jayamanne@yahoo.com>
2021-04-30 09:28:20 -0700

    Misc

--- src/client/telemetry/languageInitializer.ts
+++ src/client/telemetry/languageInitializer.ts
@@ -3,7 +3,8 @@
 
 import { injectable } from 'inversify';
 import { languages } from 'vscode';
-import { IExtensionSingleActivationService} from '../activation/types';
+import { IExtensionSingleActivationService } from '../activation/types';
+import { noop } from '../common/utils/misc';
 import { KnownNotebookLanguages } from '../datascience/constants';
 
 /**
@@ -11,13 +12,15 @@ import { KnownNotebookLanguages } from '../datascience/constants';
  */
 @injectable()
 export class LanguageInitializer implements IExtensionSingleActivationService {
-    public async activate(){
-        languages.getLanguages().then(languages => {
-            languages.map(language => language.toLowerCase()).forEach(language => {
-                if (!KnownNotebookLanguages.includes(language)){
-                    KnownNotebookLanguages.push(language);
-                }
-            })
-        })
+    public async activate() {
+        languages.getLanguages().then((languages) => {
+            languages
+                .map((language) => language.toLowerCase())
+                .forEach((language) => {
+                    if (!KnownNotebookLanguages.includes(language)) {
+                        KnownNotebookLanguages.push(language);
+                    }
+                });
+        }, noop);
     }
 }
