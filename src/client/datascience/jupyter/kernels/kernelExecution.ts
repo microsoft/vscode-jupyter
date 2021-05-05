19d6a1e691a7f19efb5110ad21230c7f400f17ad
Don Jayamanne <don.jayamanne@yahoo.com>
2021-05-05 16:13:43 -0700

    Changes to cell execution task API

--- src/client/datascience/jupyter/kernels/kernelExecution.ts
+++ src/client/datascience/jupyter/kernels/kernelExecution.ts
@@ -3,7 +3,7 @@
 
 'use strict';
 
-import { notebook, NotebookCell, NotebookCellKind, NotebookDocument } from 'vscode';
+import { notebook, NotebookCell, NotebookCellKind, NotebookController, NotebookDocument } from 'vscode';
 import { ServerStatus } from '../../../../datascience-ui/interactive-common/mainState';
 import { IApplicationShell } from '../../../common/application/types';
 import { traceInfo, traceWarning } from '../../../common/logger';
@@ -35,9 +35,10 @@ export class KernelExecution implements IDisposable {
         readonly metadata: Readonly<KernelConnectionMetadata>,
         context: IExtensionContext,
         private readonly interruptTimeout: number,
-        disposables: IDisposableRegistry
+        disposables: IDisposableRegistry,
+        private readonly controller: NotebookController
     ) {
-        this.executionFactory = new CellExecutionFactory(errorHandler, appShell, context, disposables);
+        this.executionFactory = new CellExecutionFactory(errorHandler, appShell, context, disposables, controller);
     }
 
     @captureTelemetry(Telemetry.ExecuteNativeCell, undefined, true)
@@ -244,7 +245,10 @@ export class KernelExecution implements IDisposable {
     private async getKernel(document: NotebookDocument): Promise<IKernel> {
         let kernel = this.kernelProvider.get(document.uri);
         if (!kernel) {
-            kernel = this.kernelProvider.getOrCreate(document.uri, { metadata: this.metadata });
+            kernel = this.kernelProvider.getOrCreate(document.uri, {
+                metadata: this.metadata,
+                controller: this.controller
+            });
         }
         if (!kernel) {
             throw new Error('Unable to create a Kernel to run cell');
