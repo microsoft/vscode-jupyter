7729f9e731d14cfb6bf99c2218f83bdf608e84ac
Ian Huff <ianhuff@lcs2d-Dell-04.northamerica.corp.microsoft.com>
2021-05-24 10:47:02 -0700

    copy over renderer code

--- src/datascience-ui/ipywidgets/renderer/index.ts
+++ src/datascience-ui/ipywidgets/renderer/index.ts
@@ -2,6 +2,32 @@
 // Licensed under the MIT License.
 
 import './styles.css';
+import { ActivationFunction, CellInfo } from 'vscode-notebook-renderer';
+
+export const activate: ActivationFunction = (_context) => {
+    console.log('Jupyter IPyWidget Renderer activated');
+    return {
+        renderCell(outputId, info: CellInfo) {
+            const renderOutputFunc =
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                (window as any).ipywidgetsKernel?.renderOutput || (global as any).ipywidgetsKernel?.renderOutput;
+            if (renderOutputFunc) {
+                info.element.className = (info.element.className || '') + ' cell-output-ipywidget-background';
+                return renderOutputFunc(outputId, info);
+            }
+            console.error('Rendering widgets on notebook open is not supported.');
+        },
+        destroyCell(outputId) {
+            const disposeOutputFunc =
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                (window as any).ipywidgetsKernel?.disposeOutput || (global as any).ipywidgetsKernel?.disposeOutput;
+            if (disposeOutputFunc) {
+                return disposeOutputFunc(outputId);
+            }
+        }
+    };
+};
+
 //import { NotebookOutputEventParams, NotebookRendererApi } from 'vscode-notebook-renderer';
 
 /* eslint-disable @typescript-eslint/no-explicit-any, no-console */
