14f42ce8fd9cc0b6f1332b7dd2b4f433f0f98fb2
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2021-06-01 15:07:37 -0700

    update func test mock

--- src/test/vscode-mock.ts
+++ src/test/vscode-mock.ts
@@ -64,7 +64,7 @@ export function initialize() {
     generateMock('env');
     generateMock('debug');
     generateMock('scm');
-    generateMock('notebook');
+    generateMock('notebooks');
     generateNotebookMocks();
 
     const commands = new MockCommands();
