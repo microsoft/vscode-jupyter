6f2b31b9d00d678ff420060609baccbe42e35c57
Ian Huff <ianhu@microsoft.com>
2021-06-02 17:04:20 -0700

    6/2 API Changes (#6089)

--- src/test/datascience/notebook/helpers.vscode.test.ts
+++ src/test/datascience/notebook/helpers.vscode.test.ts
@@ -33,23 +33,23 @@ suite('DataScience - VSCode Notebook - helpers', () => {
         const notebook = notebookModelToVSCNotebookData({}, Uri.file(''), cells as any, PYTHON_LANGUAGE, {});
 
         assert.isOk(notebook);
-        assert.deepEqual(notebook.cells, [
-            new NotebookCellData(
-                NotebookCellKind.Code,
-                'print(1)',
-                PYTHON_LANGUAGE,
-                [],
-                {
-                    custom: { metadata: {} }
-                },
-                {
-                    executionOrder: 10
-                }
-            ),
-            new NotebookCellData(NotebookCellKind.Markup, '# HEAD', MARKDOWN_LANGUAGE, [], {
-                custom: { metadata: {} }
-            })
-        ]);
+
+        const codeCellData = new NotebookCellData(NotebookCellKind.Code, 'print(1)', PYTHON_LANGUAGE);
+        codeCellData.outputs = [];
+        codeCellData.metadata = {
+            custom: { metadata: {} }
+        };
+        codeCellData.executionSummary = {
+            executionOrder: 10
+        };
+        const markdownCellData = new NotebookCellData(NotebookCellKind.Markup, '# HEAD', MARKDOWN_LANGUAGE);
+
+        markdownCellData.outputs = [];
+        markdownCellData.metadata = {
+            custom: { metadata: {} }
+        };
+
+        assert.deepEqual(notebook.cells, [codeCellData, markdownCellData]);
     });
     suite('Outputs', () => {
         function validateCellOutputTranslation(
