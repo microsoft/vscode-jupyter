6d7f342e4941358e6971f6c419451066dfdf4ce5
Joyce Er <huer@microsoft.com>
2021-06-08 11:45:57 -0700

    Fix eslint

--- src/client/datascience/commands/notebookCommands.ts
+++ src/client/datascience/commands/notebookCommands.ts
@@ -57,9 +57,9 @@ export class NotebookCommands implements IDisposable {
     }
 
     private renderMarkdownAndSelectBelow() {
-        this.commandManager
+        void this.commandManager
             .executeCommand('notebook.cell.quitEdit')
-            .then(() => void this.commandManager.executeCommand('notebook.cell.executeAndSelectBelow'));
+            .then(() => this.commandManager.executeCommand('notebook.cell.executeAndSelectBelow'));
     }
 
     private collapseAll() {
