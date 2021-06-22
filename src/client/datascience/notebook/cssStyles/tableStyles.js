9336d3b2fb0e3e9e6b2f74c28d2d0b4d8348ec0b
Ian Huff <ianhuff@HelloTest0617.redmond.corp.microsoft.com>
2021-06-22 16:30:06 -0700

    checking to see about making table green

--- src/client/datascience/notebook/cssStyles/tableStyles.js
+++ src/client/datascience/notebook/cssStyles/tableStyles.js
@@ -1,1 +1,6 @@
 console.log("IANHU");
+console.log("**********");
+var tableStyleSheet = document.createElement('style');
+tableStyleSheet.innerHTML = ".output_container table {background-color: green;}";
+document.body.appendChild(tableStyleSheet);
+console.log("**********");
