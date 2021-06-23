31f98b70c5a179c4a6f92a5c437db1244fa68f72
Ian Huff <ianhuff@HelloTest0617.redmond.corp.microsoft.com>
2021-06-23 06:43:01 -0700

    remove comments

--- src/client/datascience/notebook/cssStyles/tableStyles.js
+++ src/client/datascience/notebook/cssStyles/tableStyles.js
@@ -1,6 +1,3 @@
-console.log("IANHU");
-console.log("**********");
 var tableStyleSheet = document.createElement('style');
 tableStyleSheet.innerHTML = "#container > div > div:not(.preview) > div {overflow-x: overlay;}";
 document.body.appendChild(tableStyleSheet);
-console.log("**********");
