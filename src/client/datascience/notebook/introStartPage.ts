c0dfb163f8820fade02dfc1a48be9671bdb30dfd
Don Jayamanne <don.jayamanne@yahoo.com>
2021-07-07 13:53:46 -0700

    Fixed

--- src/client/datascience/notebook/introStartPage.ts
+++ src/client/datascience/notebook/introStartPage.ts
@@ -39,7 +39,7 @@ export class IntroduceNativeNotebookStartPage implements IExtensionSingleActivat
         }
         this.appShell
             .showInformationMessage(
-                'The notebook interface has been revamped. To learn more about this improved experience, click [here](https://github.com/microsoft/vscode-jupyter/wiki/Introducing-Native-Notebooks)'
+                'The notebook interface has been revamped. To learn more about this improved experience, click [here](https://github.com/microsoft/vscode-jupyter/wiki/Native-Notebook-Support-in-VS-Code)'
             )
             .then(noop, noop);
     }
