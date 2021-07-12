d4c1a5dd96aeab0291e6c10a3bd647e0ea9c2a5c
Don Jayamanne <don.jayamanne@yahoo.com>
2021-07-12 12:31:37 -0700

    Refactor preferred controller for interactive

--- src/client/datascience/notebook/types.ts
+++ src/client/datascience/notebook/types.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { Event, NotebookDocument, NotebookEditor, Uri } from 'vscode';
-import { PythonEnvironment } from '../../pythonEnvironments/info';
 import { VSCodeNotebookController } from './vscodeNotebookController';
 
 export const INotebookKernelResolver = Symbol('INotebookKernelResolver');
@@ -13,10 +12,7 @@ export interface INotebookControllerManager {
     getSelectedNotebookController(document: NotebookDocument): VSCodeNotebookController | undefined;
     // Marked test only, just for tests to access registered controllers
     registeredNotebookControllers(): VSCodeNotebookController[];
-    getOrCreateController(
-        pythonInterpreter: PythonEnvironment,
-        notebookType: 'interactive' | 'jupyter-notebook'
-    ): VSCodeNotebookController | undefined;
+    getInteractiveController(): Promise<VSCodeNotebookController | undefined>;
 }
 export enum CellOutputMimeTypes {
     error = 'application/vnd.code.notebook.error',
