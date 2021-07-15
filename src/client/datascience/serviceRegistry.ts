887a3e09c857df4e54a7a347d86485b3907dbae8
Joyce Er <huer@microsoft.com>
2021-07-15 13:00:14 -0700

    Leave users with a way to opt out

--- src/client/datascience/serviceRegistry.ts
+++ src/client/datascience/serviceRegistry.ts
@@ -297,14 +297,10 @@ export function registerTypes(serviceManager: IServiceManager, inNotebookApiExpe
     serviceManager.addSingleton<IExtensionSingleActivationService>(IExtensionSingleActivationService, VariableViewActivationService);
     serviceManager.addSingleton<IInteractiveWindowListener>(IInteractiveWindowListener, DataScienceSurveyBannerLogger);
     const configuration = workspace.getConfiguration();
-    const insidersChannel = configuration.get<string>('python.insidersChannel');
     if (
         configuration.get<boolean>('jupyter.experiments.enabled') === true &&
         !configuration.get<string[]>('jupyter.experiments.optOutFrom')?.includes('All') &&
-        // If in Daily or Weekly Insiders channel and in VS Code Insiders, opt in by default
-        (((insidersChannel === 'daily' || insidersChannel === 'weekly') && isVSCInsiders) ||
-            // If user explicitly asked to be in the experiment, also opt in
-            configuration.get<boolean>('jupyter.enableNativeInteractiveWindow') === true)
+        configuration.get<boolean>('jupyter.enableNativeInteractiveWindow') === true
     ) {
         serviceManager.addSingleton<IInteractiveWindowProvider>(IInteractiveWindowProvider, NativeInteractiveWindowProvider);
         serviceManager.addSingleton<IDataScienceCommandListener>(IDataScienceCommandListener, NativeInteractiveWindowCommandListener);
