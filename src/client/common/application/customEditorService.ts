dc6ea101b04b635151c989793130d66f03ad36b5
Joyce Er <joyce.er@microsoft.com>
2021-07-22 13:26:03 -0700

    Remove reading/writing `workbench.editorAssociations`

--- src/client/common/application/customEditorService.ts
+++ src/client/common/application/customEditorService.ts
@@ -4,15 +4,10 @@
 import { inject, injectable } from 'inversify';
 import * as vscode from 'vscode';
 import { IExtensionSingleActivationService } from '../../activation/types';
-import { OurNotebookProvider, Telemetry } from '../../datascience/constants';
-import {
-    NewEditorAssociationSetting,
-    ensureUpdatedEditorAssociationSettingFormat
-} from '../../datascience/notebook/integration';
-import { sendTelemetryEvent } from '../../telemetry';
+import { OurNotebookProvider } from '../../datascience/constants';
 
 import { UseCustomEditorApi } from '../constants';
-import { CustomEditorProvider, ICommandManager, ICustomEditorService, IWorkspaceService } from './types';
+import { CustomEditorProvider, ICommandManager, ICustomEditorService } from './types';
 
 export const ViewType = 'jupyter.notebook.ipynb';
 
@@ -20,10 +15,9 @@ export const ViewType = 'jupyter.notebook.ipynb';
 export class CustomEditorService implements ICustomEditorService, IExtensionSingleActivationService {
     constructor(
         @inject(UseCustomEditorApi) private readonly useCustomEditorApi: boolean,
-        @inject(IWorkspaceService) private readonly workspace: IWorkspaceService,
         @inject(OurNotebookProvider) private readonly editorProvider: CustomEditorProvider,
         @inject(ICommandManager) private commandManager: ICommandManager
-    ) {}
+    ) { }
 
     public async openEditor(file: vscode.Uri, viewType: string): Promise<void> {
         // This is necessary to abstract the open for functional tests.
@@ -33,34 +27,7 @@ export class CustomEditorService implements ICustomEditorService, IExtensionSing
     }
 
     public async activate() {
-        let updateType: 'added' | 'removed' | undefined;
-        try {
-            const settings = this.workspace.getConfiguration('workbench', undefined);
-            const editorAssociations = settings.get('editorAssociations'); // At this point we don't know if this is the old or new format
-            const updatedSettings = ensureUpdatedEditorAssociationSettingFormat(
-                editorAssociations
-            ) as NewEditorAssociationSetting;
-
-            // Update the settings.
-            if (this.useCustomEditorApi && updatedSettings['*.ipynb'] !== ViewType) {
-                updatedSettings['*.ipynb'] = ViewType;
-                updateType = 'added';
-                await settings.update('editorAssociations', updatedSettings, vscode.ConfigurationTarget.Global);
-                sendTelemetryEvent(Telemetry.UpdateCustomEditorAssociation, undefined, { type: updateType });
-            }
-
-            // Revert the settings.
-            if (!this.useCustomEditorApi && updatedSettings['*.ipynb'] === ViewType) {
-                updatedSettings['*ipynb'] = undefined;
-                updateType = 'removed';
-                await settings.update('editorAssociations', updatedSettings, vscode.ConfigurationTarget.Global);
-                sendTelemetryEvent(Telemetry.UpdateCustomEditorAssociation, undefined, { type: updateType });
-            }
-        } catch (ex) {
-            sendTelemetryEvent(Telemetry.UpdateCustomEditorAssociation, undefined, { type: updateType! }, ex, true);
-        }
-
-        // Always register our custom editor provider even if not in the experiment. This allows us
+        // Always register our custom editor provider. This allows us
         // to handle custom editor requests even when not the default
         vscode.window.registerCustomEditorProvider('jupyter.notebook.ipynb', this.editorProvider, {
             webviewOptions: {
