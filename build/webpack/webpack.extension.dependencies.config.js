fe735ea4614919c457c6882e8438dfefb7d389ee
Don Jayamanne <don.jayamanne@yahoo.com>
2021-08-04 13:54:54 -0700

    More items to remove

--- build/webpack/webpack.extension.dependencies.config.js
+++ build/webpack/webpack.extension.dependencies.config.js
@@ -37,12 +37,7 @@ const config = {
         ]
     },
     externals: ['vscode', 'commonjs'],
-    plugins: [
-        ...common.getDefaultPlugins('dependencies'),
-        // vsls requires our package.json to be next to node_modules. It's how they
-        // 'find' the calling extension.
-        new copyWebpackPlugin({ patterns: [{ from: './package.json', to: '.' }] })
-    ],
+    plugins: [...common.getDefaultPlugins('dependencies')],
     resolve: {
         alias: {
             // Pointing pdfkit to a dummy js file so webpack doesn't fall over.
--- gulpfile.js
+++ gulpfile.js
@@ -234,7 +234,6 @@ function getAllowedWarningsForWebPack(buildConfig) {
                 'WARNING in asset size limit: The following asset(s) exceed the recommended size limit (244 KiB).',
                 'WARNING in entrypoint size limit: The following entrypoint(s) combined asset size exceeds the recommended limit (244 KiB). This can impact web performance.',
                 'WARNING in webpack performance recommendations:',
-                'WARNING in ./node_modules/vsls/vscode.js',
                 'WARNING in ./node_modules/encoding/lib/iconv-loader.js',
                 'WARNING in ./node_modules/keyv/src/index.js',
                 'ERROR in ./node_modules/got/index.js',
