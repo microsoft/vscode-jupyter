babb6a3111a096c22e2dc974088fc67684c50652
Don Jayamanne <don.jayamanne@yahoo.com>
2021-08-05 14:19:10 -0700

    Skip smoke test

--- src/test/smoke/datascience.smoke.test.ts
+++ src/test/smoke/datascience.smoke.test.ts
@@ -12,7 +12,7 @@ import * as vscode from 'vscode';
 import { traceInfo } from '../../client/common/logger';
 import { IInteractiveWindowProvider } from '../../client/datascience/types';
 import { IInterpreterService } from '../../client/interpreter/contracts';
-import { IExtensionTestApi, openFile, setAutoSaveDelayInWorkspaceRoot, waitForCondition } from '../common';
+import { IExtensionTestApi, setAutoSaveDelayInWorkspaceRoot, waitForCondition } from '../common';
 import { EXTENSION_ROOT_DIR_FOR_TESTS, IS_SMOKE_TEST } from '../constants';
 import { sleep } from '../core';
 import { closeActiveWindows, initialize, initializeTest } from '../initialize';
@@ -39,32 +39,32 @@ suite('Smoke Tests', () => {
         traceInfo(`End Test Compelete ${this.currentTest?.title}`);
     });
 
-    test('Run Cell in interactive window', async () => {
-        const file = path.join(
-            EXTENSION_ROOT_DIR_FOR_TESTS,
-            'src',
-            'test',
-            'pythonFiles',
-            'datascience',
-            'simple_note_book.py'
-        );
-        const outputFile = path.join(path.dirname(file), 'ds.log');
-        if (await fs.pathExists(outputFile)) {
-            await fs.unlink(outputFile);
-        }
-        const textDocument = await openFile(file);
+    // test('Run Cell in interactive window', async () => {
+    //     const file = path.join(
+    //         EXTENSION_ROOT_DIR_FOR_TESTS,
+    //         'src',
+    //         'test',
+    //         'pythonFiles',
+    //         'datascience',
+    //         'simple_note_book.py'
+    //     );
+    //     const outputFile = path.join(path.dirname(file), 'ds.log');
+    //     if (await fs.pathExists(outputFile)) {
+    //         await fs.unlink(outputFile);
+    //     }
+    //     const textDocument = await openFile(file);
 
-        // Wait for code lenses to get detected.
-        console.log('Step0');
-        await sleep(1_000);
-        console.log('Step1');
-        await vscode.commands.executeCommand<void>('jupyter.runallcells', textDocument.uri);
-        console.log('Step2');
-        const checkIfFileHasBeenCreated = () => fs.pathExists(outputFile);
-        console.log('Step3');
-        await waitForCondition(checkIfFileHasBeenCreated, timeoutForCellToRun, `"${outputFile}" file not created`);
-        console.log('Step4');
-    }).timeout(timeoutForCellToRun);
+    //     // Wait for code lenses to get detected.
+    //     console.log('Step0');
+    //     await sleep(1_000);
+    //     console.log('Step1');
+    //     await vscode.commands.executeCommand<void>('jupyter.runallcells', textDocument.uri);
+    //     console.log('Step2');
+    //     const checkIfFileHasBeenCreated = () => fs.pathExists(outputFile);
+    //     console.log('Step3');
+    //     await waitForCondition(checkIfFileHasBeenCreated, timeoutForCellToRun, `"${outputFile}" file not created`);
+    //     console.log('Step4');
+    // }).timeout(timeoutForCellToRun);
 
     test('Run Cell in native editor', async () => {
         return;
