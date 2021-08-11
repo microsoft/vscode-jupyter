18be257f7fd01e074c516964aaf7aa5681000646
Joyce Er <joyce.er@microsoft.com>
2021-08-11 13:47:14 -0700

    Fix smoke test

--- src/client/datascience/notebook/vscodeNotebookController.ts
+++ src/client/datascience/notebook/vscodeNotebookController.ts
@@ -414,7 +414,7 @@ export class VSCodeNotebookController implements Disposable {
     private async setAsActiveControllerForTests(notebook: NotebookDocument) {
         // Only when running tests should we force the selection of the kernel.
         // Else the general VS Code behavior is for the user to select a kernel (here we make it look as though use selected it).
-        if (this.context.extensionMode !== ExtensionMode.Test) {
+        if (this.context.extensionMode !== ExtensionMode.Test || notebook.notebookType === InteractiveWindowView) {
             return;
         }
         traceInfoIf(isCI, `Command notebook.selectKernel executing for ${notebook.uri.toString()} ${this.id}`);
