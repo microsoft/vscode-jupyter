3aa51df81793c2e45c30b5ec8324f16f26d4db4f
Joyce Er <joyce.er@microsoft.com>
2021-08-25 22:02:37 -0700

    Fix sourcemaps for interactive window debugging (#7298)

--- src/client/datascience/interactive-window/nativeInteractiveWindow.ts
+++ src/client/datascience/interactive-window/nativeInteractiveWindow.ts
@@ -641,7 +641,8 @@ export class NativeInteractiveWindow implements IInteractiveWindowLoadable {
             interactiveWindowCellMarker,
             interactive: {
                 file: file.fsPath,
-                line: line
+                line: line,
+                originalSource: code
             },
             executionId: id
         };
