fa24c05774dc083b40b4ad0585593013288e0261
Don Jayamanne <don.jayamanne@yahoo.com>
2021-09-03 08:30:31 -0700

    Disable interacgive window

--- src/test/datascience/interactiveWindow.vscode.test.ts
+++ src/test/datascience/interactiveWindow.vscode.test.ts
@@ -22,6 +22,7 @@ suite('Interactive window', async () => {
         if (IS_REMOTE_NATIVE_TEST) {
             return this.skip();
         }
+        return this.skip();
         api = await initialize();
         interactiveWindowProvider = api.serviceManager.get(IInteractiveWindowProvider);
     });
