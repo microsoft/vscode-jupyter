3429ffca26c8bb13a7c02167e9f23abe09894148
Don Jayamanne <don.jayamanne@yahoo.com>
2021-09-07 13:19:15 -0700

    Fixes

--- src/test/common.ts
+++ src/test/common.ts
@@ -142,7 +142,7 @@ async function setGlobalPathToInterpreter(pythonPath?: string): Promise<void> {
 export async function disableExperimentsInPythonExtension(): Promise<void> {
     const vscode = require('vscode') as typeof import('vscode');
     const pythonConfig = vscode.workspace.getConfiguration('python', (null as any) as Uri);
-    await pythonConfig.update('experiments.enabled', false, vscode.ConfigurationTarget.Global);
+    await pythonConfig.update('experiments.enabled', false, vscode.ConfigurationTarget.Global).then(noop, noop);
 }
 export const resetGlobalPythonPathSetting = async () => retryAsync(restoreGlobalPythonPathSetting)();
 
