334caf97bd2c70c9b01f598582781e0e188d2f31
Don Jayamanne <don.jayamanne@yahoo.com>
2021-09-08 12:56:46 -0700

    Fixes

--- src/client/datascience/jupyter/pythonVariableRequester.ts
+++ src/client/datascience/jupyter/pythonVariableRequester.ts
@@ -1,7 +1,6 @@
 // import { nbformat } from '@jupyterlab/coreutils';
 import { nbformat } from '@jupyterlab/coreutils';
 import { inject, injectable } from 'inversify';
-import * as uuid from 'uuid/v4';
 import * as path from 'path';
 import stripAnsi from 'strip-ansi';
 import { CancellationToken } from 'vscode';
@@ -9,9 +8,10 @@ import { traceError } from '../../common/logger';
 import { IFileSystem } from '../../common/platform/types';
 import { IDisposable } from '../../common/types';
 import * as localize from '../../common/utils/localize';
-import { DataFrameLoading, GetVariableInfo, Identifiers } from '../constants';
-import { ICell, IJupyterVariable, IKernelVariableRequester, INotebook } from '../types';
+import { DataFrameLoading, GetVariableInfo } from '../constants';
+import { IJupyterVariable, IKernelVariableRequester, INotebook } from '../types';
 import { JupyterDataRateLimitError } from './jupyterDataRateLimitError';
+import { executeSilently } from './kernels/kernel';
 
 @injectable()
 export class PythonVariablesRequester implements IKernelVariableRequester {
@@ -29,13 +29,9 @@ export class PythonVariablesRequester implements IKernelVariableRequester {
         await this.importDataFrameScripts(notebook);
 
         // Then execute a call to get the info and turn it into JSON
-        const results = await notebook.execute(
-            `print(${DataFrameLoading.DataFrameInfoFunc}(${expression}))`,
-            Identifiers.EmptyFileName,
-            0,
-            uuid(),
-            undefined,
-            true
+        const results = await executeSilently(
+            notebook.session,
+            `print(${DataFrameLoading.DataFrameInfoFunc}(${expression}))`
         );
 
         const fileName = path.basename(notebook.identity.path);
@@ -52,27 +48,24 @@ export class PythonVariablesRequester implements IKernelVariableRequester {
         await this.importDataFrameScripts(notebook);
 
         // Then execute a call to get the rows and turn it into JSON
-        const results = await notebook.execute(
-            `print(${DataFrameLoading.DataFrameRowFunc}(${expression}, ${start}, ${end}))`,
-            Identifiers.EmptyFileName,
-            0,
-            uuid(),
-            undefined,
-            true
+        const results = await executeSilently(
+            notebook.session,
+            `print(${DataFrameLoading.DataFrameRowFunc}(${expression}, ${start}, ${end}))`
         );
+
         return this.deserializeJupyterResult(results);
     }
 
     public async getVariableProperties(
         word: string,
         notebook: INotebook,
-        cancelToken: CancellationToken | undefined,
+        _cancelToken: CancellationToken | undefined,
         matchingVariable: IJupyterVariable | undefined,
         languageSettings: { [typeNameKey: string]: string[] },
         inEnhancedTooltipsExperiment: boolean
     ): Promise<{ [attributeName: string]: string }> {
         // Import the variable info script directory if we haven't already
-        await this.importGetVariableInfoScripts(notebook, cancelToken);
+        await this.importGetVariableInfoScripts(notebook);
 
         let result: { [attributeName: string]: string } = {};
         if (matchingVariable && matchingVariable.value) {
@@ -81,13 +74,9 @@ export class PythonVariablesRequester implements IKernelVariableRequester {
                 const attributeNames = languageSettings[type];
                 const stringifiedAttributeNameList =
                     '[' + attributeNames.reduce((accumulator, currVal) => accumulator + `"${currVal}", `, '') + ']';
-                const attributes = await notebook.execute(
-                    `print(${GetVariableInfo.VariablePropertiesFunc}(${matchingVariable.name}, ${stringifiedAttributeNameList}))`,
-                    Identifiers.EmptyFileName,
-                    0,
-                    uuid(),
-                    cancelToken,
-                    true
+                const attributes = await executeSilently(
+                    notebook.session,
+                    `print(${GetVariableInfo.VariablePropertiesFunc}(${matchingVariable.name}, ${stringifiedAttributeNameList}))`
                 );
                 result = { ...result, ...this.deserializeJupyterResult(attributes) };
             } else {
@@ -99,20 +88,16 @@ export class PythonVariablesRequester implements IKernelVariableRequester {
 
     public async getVariableNamesAndTypesFromKernel(
         notebook: INotebook,
-        token?: CancellationToken
+        _token?: CancellationToken
     ): Promise<IJupyterVariable[]> {
         if (notebook) {
             // Add in our get variable info script to get types
-            await this.importGetVariableInfoScripts(notebook, token);
+            await this.importGetVariableInfoScripts(notebook);
 
             // VariableTypesFunc takes in list of vars and the corresponding var names
-            const results = await notebook.execute(
-                `_rwho_ls = %who_ls\nprint(${GetVariableInfo.VariableTypesFunc}(_rwho_ls))`,
-                Identifiers.EmptyFileName,
-                0,
-                uuid(),
-                token,
-                true
+            const results = await executeSilently(
+                notebook.session,
+                `_rwho_ls = %who_ls\nprint(${GetVariableInfo.VariableTypesFunc}(_rwho_ls))`
             );
 
             const varNameTypeMap = this.deserializeJupyterResult(results) as Map<String, String>;
@@ -140,19 +125,15 @@ export class PythonVariablesRequester implements IKernelVariableRequester {
     public async getFullVariable(
         targetVariable: IJupyterVariable,
         notebook: INotebook,
-        token?: CancellationToken
+        _token?: CancellationToken
     ): Promise<IJupyterVariable> {
         // Import the variable info script directory if we haven't already
-        await this.importGetVariableInfoScripts(notebook, token);
+        await this.importGetVariableInfoScripts(notebook);
 
         // Then execute a call to get the info and turn it into JSON
-        const results = await notebook.execute(
-            `print(${GetVariableInfo.VariableInfoFunc}(${targetVariable.name}))`,
-            Identifiers.EmptyFileName,
-            0,
-            uuid(),
-            token,
-            true
+        const results = await executeSilently(
+            notebook.session,
+            `print(${GetVariableInfo.VariableInfoFunc}(${targetVariable.name}))`
         );
 
         // Combine with the original result (the call only returns the new fields)
@@ -162,7 +143,7 @@ export class PythonVariablesRequester implements IKernelVariableRequester {
         };
     }
 
-    private async importDataFrameScripts(notebook: INotebook, token?: CancellationToken): Promise<void> {
+    private async importDataFrameScripts(notebook: INotebook): Promise<void> {
         const key = notebook.identity.toString();
         if (!this.importedDataFrameScripts.get(key)) {
             // Clear our flag if the notebook disposes or restarts
@@ -176,13 +157,13 @@ export class PythonVariablesRequester implements IKernelVariableRequester {
             disposables.push(notebook.onKernelRestarted(handler));
 
             // First put the code from our helper files into the notebook
-            await this.runScriptFile(notebook, DataFrameLoading.ScriptPath, token);
+            await this.runScriptFile(notebook, DataFrameLoading.ScriptPath);
 
             this.importedDataFrameScripts.set(notebook.identity.toString(), true);
         }
     }
 
-    private async importGetVariableInfoScripts(notebook: INotebook, token?: CancellationToken): Promise<void> {
+    private async importGetVariableInfoScripts(notebook: INotebook): Promise<void> {
         const key = notebook.identity.toString();
         if (!this.importedGetVariableInfoScripts.get(key)) {
             // Clear our flag if the notebook disposes or restarts
@@ -195,69 +176,62 @@ export class PythonVariablesRequester implements IKernelVariableRequester {
             disposables.push(notebook.onKernelChanged(handler));
             disposables.push(notebook.onKernelRestarted(handler));
 
-            await this.runScriptFile(notebook, GetVariableInfo.ScriptPath, token);
+            await this.runScriptFile(notebook, GetVariableInfo.ScriptPath);
 
             this.importedGetVariableInfoScripts.set(notebook.identity.toString(), true);
         }
     }
 
     // Read in a .py file and execute it silently in the given notebook
-    private async runScriptFile(notebook: INotebook, scriptFile: string, token?: CancellationToken) {
+    private async runScriptFile(notebook: INotebook, scriptFile: string) {
         if (await this.fs.localFileExists(scriptFile)) {
             const fileContents = await this.fs.readLocalFile(scriptFile);
-            return notebook.execute(fileContents, Identifiers.EmptyFileName, 0, uuid(), token, true);
+            return executeSilently(notebook.session, fileContents);
         } else {
             traceError('Cannot run non-existant script file');
         }
     }
 
-    private extractJupyterResultText(cells: ICell[]): string {
+    private extractJupyterResultText(outputs: nbformat.IOutput[]): string {
         // Verify that we have the correct cell type and outputs
-        if (cells.length > 0 && cells[0].data) {
-            const codeCell = cells[0].data as nbformat.ICodeCell;
-            if (codeCell.outputs.length > 0) {
-                const codeCellOutput = codeCell.outputs[0] as nbformat.IOutput;
-                if (
-                    codeCellOutput &&
-                    codeCellOutput.output_type === 'stream' &&
-                    codeCellOutput.name === 'stderr' &&
-                    codeCellOutput.hasOwnProperty('text')
-                ) {
-                    const resultString = codeCellOutput.text as string;
-                    // See if this the IOPUB data rate limit problem
-                    if (resultString.includes('iopub_data_rate_limit')) {
-                        throw new JupyterDataRateLimitError();
-                    } else {
-                        const error = localize.DataScience.jupyterGetVariablesExecutionError().format(resultString);
-                        traceError(error);
-                        throw new Error(error);
-                    }
-                }
-                if (codeCellOutput && codeCellOutput.output_type === 'execute_result') {
-                    const data = codeCellOutput.data;
-                    if (data && data.hasOwnProperty('text/plain')) {
-                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                        return (data as any)['text/plain'];
-                    }
-                }
-                if (
-                    codeCellOutput &&
-                    codeCellOutput.output_type === 'stream' &&
-                    codeCellOutput.hasOwnProperty('text')
-                ) {
-                    return codeCellOutput.text as string;
-                }
-                if (
-                    codeCellOutput &&
-                    codeCellOutput.output_type === 'error' &&
-                    codeCellOutput.hasOwnProperty('traceback')
-                ) {
-                    const traceback: string[] = codeCellOutput.traceback as string[];
-                    const stripped = traceback.map(stripAnsi).join('\r\n');
-                    const error = localize.DataScience.jupyterGetVariablesExecutionError().format(stripped);
+        if (outputs.length > 0) {
+            const codeCellOutput = outputs[0] as nbformat.IOutput;
+            if (
+                codeCellOutput &&
+                codeCellOutput.output_type === 'stream' &&
+                codeCellOutput.name === 'stderr' &&
+                codeCellOutput.hasOwnProperty('text')
+            ) {
+                const resultString = codeCellOutput.text as string;
+                // See if this the IOPUB data rate limit problem
+                if (resultString.includes('iopub_data_rate_limit')) {
+                    throw new JupyterDataRateLimitError();
+                } else {
+                    const error = localize.DataScience.jupyterGetVariablesExecutionError().format(resultString);
                     traceError(error);
                     throw new Error(error);
                 }
+            }
+            if (codeCellOutput && codeCellOutput.output_type === 'execute_result') {
+                const data = codeCellOutput.data;
+                if (data && data.hasOwnProperty('text/plain')) {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    return (data as any)['text/plain'];
+                }
+            }
+            if (codeCellOutput && codeCellOutput.output_type === 'stream' && codeCellOutput.hasOwnProperty('text')) {
+                return codeCellOutput.text as string;
+            }
+            if (
+                codeCellOutput &&
+                codeCellOutput.output_type === 'error' &&
+                codeCellOutput.hasOwnProperty('traceback')
+            ) {
+                const traceback: string[] = codeCellOutput.traceback as string[];
+                const stripped = traceback.map(stripAnsi).join('\r\n');
+                const error = localize.DataScience.jupyterGetVariablesExecutionError().format(stripped);
+                traceError(error);
+                throw new Error(error);
             }
         }
 
@@ -265,8 +239,8 @@ export class PythonVariablesRequester implements IKernelVariableRequester {
     }
 
     // Pull our text result out of the Jupyter cell
-    private deserializeJupyterResult<T>(cells: ICell[]): T {
-        const text = this.extractJupyterResultText(cells);
+    private deserializeJupyterResult<T>(outputs: nbformat.IOutput[]): T {
+        const text = this.extractJupyterResultText(outputs);
         return JSON.parse(text) as T;
     }
 }
