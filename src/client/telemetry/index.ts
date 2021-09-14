b6c90caae2c6b25bd6e4c6b1056ad4e63992593e
Don Jayamanne <don.jayamanne@yahoo.com>
2021-09-14 12:16:08 -0700

    Update comments

--- src/client/telemetry/index.ts
+++ src/client/telemetry/index.ts
@@ -811,7 +811,7 @@ export interface IEventNamePropertyMapping {
             | 'failed' // Installation disabled (this is what python extension returns).
             | 'install' // User chose install from prompt.
             | 'donotinstall' // User chose not to install from prompt.
-            | 'differentKernel' // User chose not to select a different kernel/interpreter.
+            | 'differentKernel' // User chose to select a different kernel.
             | 'dismissed'; // User chose to dismiss the prompt.
     };
     /**
