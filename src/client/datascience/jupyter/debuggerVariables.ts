2dead329935086a160695d361945673842fa48ef
David Kutugata <dakutuga@microsoft.com>
2021-09-14 16:37:22 -0700

    oops

--- src/client/datascience/jupyter/debuggerVariables.ts
+++ src/client/datascience/jupyter/debuggerVariables.ts
@@ -54,9 +54,7 @@ export class DebuggerVariables extends DebugLocationTracker
         @inject(IVSCodeNotebook) private readonly vscNotebook: IVSCodeNotebook
     ) {
         super(undefined);
-        this.debuggingManager.doneDebugging(() => {
-            this.refreshEventEmitter.fire();
-        }, this);
+        this.debuggingManager.doneDebugging(() => this.refreshEventEmitter.fire(), this);
     }
 
     public get refreshRequired(): Event<void> {
