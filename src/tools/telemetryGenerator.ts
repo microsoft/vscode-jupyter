78c3849cc526bdfd5cf8ecb8808df4019176742b
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-10-01 16:56:32 -0700

    Print out references better

--- src/tools/telemetryGenerator.ts
+++ src/tools/telemetryGenerator.ts
@@ -1,18 +1,84 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as ts from 'typescript';
+import * as fs from 'fs-extra';
 import * as glob from 'glob';
+
+/**
+ * A TypeScript language service host
+ */
+class TypeScriptLanguageServiceHost implements ts.LanguageServiceHost {
+    private readonly _files: string[];
+    private readonly _compilerOptions: ts.CompilerOptions;
+
+    constructor(files: string[], compilerOptions: ts.CompilerOptions) {
+        this._files = files;
+        this._compilerOptions = compilerOptions;
+    }
+
+    // --- language service host ---------------
+
+    getCompilationSettings(): ts.CompilerOptions {
+        return this._compilerOptions;
+    }
+    getScriptFileNames(): string[] {
+        return this._files;
+    }
+    getScriptVersion(_fileName: string): string {
+        return '1';
+    }
+    getProjectVersion(): string {
+        return '1';
+    }
+    getScriptSnapshot(fileName: string): ts.IScriptSnapshot {
+        if (this._files.includes(fileName)) {
+            return ts.ScriptSnapshot.fromString(fs.readFileSync(fileName).toString());
+        } else {
+            return ts.ScriptSnapshot.fromString('');
+        }
+    }
+    getScriptKind(_fileName: string): ts.ScriptKind {
+        return ts.ScriptKind.TS;
+    }
+    getCurrentDirectory(): string {
+        return '';
+    }
+    getDefaultLibFileName(_options: ts.CompilerOptions): string {
+        return 'defaultLib:lib.d.ts';
+    }
+    isDefaultLibFileName(fileName: string): boolean {
+        return fileName === this.getDefaultLibFileName(this._compilerOptions);
+    }
+}
+
+function findNode(sourceFile: ts.SourceFile, position: number): ts.Node | undefined {
+    let found: ts.Node | undefined;
+    sourceFile.forEachChild(visit);
+    function visit(node: ts.Node) {
+        if (node.pos === position) {
+            found = node;
+            return;
+        } else if (node.pos > position) {
+            return;
+        }
+        ts.forEachChild(node, visit);
+    }
+    return found;
+}
 
 /** Generate documentation for all classes in a set of .ts files */
 function generateDocumentation(fileNames: string[], options: ts.CompilerOptions): void {
-    // Build a program using the set of root file names in fileNames
-    let program = ts.createProgram(fileNames, options);
+    let host = new TypeScriptLanguageServiceHost(fileNames, options);
+    let languageService = ts.createLanguageService(host, undefined, ts.LanguageServiceMode.Semantic);
+    let program = languageService.getProgram();
 
     // Visit every sourceFile in the program
-    for (const sourceFile of program.getSourceFiles()) {
-        if (!sourceFile.isDeclarationFile) {
-            // Walk the tree to search for classes
-            ts.forEachChild(sourceFile, visit.bind(undefined, sourceFile));
+    if (program) {
+        for (const sourceFile of program.getSourceFiles()) {
+            if (!sourceFile.isDeclarationFile) {
+                // Walk the tree to search for classes
+                ts.forEachChild(sourceFile, visit.bind(undefined, sourceFile));
+            }
         }
     }
 
@@ -32,6 +98,31 @@ function generateDocumentation(fileNames: string[], options: ts.CompilerOptions)
                 // This is a telemetry enum. Print out members
                 node.members.forEach((m) => {
                     console.log(`   ${m.getText(sourceFile)}`);
+
+                    // Find all references for this enum
+                    const references = languageService.getReferencesAtPosition(
+                        m.getSourceFile().fileName,
+                        m.name.pos + m.name.getLeadingTriviaWidth()
+                    );
+                    if (references) {
+                        console.log(`    References:`);
+                        references.forEach((r) => {
+                            if (!r.isDefinition) {
+                                const refSourceFile = program?.getSourceFile(r.fileName);
+                                if (refSourceFile) {
+                                    const refNode = findNode(refSourceFile, r.textSpan.start);
+                                    const endRefLine = refSourceFile.getLineEndOfPosition(refNode?.pos || 0);
+                                    const grandParent = refNode?.parent?.parent;
+                                    const snapshot = host.getScriptSnapshot(`./${r.fileName}`);
+                                    console.log(
+                                        `        ${r.fileName} =>${snapshot.getText(grandParent?.pos || 0, endRefLine)}`
+                                    );
+                                } else {
+                                    console.log(`        ${r.fileName} => ${JSON.stringify(r.textSpan)}`);
+                                }
+                            }
+                        });
+                    }
                 });
             }
         } else if (ts.isModuleDeclaration(node)) {
