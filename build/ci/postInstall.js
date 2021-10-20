2a2f9d4165acfb1032284dc665efba02697413ed
Don Jayamanne <don.jayamanne@outlook.com>
2021-10-20 15:46:00 -0700

    Fixes to compiler issues resulting from types in @types/jsdom/default.d.ts (#7987)

--- build/ci/postInstall.js
+++ build/ci/postInstall.js
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 'use strict';
 
+const { EOL } = require('os');
 const colors = require('colors/safe');
 const fs = require('fs-extra');
 const path = require('path');
@@ -34,6 +35,34 @@ function createJupyterKernelWithoutSerialization() {
     var destPath = path.join(path.dirname(filePath), 'nonSerializingKernel.js');
     fs.writeFileSync(destPath, replacedContents);
     console.log(colors.green(destPath + ' file generated (by Jupyter VSC)'));
+}
+
+/**
+ * Fix compilation issues in jsdom files.
+ */
+function updateJSDomTypeDefinition() {
+    var relativePath = path.join('node_modules', '@types', 'jsdom', 'base.d.ts');
+    var filePath = path.join(constants.ExtensionRootDir, relativePath);
+    if (!fs.existsSync(filePath)) {
+        console.warn("JSdom base.d.ts not found '" + filePath + "' (Jupyter Extension post install script)");
+        return;
+    }
+    var fileContents = fs.readFileSync(filePath, { encoding: 'utf8' });
+    var replacedContents = fileContents.replace(
+        /\s*globalThis: DOMWindow;\s*readonly \["Infinity"]: number;\s*readonly \["NaN"]: number;/g,
+        [
+            'globalThis: DOMWindow;',
+            '// @ts-ignore',
+            'readonly ["Infinity"]: number;',
+            '// @ts-ignore',
+            'readonly ["NaN"]: number;'
+        ].join(`${EOL}        `)
+    );
+    if (replacedContents === fileContents) {
+        console.warn('JSdom base.d.ts not updated');
+        return;
+    }
+    fs.writeFileSync(filePath, replacedContents);
 }
 
 /**
@@ -76,3 +105,4 @@ function makeVariableExplorerAlwaysSorted() {
 
 makeVariableExplorerAlwaysSorted();
 createJupyterKernelWithoutSerialization();
+updateJSDomTypeDefinition();
