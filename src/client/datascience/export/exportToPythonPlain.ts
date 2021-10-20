eed945b449f49c2bd8ad459c29c71f530ef15a1a
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2021-10-20 13:44:44 -0700

    working with basic text

--- src/client/datascience/export/exportToPythonPlain.ts
+++ src/client/datascience/export/exportToPythonPlain.ts
@@ -1,12 +1,17 @@
 import { inject, injectable } from 'inversify';
-import { CancellationToken, NotebookCell, NotebookDocument, Uri } from 'vscode';
+import { CancellationToken, NotebookCell, NotebookCellKind, NotebookDocument, Uri } from 'vscode';
+import { appendLineFeed } from '../../../datascience-ui/common';
 import { IFileSystem } from '../../common/platform/types';
+import { IConfigurationService } from '../../common/types';
 import { IExport } from './types';
 
 // Handles exporting a NotebookDocument to python
 @injectable()
 export class ExportToPythonPlain implements IExport {
-    public constructor(@inject(IFileSystem) private readonly fs: IFileSystem) {}
+    public constructor(
+        @inject(IFileSystem) private readonly fs: IFileSystem,
+        @inject(IConfigurationService) private readonly configuration: IConfigurationService
+    ) {}
 
     public async export(sourceDocument: NotebookDocument, target: Uri, token: CancellationToken): Promise<void> {
         if (token.isCancellationRequested) {
@@ -18,13 +23,32 @@ export class ExportToPythonPlain implements IExport {
         await this.fs.writeFile(target, contents);
     }
 
+    // Convert an entire NotebookDocument to a single string
     private exportDocument(document: NotebookDocument): string {
         return document
             .getCells()
             .reduce((previousValue, currentValue) => previousValue + this.exportCell(currentValue), '');
     }
 
-    private exportCell(_cell: NotebookCell): string {
-        return 'testing\n\r';
+    // Convert one NotebookCell to a string, created a cell marker for it
+    private exportCell(cell: NotebookCell): string {
+        if (cell.document.lineCount) {
+            const cellMarker = this.cellMarker(cell);
+            let code = cell.document.getText().splitLines({ trim: false, removeEmptyEntries: false });
+
+            // IANHU: Combine
+            const results = appendLineFeed([cellMarker, ...code, '\n']).join('');
+            return results;
+        }
+
+        return '';
+    }
+
+    // Determine the cell marker for a notebook cell, if it's in the metadata use that
+    // if not use the default setting
+    private cellMarker(cell: NotebookCell): string {
+        const settings = this.configuration.getSettings(cell.notebook.uri);
+        const marker = cell.metadata.interactiveWindowCellMarker ?? settings.defaultCellMarker;
+        return cell.kind === NotebookCellKind.Code ? `${marker}` : `${marker} [markdown]`;
     }
 }
