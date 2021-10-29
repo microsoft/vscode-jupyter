e5bbb5370a4896b434dd3aec56a129bf510bbf22
Don Jayamanne <don.jayamanne@outlook.com>
2021-10-29 14:10:03 -0700

    misc

--- src/client/datascience/jupyter/kernels/kernel.ts
+++ src/client/datascience/jupyter/kernels/kernel.ts
@@ -455,7 +455,7 @@ export class Kernel implements IKernel {
         traceInfoIfCI('After running startup commands');
 
         try {
-            traceInfoIfCI('Requqesting Kernel info');
+            traceInfoIfCI('Requesting Kernel info');
             const info = await notebook.session.requestKernelInfo();
             traceInfoIfCI('Got Kernel info');
             this._info = info?.content;
