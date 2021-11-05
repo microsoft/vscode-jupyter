802c3c67835a9490d865473570249c6bf6b3991d
Don Jayamanne <don.jayamanne@outlook.com>
2021-11-05 10:09:26 -0700

    fix formatting

--- src/test/mocks/vsc/index.ts
+++ src/test/mocks/vsc/index.ts
@@ -66,6 +66,15 @@ export namespace vscMock {
         };
     }
 
+    /**
+     * An error type that should be used to signal cancellation of an operation.
+     *
+     * This type can be used in response to a {@link CancellationToken cancellation token}
+     * being cancelled or when an operation is being cancelled by the
+     * executor of that operation.
+     */
+    export class CancellationError extends Error {}
+
     export class CancellationToken extends EventEmitter<any> implements vscode.CancellationToken {
         public isCancellationRequested!: boolean;
         public onCancellationRequested: vscode.Event<any>;
