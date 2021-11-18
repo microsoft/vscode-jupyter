60e81417184e3ccfb59e90225a8d6d8b95c1df74
Don Jayamanne <don.jayamanne@outlook.com>
2021-11-18 09:07:01 -0800

    Misc

--- src/client/common/variables/environmentVariablesProvider.ts
+++ src/client/common/variables/environmentVariablesProvider.ts
@@ -3,9 +3,10 @@
 import { inject, injectable, optional } from 'inversify';
 import * as path from 'path';
 import { ConfigurationChangeEvent, Disposable, Event, EventEmitter, FileSystemWatcher, Uri } from 'vscode';
+import { TraceOptions } from '../../logging/trace';
 import { sendFileCreationTelemetry } from '../../telemetry/envFileTelemetry';
 import { IWorkspaceService } from '../application/types';
-import { traceVerbose } from '../logger';
+import { traceDecorators, traceVerbose } from '../logger';
 import { IDisposableRegistry } from '../types';
 import { InMemoryCache } from '../utils/cacheUtils';
 import { EnvironmentVariables, IEnvironmentVariablesProvider, IEnvironmentVariablesService } from './types';
@@ -41,6 +42,7 @@ export class EnvironmentVariablesProvider implements IEnvironmentVariablesProvid
         });
     }
 
+    @traceDecorators.verbose('Get Custom Env Variables', TraceOptions.BeforeCall | TraceOptions.Arguments)
     public async getEnvironmentVariables(resource?: Uri): Promise<EnvironmentVariables> {
         // Cache resource specific interpreter data
         const key = this.workspaceService.getWorkspaceFolderIdentifier(resource);
