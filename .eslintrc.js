0678d283c3fd4a1b1058b80f6653315662d28fae
Ke Zhu <shawnzhu@users.noreply.github.com>
2021-12-06 14:09:13 -0500

    Be able to retrieve XSRF token with session cookie (#8442)
    * Be capable to retrieve XSRF token with session cookie
    
    * Supports #8441
    
    * Fix linter
    
    * Update src/client/datascience/jupyter/jupyterPasswordConnect.ts
    
    Co-authored-by: Fabrizio Primerano <fabri.96p@gmail.com>
    
    * Update src/client/datascience/jupyter/jupyterPasswordConnect.ts
    
    Co-authored-by: Fabrizio Primerano <fabri.96p@gmail.com>
    
    * Update src/client/datascience/jupyter/jupyterPasswordConnect.ts
    
    Co-authored-by: Fabrizio Primerano <fabri.96p@gmail.com>
    
    * Update src/client/datascience/jupyter/jupyterPasswordConnect.ts
    
    Co-authored-by: Fabrizio Primerano <fabri.96p@gmail.com>
    
    * Update src/client/datascience/jupyter/jupyterPasswordConnect.ts
    
    Co-authored-by: Fabrizio Primerano <fabri.96p@gmail.com>
    
    * Update src/client/datascience/jupyter/jupyterPasswordConnect.ts
    
    Co-authored-by: Fabrizio Primerano <fabri.96p@gmail.com>
    
    * Update src/client/datascience/jupyter/jupyterPasswordConnect.ts
    
    Co-authored-by: Fabrizio Primerano <fabri.96p@gmail.com>
    
    Co-authored-by: Rich Chiodo <rchiodo@users.noreply.github.com>
    Co-authored-by: Fabrizio Primerano <fabri.96p@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -680,7 +680,6 @@ module.exports = {
         'src/client/datascience/jupyter/interpreter/jupyterInterpreterOldCacheStateStore.ts',
         'src/client/datascience/jupyter/interpreter/jupyterInterpreterSelector.ts',
         'src/client/datascience/jupyter/commandLineSelector.ts',
-        'src/client/datascience/jupyter/jupyterPasswordConnect.ts',
         'src/client/datascience/jupyter/jupyterWebSocket.ts',
         'src/client/datascience/jupyter/variableScriptLoader.ts',
         'src/client/datascience/jupyter/jupyterImporter.ts',
