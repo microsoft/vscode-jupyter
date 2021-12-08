345a4bdd6e65532e39eb210dc09d618c91555c74
Don Jayamanne <don.jayamanne@outlook.com>
2021-12-08 09:15:11 -0800

    Remove redundant code

--- src/client/common/application/encryptedStorage.ts
+++ src/client/common/application/encryptedStorage.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { inject, injectable } from 'inversify';
-import { ExtensionMode } from 'vscode';
-import { IS_REMOTE_NATIVE_TEST } from '../../../test/constants';
 import { traceError } from '../logger';
 import { IExtensionContext } from '../types';
 import { IEncryptedStorage } from './types';
@@ -15,15 +13,7 @@ import { IEncryptedStorage } from './types';
 export class EncryptedStorage implements IEncryptedStorage {
     constructor(@inject(IExtensionContext) private readonly extensionContext: IExtensionContext) {}
 
-    private readonly testingState = new Map<string, string>();
-
     public async store(service: string, key: string, value: string | undefined): Promise<void> {
-        // On CI we don't need to use keytar for testing (else it hangs).
-        if (IS_REMOTE_NATIVE_TEST && this.extensionContext.extensionMode !== ExtensionMode.Production) {
-            this.testingState.set(`${service}#${key}`, value || '');
-            return;
-        }
-
         if (!value) {
             try {
                 await this.extensionContext.secrets.delete(`${service}.${key}`);
@@ -35,10 +25,6 @@ export class EncryptedStorage implements IEncryptedStorage {
         }
     }
     public async retrieve(service: string, key: string): Promise<string | undefined> {
-        // On CI we don't need to use keytar for testing (else it hangs).
-        if (IS_REMOTE_NATIVE_TEST && this.extensionContext.extensionMode !== ExtensionMode.Production) {
-            return this.testingState.get(`${service}#${key}`);
-        }
         try {
             // eslint-disable-next-line
             const val = await this.extensionContext.secrets.get(`${service}.${key}`);
