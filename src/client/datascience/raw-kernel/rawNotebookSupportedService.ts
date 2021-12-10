63ecb071390c11008f70e01daf568cf2c431fd2d
Don Jayamanne <don.jayamanne@outlook.com>
2021-12-09 16:53:39 -0800

    Check if raw is supported even when using remote connections (#8513)

--- src/client/datascience/raw-kernel/rawNotebookSupportedService.ts
+++ src/client/datascience/raw-kernel/rawNotebookSupportedService.ts
@@ -6,7 +6,7 @@ import { IS_NON_RAW_NATIVE_TEST } from '../../../test/constants';
 import { traceError, traceInfo } from '../../common/logger';
 import { IConfigurationService } from '../../common/types';
 import { sendTelemetryEvent } from '../../telemetry';
-import { Settings, Telemetry } from '../constants';
+import { Telemetry } from '../constants';
 import { IRawNotebookSupportedService } from '../types';
 
 // This class check to see if we have everything in place to support a raw kernel launch on the machine
@@ -17,26 +17,12 @@ export class RawNotebookSupportedService implements IRawNotebookSupportedService
 
     // Check to see if we have all that we need for supporting raw kernel launch
     public get isSupported(): boolean {
-        if (!this.localLaunch()) {
-            return false;
-        }
         return this.isSupportedForLocalLaunch();
     }
 
     private isSupportedForLocalLaunch(): boolean {
         // Save the ZMQ support for last, since it's probably the slowest part
         return !this.isZQMDisabled() && this.zmqSupported();
-    }
-
-    private localLaunch(): boolean {
-        const settings = this.configuration.getSettings(undefined);
-        const serverType: string | undefined = settings.jupyterServerType;
-
-        if (!serverType || serverType.toLowerCase() === Settings.JupyterServerLocalLaunch) {
-            return true;
-        }
-
-        return false;
     }
 
     // Check to see if our hidden setting has been turned on to disable local ZMQ support
