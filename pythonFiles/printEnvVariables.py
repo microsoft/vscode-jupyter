8d8b80191dc6d0799b96f0d5896dd51bd771d793
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-12-15 16:05:18 -0800

    Non ZMQ scenarios failing when using conda env (#8556)
    * Add sys path to PYTHONPATH
    
    * Fix black formatting

--- pythonFiles/printEnvVariables.py
+++ pythonFiles/printEnvVariables.py
@@ -4,6 +4,14 @@
 import os
 import builtins
 import json
+import sys
 
 builtins.print("e976ee50-99ed-4aba-9b6b-9dcd5634d07d")
+
+# Special case PYTHONPATH to include sys.path
+formatted_sys_path = os.pathsep.join(sys.path)
+existing = f"{os.getenv('PYTHONPATH')};" if os.getenv("PYTHONPATH") else ""
+os.environ["PYTHONPATH"] = f"{existing}{formatted_sys_path}"
+
+# Dump results
 builtins.print(json.dumps(dict(os.environ)))
--- pythonFiles/printEnvVariablesToFile.py
+++ pythonFiles/printEnvVariablesToFile.py
@@ -9,5 +9,10 @@ import sys
 # Last argument is the target file into which we'll write the env variables as json.
 json_file = sys.argv[-1]
 
+# Special case PYTHONPATH to include sys.path
+formatted_sys_path = os.pathsep.join(sys.path)
+existing = f"{os.getenv('PYTHONPATH')};" if os.getenv("PYTHONPATH") else ""
+os.environ["PYTHONPATH"] = f"{existing}{formatted_sys_path}"
+
 with open(json_file, "w") as outfile:
     json.dump(dict(os.environ), outfile)
