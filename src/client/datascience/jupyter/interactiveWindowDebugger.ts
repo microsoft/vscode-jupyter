e450644f2b8b14cbb98f7391c6125fb4da1260d0
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2022-01-13 17:24:06 -0800

    remove logging

--- src/client/datascience/jupyter/interactiveWindowDebugger.ts
+++ src/client/datascience/jupyter/interactiveWindowDebugger.ts
@@ -69,7 +69,6 @@ export class InteractiveWindowDebugger implements IInteractiveWindowDebugger, IC
             traceInfo('stop debugging');
 
             // Tell our debug service to shutdown if possible
-            traceInfo(`IANHUZ Debug Detach`);
             this.debuggingActive = false;
             this.debugService.stop();
 
@@ -88,7 +87,6 @@ export class InteractiveWindowDebugger implements IInteractiveWindowDebugger, IC
             await Promise.all(
                 hashes.map(async (fileHash) => {
                     if (this.debuggingActive) {
-                        traceInfo(`IANHUZ ${this.debuggingActive}`);
                         return this.debugService.activeDebugSession!.customRequest(
                             'setPydevdSourceMap',
                             this.buildSourceMap(fileHash)
@@ -127,11 +125,9 @@ export class InteractiveWindowDebugger implements IInteractiveWindowDebugger, IC
         if (config) {
             traceInfo('connected to notebook during debugging');
 
-            traceInfo(`IANHUZ start Debug Session`);
             this.debuggingActive = await startCommand(config);
 
             if (this.debuggingActive) {
-                traceInfo(`IANHUZ start Debug Session debug active`);
                 // Force the debugger to update its list of breakpoints. This is used
                 // to make sure the breakpoint list is up to date when we do code file hashes
                 this.debugService.removeBreakpoints([]);
