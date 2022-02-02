d85afad38200148476622c4e593f570d318e40dc
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-02-01 17:37:28 -0800

    Add some more logging to look at debug test failures

--- src/test/datascience/notebook/helper.ts
+++ src/test/datascience/notebook/helper.ts
@@ -894,6 +894,7 @@ export async function waitForDebugEvent<T>(
 }
 
 export async function waitForStoppedEvent(debugAdapter: IKernelDebugAdapter): Promise<DebugProtocol.StoppedEvent> {
+    assert.ok(debugAdapter, `No debug adapter when waiting for stoppped event`);
     return waitForDebugEvent('stopped', debugAdapter, 10_000);
 }
 
