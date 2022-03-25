63c9f3917292b968bc2c9411b85fff57580a2828
Don Jayamanne <don.jayamanne@outlook.com>
2022-03-25 14:46:02 +1100

    Fixs

--- src/test/datascience/export/exportToHTML.vscode.test.ts
+++ src/test/datascience/export/exportToHTML.vscode.test.ts
@@ -29,9 +29,7 @@ suite('DataScience - Export HTML', function () {
     });
     teardown(closeActiveWindows);
     suiteTeardown(closeActiveWindows);
-    test('Export To HTML', async function () {
-        // See this issue: https://github.com/microsoft/vscode-jupyter/issues/9468
-        this.skip();
+    test('Export To HTML', async () => {
         const fileSystem = api.serviceContainer.get<IFileSystem>(IFileSystem);
         const exportToHTML = api.serviceContainer.get<INbConvertExport>(INbConvertExport, ExportFormat.html);
         const exportInterpreterFinder = api.serviceContainer.get<ExportInterpreterFinder>(ExportInterpreterFinder);
