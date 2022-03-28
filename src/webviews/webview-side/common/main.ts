5b37a1ababf59a81113f6db0ccb76109f5c0806f
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-03-28 16:14:12 -0700

    Fix linter

--- src/webviews/webview-side/common/main.ts
+++ src/webviews/webview-side/common/main.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 declare let __webpack_public_path__: string;
 
