0e62643ff45586357127b9b43a62fe3c5e5674ca
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2022-03-29 08:55:17 -0700

    prettier not run on file

--- src/platform/common/application/commands.ts
+++ src/platform/common/application/commands.ts
@@ -94,8 +94,8 @@ export interface ICommandNameArgumentTypeMapping extends ICommandNameWithoutArgu
     ['notebook.execute']: [];
     ['notebook.cell.edit']: [];
     ['notebook.cell.execute']:
-    | []
-    | [{ ranges: { start: number; end: number }[]; document?: Uri; autoReveal?: boolean }]; // TODO update this
+        | []
+        | [{ ranges: { start: number; end: number }[]; document?: Uri; autoReveal?: boolean }]; // TODO update this
     ['notebook.cell.insertCodeCellBelow']: [];
     ['notebook.undo']: [];
     ['notebook.redo']: [];
@@ -104,13 +104,13 @@ export interface ICommandNameArgumentTypeMapping extends ICommandNameWithoutArgu
     ['workbench.action.files.saveAs']: [Uri];
     ['workbench.action.files.save']: [Uri];
     ['notebook.selectKernel']:
-    | [
-        // This set of args will set the kernel/controller to the one with the id provided.
-        | { id: string; extension: string }
-        // This set of args will display the kernel picker.
-        | { notebookEditor: NotebookEditor }
-    ]
-    | [];
+        | [
+              // This set of args will set the kernel/controller to the one with the id provided.
+              | { id: string; extension: string }
+              // This set of args will display the kernel picker.
+              | { notebookEditor: NotebookEditor }
+          ]
+        | [];
     ['undo']: [];
     ['interactive.open']: [
         { preserveFocus?: boolean; viewColumn?: ViewColumn },
