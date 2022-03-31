b4d6cbb09c93f219932b67381b8c68de6923c144
Don Jayamanne <don.jayamanne@outlook.com>
2022-04-01 10:40:06 +1100

    MIsc

--- src/kernels/installer/moduleInstaller.node.ts
+++ src/kernels/installer/moduleInstaller.node.ts
@@ -115,13 +115,11 @@ export abstract class ModuleInstaller implements IModuleInstaller {
             if (observable) {
                 observable.out.subscribe({
                     next: (output) => {
-                        if (progress) {
-                            const message =
-                                output.out.length > 100
-                                    ? `${output.out.substring(0, 50)}...${output.out.substring(output.out.length - 50)}`
-                                    : output.out;
-                            progress.report({ message });
-                        }
+                        const message =
+                            output.out.length > 100
+                                ? `${output.out.substring(0, 50)}...${output.out.substring(output.out.length - 50)}`
+                                : output.out;
+                        progress.report({ message });
                         traceInfo(output.out);
                         if (output.source === 'stderr') {
                             lastStdErr = output.out;
