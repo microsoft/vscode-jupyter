0bb58aa62e922fb6ea4ee9ce373ade38de89d34b
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-05-06 11:16:41 -0700

    Revert "Fix uri path problem"
    This reverts commit ce233b5e1bd7de6f4dc5c9a69b35b755627ce93c.

--- src/test/datascience/jupyterServer.node.ts
+++ src/test/datascience/jupyterServer.node.ts
@@ -114,9 +114,7 @@ export class JupyterServer {
                         useCert: true
                     });
                     await sleep(5_000); // Wait for some time for Jupyter to warm up & be ready to accept connections.
-
-                    // Certs should be using https, not http
-                    resolve(`https://localhost:${port}/?token=${token}`);
+                    resolve(`http://localhost:${port}/?token=${token}`);
                 } catch (ex) {
                     reject(ex);
                 }
