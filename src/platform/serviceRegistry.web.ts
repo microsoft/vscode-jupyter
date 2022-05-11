d50d450e2703a18f1d79e0f3011b6a968e46eeba
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2022-05-11 13:32:29 -0700

    rename debugging manager and add to web service registry

--- src/platform/serviceRegistry.web.ts
+++ src/platform/serviceRegistry.web.ts
@@ -27,6 +27,8 @@ import { GlobalActivation } from './common/globalActivation';
 import { IExtensionSingleActivationService } from './activation/types';
 import { ExtensionSideRenderer, IExtensionSideRenderer } from '../webviews/extension-side/renderer';
 import { OutputCommandListener } from './logging/outputCommandListener';
+import { IDebuggingManager } from './debugger/types';
+import { DebuggingManager } from './debugger/jupyter/debuggingManager';
 
 export function registerTypes(context: IExtensionContext, serviceManager: IServiceManager, isDevMode: boolean) {
     serviceManager.addSingleton<ICommandManager>(ICommandManager, CommandManager);
@@ -44,4 +46,8 @@ export function registerTypes(context: IExtensionContext, serviceManager: IServi
     registerApiTypes(serviceManager);
     registerActivationTypes(serviceManager);
     registerDevToolTypes(context, serviceManager, isDevMode);
+
+    serviceManager.addSingleton<IDebuggingManager>(IDebuggingManager, DebuggingManager, undefined, [
+        IExtensionSingleActivationService
+    ]);
 }
