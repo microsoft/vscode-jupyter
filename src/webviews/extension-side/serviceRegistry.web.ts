03bde7fca49c6f9c4aa20a46adb15b6b26b8055e
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2022-05-12 19:42:57 -0400

    wip

--- src/webviews/extension-side/serviceRegistry.web.ts
+++ src/webviews/extension-side/serviceRegistry.web.ts
@@ -2,12 +2,21 @@
 // Licensed under the MIT License.
 'use strict';
 
+import { IExtensionSingleActivationService } from '../../platform/activation/types';
 import { IWebviewViewProvider, IWebviewPanelProvider } from '../../platform/common/application/types';
 import { IServiceManager } from '../../platform/ioc/types';
 import { WebviewViewProvider } from './webviewViews/webviewViewProvider.web';
 import { WebviewPanelProvider } from './webviewPanels/webviewPanelProvider.web';
+import { VariableViewActivationService } from './variablesView/variableViewActivationService.web';
+import { IVariableViewProvider } from './variablesView/types';
+import { VariableViewProvider } from './variablesView/variableViewProvider.web';
 
 export function registerTypes(serviceManager: IServiceManager, _isDevMode: boolean) {
     serviceManager.add<IWebviewViewProvider>(IWebviewViewProvider, WebviewViewProvider);
     serviceManager.add<IWebviewPanelProvider>(IWebviewPanelProvider, WebviewPanelProvider);
+    serviceManager.addSingleton<IExtensionSingleActivationService>(
+        IExtensionSingleActivationService,
+        VariableViewActivationService
+    );
+    serviceManager.addSingleton<IVariableViewProvider>(IVariableViewProvider, VariableViewProvider);
 }
