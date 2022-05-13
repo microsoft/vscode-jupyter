8beca33b170a91df23a5450cb8ae8bb9df0baebe
rebornix <penn.lv@gmail.com>
2022-05-12 17:22:31 -0700

    :spider_web: Plain Python

--- src/platform/export/exportToPythonPlain.web.ts
+++ src/platform/export/exportToPythonPlain.web.ts
@@ -1,25 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
 import { inject, injectable } from 'inversify';
-import * as os from 'os';
 import { Uri } from 'vscode';
-import { IFileSystem } from '../common/platform/types.node';
+import { writeFile } from '../common/platform/fileSystem';
 import { IConfigurationService } from '../common/types';
 import { ExportToPythonPlainBase } from './exportToPythonPlain';
 
 // Handles exporting a NotebookDocument to python
 @injectable()
 export class ExportToPythonPlain extends ExportToPythonPlainBase {
-    public constructor(
-        @inject(IFileSystem) private readonly fs: IFileSystem,
-        @inject(IConfigurationService) configuration: IConfigurationService
-    ) {
+    public constructor(@inject(IConfigurationService) configuration: IConfigurationService) {
         super(configuration);
     }
 
     override async writeFile(target: Uri, contents: string): Promise<void> {
-        await this.fs.writeFile(target, contents);
-    }
-
-    override getEOL(): string {
-        return os.EOL;
+        await writeFile(target, contents);
     }
 }
