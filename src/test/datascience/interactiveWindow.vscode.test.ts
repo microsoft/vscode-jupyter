0411aa7c7ed8698fae013cd22210dbe6aa4759a0
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-05-27 11:28:43 -0700

    Second line number also off

--- src/test/datascience/interactiveWindow.vscode.test.ts
+++ src/test/datascience/interactiveWindow.vscode.test.ts
@@ -494,8 +494,8 @@ ${actualCode}
             // Should be three hrefs for the two lines in the call stack
             const hrefs = html.match(/<a\s+href='.*\?line=(\d+)'/gm);
             assert.equal(hrefs?.length, 4, '4 hrefs not found in traceback');
-            assert.ok(hrefs[0].endsWith("line=4'"), `Wrong second ref line : ${hrefs[0]}`);
-            assert.ok(hrefs[1].endsWith("line=1'"), `Wrong third ref line : ${hrefs[1]}`);
+            assert.ok(hrefs[0].endsWith("line=4'"), `Wrong first ref line : ${hrefs[0]}`);
+            assert.ok(hrefs[1].endsWith("line=5'"), `Wrong second ref line : ${hrefs[1]}`);
             assert.ok(hrefs[2].endsWith("line=2'"), `Wrong last ref line : ${hrefs[2]}`);
         });
 
