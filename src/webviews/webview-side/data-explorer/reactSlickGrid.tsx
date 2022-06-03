795cfc2c38698d5eedc8f179c5b78d2c8d600677
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-06-03 12:56:53 -0700

    Dataframe viewer is broken (#10303)
    * Fix variables view closing/reopening
    
    * Fix dataframe viewer to show up again

--- src/webviews/webview-side/data-explorer/reactSlickGrid.tsx
+++ src/webviews/webview-side/data-explorer/reactSlickGrid.tsx
@@ -195,7 +195,7 @@ export class ReactSlickGrid extends React.Component<ISlickGridProps, ISlickGridS
         if (this.containerRef.current) {
             // Compute font size. Default to 15 if not found.
             let fontSize = parseInt(
-                getComputedStyle(this.containerRef.current).getPropertyValue('--code-font-size'),
+                getComputedStyle(this.containerRef.current).getPropertyValue('--vscode-font-size'),
                 10
             );
             if (isNaN(fontSize)) {
