dd80acc34635dca5d19a650a171e66e29c44f2c5
rebornix <penn.lv@gmail.com>
2022-06-10 13:59:32 -0700

    misc

--- src/platform/vscode-path/strings.ts
+++ src/platform/vscode-path/strings.ts
@@ -9,40 +9,6 @@ import { LRUCachedComputed } from './cache';
 import { CharCode } from './charCode';
 import { Lazy } from './lazy';
 
-/**
- * Removes all occurrences of needle from the end of haystack.
- * @param haystack string to trim
- * @param needle the thing to trim
- */
-export function rtrim(haystack: string, needle: string): string {
-    if (!haystack || !needle) {
-        return haystack;
-    }
-
-    const needleLen = needle.length,
-        haystackLen = haystack.length;
-
-    if (needleLen === 0 || haystackLen === 0) {
-        return haystack;
-    }
-
-    let offset = haystackLen,
-        idx = -1;
-
-    while (true) {
-        idx = haystack.lastIndexOf(needle, offset - 1);
-        if (idx === -1 || idx + needleLen !== offset) {
-            break;
-        }
-        if (idx === 0) {
-            return '';
-        }
-        offset = idx;
-    }
-
-    return haystack.substring(0, offset);
-}
-
 export function compare(a: string, b: string): number {
     if (a < b) {
         return -1;
