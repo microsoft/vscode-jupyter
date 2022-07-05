21c6fd4d558c6ae34f0f36d7ff0b6be38863a896
Don Jayamanne <don.jayamanne@outlook.com>
2022-07-06 07:49:27 +1000

    Replace all CRLF with LF in telemetry report

--- src/platform/tools/telemetryGenerator.node.ts
+++ src/platform/tools/telemetryGenerator.node.ts
@@ -280,7 +280,7 @@ function writeTelemetryEntry(entry: TelemetryEntry) {
             const link = `https://github.com/microsoft/vscode-jupyter/tree/main/${l.file}`;
             writeOutput(`\n[${l.file}](${link})`);
             writeOutput('```typescript');
-            writeOutput(l.code.replace('\\r\\n', '\n'));
+            writeOutput(l.code.replace(/\r\n/g, '\n'));
             writeOutput('```\n');
         });
     }
