59023b48bf051f29f12937a17159aa8386df5380
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2022-07-14 19:08:00 -0400

    test fix

--- src/test/datascience/data-viewing/dataViewerDependencyService.unit.test.ts
+++ src/test/datascience/data-viewing/dataViewerDependencyService.unit.test.ts
@@ -119,7 +119,7 @@ suite('DataScience - DataViewerDependencyService', () => {
         assert.equal(await resultPromise, undefined);
         assert.deepEqual(
             stub.getCalls().map((call) => call.lastArg),
-            [kernelGetPandasVersion[0], '%pip install pandas']
+            [kernelGetPandasVersion, '%pip install pandas']
         );
     });
 
