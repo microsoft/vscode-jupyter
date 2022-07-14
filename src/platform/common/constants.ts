f1137fd59ffa2a8d0f20f903dd50e0670a01f7f7
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-07-14 16:01:03 -0700

    Use new telemetry key (#10821)

--- src/platform/common/constants.ts
+++ src/platform/common/constants.ts
@@ -24,7 +24,7 @@ export const NOTEBOOK_SELECTOR = [
 ];
 
 export const JVSC_EXTENSION_ID = 'ms-toolsai.jupyter';
-export const AppinsightsKey = 'AIF-d9b70cd4-b9f9-4d70-929b-a071c400b217';
+export const AppinsightsKey = '0c6ae279ed8443289764825290e4f9e2-1a736e7c-1324-4338-be46-fc2a58ae4d14-7255';
 
 export const DEFAULT_INTERPRETER_SETTING = 'python';
 
