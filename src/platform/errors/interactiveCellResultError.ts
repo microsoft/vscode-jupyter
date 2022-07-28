2e526845ab79340824ff4c265e5eeaee734502f4
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-07-28 15:18:14 -0700

    First try at documenting an error

--- src/platform/errors/interactiveCellResultError.ts
+++ src/platform/errors/interactiveCellResultError.ts
@@ -3,7 +3,16 @@
 
 import { NotebookCell } from 'vscode';
 
-// Small helper error to use in our class
+/**
+ * Error thrown when running cells in the interactive window.
+ *
+ * Cause:
+ * Kernel.executeCell() returns an error when running a cell in the interactive window.
+ *
+ * Handled by:
+ * Running cell in IW shows the error. If no running cell, then a notification is shown to the user.
+ *
+ */
 export class InteractiveCellResultError extends Error {
     constructor(public readonly cell: NotebookCell) {
         super('Cell failed to execute');
