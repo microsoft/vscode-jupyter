aba0560ca96e04a55e5ab98f338b6c2ea85fc7cd
Don Jayamanne <don.jayamanne@outlook.com>
2022-08-30 04:51:19 +1000

    Remove files

--- src/platform/telemetry/index.ts
+++ src/platform/telemetry/index.ts
@@ -369,10 +369,6 @@ export function capturePerfTelemetry<This, P extends IEventNamePropertyMapping, 
  * Decorates a method, sending a telemetry event with the given properties.
  * @param eventName The event name to send.
  * @param properties Properties to send with the event; must be valid for the event.
- * @param captureDuration True if the method's execution duration should be captured.
- * @param failureEventName If the decorated method returns a Promise and fails, send this event instead of eventName.
- * @param lazyProperties A static function on the decorated class which returns extra properties to add to the event.
- * This can be used to provide properties which are only known at runtime (after the decorator has executed).
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any,
 export function captureUsageTelemetry<This, P extends IEventNamePropertyMapping, E extends keyof P>(
