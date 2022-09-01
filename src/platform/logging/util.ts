5b9207a30739cbce2555571ae80b12dcdf51cad3
Rob Lourens <roblourens@gmail.com>
2022-09-01 13:02:08 -0500

    Fix log timestamp formatting (#11257)

--- src/platform/logging/util.ts
+++ src/platform/logging/util.ts
@@ -53,5 +53,9 @@ export function returnValueToLogString(returnValue: unknown): string {
 }
 export function getTimeForLogging(): string {
     const date = new Date();
-    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`;
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const millis = String(date.getMilliseconds()).padStart(3, '0');
+    return `${hours}:${minutes}:${seconds}.${millis}`;
 }
