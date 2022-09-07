0147943f33b35149462461511dcdb4c9c2d02b14
Don Jayamanne <don.jayamanne@outlook.com>
2022-09-08 06:36:31 +1000

    Misc

--- src/platform/constants.ts
+++ src/platform/constants.ts
@@ -7,10 +7,21 @@ export const HiddenFileFormatString = '_HiddenFile_{0}.py';
 
 export const MillisecondsInADay = 24 * 60 * 60 * 1_000;
 
-declare const IS_PRE_RELEASE_VERSION_OF_JUPYTER_EXTENSION: 'true' | 'false';
+/**
+ * The value for the key `IS_PRE_RELEASE_VERSION_OF_JUPYTER_EXTENSION` will be replaced by webpack.
+ */
+const isPreRelease = {
+    IS_PRE_RELEASE_VERSION_OF_JUPYTER_EXTENSION: undefined
+};
 export function isPreReleaseVersionOfExtension(): boolean {
     try {
-        return IS_PRE_RELEASE_VERSION_OF_JUPYTER_EXTENSION === 'true';
+        if ('true' in isPreRelease) {
+            return true;
+        } else if ('false' in isPreRelease) {
+            return false;
+        }
+        // Development env of extension is the same as pre-release.
+        return true;
     } catch {
         return false;
     }
