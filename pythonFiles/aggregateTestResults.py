5e6012b7f7fe81881c5a934ec890968f73d06de5
Aaron Munger <aamunger@microsoft.com>
2022-09-30 10:14:29 -0700

    include all `build and test` runs that run against main (#11494)
    * include all events that run against main
    
    * just use a query parameter for branch filtering
    
    * use parameter map

--- pythonFiles/aggregateTestResults.py
+++ pythonFiles/aggregateTestResults.py
@@ -11,8 +11,8 @@ print("Using authtoken with prefix: " + authtoken[:4])
 # %%
 def getRuns(createdDate):
     runsResponse = requests.get(
-        "https://api.github.com/repos/microsoft/vscode-jupyter/actions/runs",
-        params={"event": "push", "created": createdDate},
+        "https://api.github.com/repos/microsoft/vscode-jupyter/actions/workflows/build-test.yml/runs",
+        params={"created": createdDate, "branch": "main"},
         headers={
             "Accept": "application/vnd.github+json",
             "Authorization": f"Bearer {authtoken}",
@@ -23,9 +23,12 @@ def getRuns(createdDate):
         print(f"Error {runsResponse.status_code}")
         raise Exception("Error getting runs")
 
-    print(f"Found {len(runsResponse.json()['workflow_runs'])} runs")
+    runs = runsResponse.json()["workflow_runs"]
 
-    return runsResponse.json()["workflow_runs"]
+    for run in runs:
+        print(f"Found run {run['id']} for event '{run['event']}'")
+
+    return runs
 
 
 def getArtifactData(id):
@@ -157,8 +160,7 @@ runs = getRuns(collectionDate)
 # %%
 runResults = []
 for run in runs:
-    if run["name"] == "Build and Test":
-        runResults.append(getResultsForRun(run))
+    runResults.append(getResultsForRun(run))
 
 # %%
 resultFile = f'AggTestResults-{collectionDate.strftime("%Y-%m-%d")}.json'
