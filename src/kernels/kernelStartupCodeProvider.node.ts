4f6d44acb8c61cfef539babb994650c461ccb658
Don Jayamanne <don.jayamanne@outlook.com>
2022-10-07 09:09:07 +1100

    Address code review comments

--- src/kernels/kernelStartupCodeProvider.node.ts
+++ src/kernels/kernelStartupCodeProvider.node.ts
@@ -5,9 +5,8 @@ import { inject, injectable } from 'inversify';
 import { CodeSnippets } from '../platform/common/constants';
 import { getFilePath } from '../platform/common/platform/fs-paths';
 import { traceInfo } from '../platform/logging';
-import { isLocalHostConnection, isPythonKernelConnection } from './helpers';
 import { KernelWorkingFolder } from './kernelWorkingFolder.node';
-import { IKernel, isLocalConnection, IStartupCodeProvider, StartupCodePriority } from './types';
+import { IKernel, IStartupCodeProvider, StartupCodePriority } from './types';
 
 @injectable()
 export class KernelStartupCodeProvider implements IStartupCodeProvider {
@@ -16,25 +15,6 @@ export class KernelStartupCodeProvider implements IStartupCodeProvider {
     constructor(@inject(KernelWorkingFolder) private readonly kernelWorkingFolder: KernelWorkingFolder) {}
 
     async getCode(kernel: IKernel): Promise<string[]> {
-        // We cannot change paths for remote live kernels.
-        if (kernel.kernelConnectionMetadata.kind === 'connectToLiveRemoteKernel') {
-            return [];
-        }
-        // Only local kernels are supported.
-        if (
-            !isLocalConnection(kernel.kernelConnectionMetadata) &&
-            !isLocalHostConnection(kernel.kernelConnectionMetadata)
-        ) {
-            return [];
-        }
-        if (!isPythonKernelConnection(kernel.kernelConnectionMetadata)) {
-            return [];
-        }
-
-        // If this is a live kernel, we shouldn't be changing anything by running startup code.
-        if (!isPythonKernelConnection(kernel.kernelConnectionMetadata)) {
-            return [];
-        }
         const suggestedDir = await this.kernelWorkingFolder.getWorkingDirectory(kernel);
         if (suggestedDir) {
             traceInfo('UpdateWorkingDirectoryAndPath in Kernel');
--- src/kernels/kernelStartupCodeProvider.node.unit.test.ts
+++ src/kernels/kernelStartupCodeProvider.node.unit.test.ts
@@ -5,54 +5,26 @@ import { assert } from 'chai';
 import { anything, instance, mock, when } from 'ts-mockito';
 import { Uri } from 'vscode';
 import { KernelWorkingFolder } from './kernelWorkingFolder.node';
-import { IJupyterKernelSpec, IKernel, KernelConnectionMetadata, LocalKernelSpecConnectionMetadata } from './types';
+import { IKernel } from './types';
 import { KernelStartupCodeProvider } from './kernelStartupCodeProvider.node';
-import { PYTHON_LANGUAGE } from '../platform/common/constants';
 
 suite('Kernel Startup Code Provider', function () {
     let kernelWorkingFolder: KernelWorkingFolder;
     let startupCodeProvider: KernelStartupCodeProvider;
     let kernel: IKernel;
-    let connectionMetadata: KernelConnectionMetadata;
-    let kernelSpec: IJupyterKernelSpec;
     setup(() => {
         kernel = mock<IKernel>();
-        kernelSpec = mock<IJupyterKernelSpec>();
-        connectionMetadata = mock<KernelConnectionMetadata>();
-        when(kernel.kernelConnectionMetadata).thenReturn(instance(connectionMetadata));
 
         kernelWorkingFolder = mock<KernelWorkingFolder>();
-        when(kernelWorkingFolder.getWorkingDirectory(anything())).thenResolve(Uri.file(__dirname));
         startupCodeProvider = new KernelStartupCodeProvider(instance(kernelWorkingFolder));
     });
-    test('No startup code for Remote Live Kernel', async () => {
-        when(connectionMetadata.kind).thenReturn('connectToLiveRemoteKernel');
+    test('Has startup code when we do not have a working directory', async () => {
+        when(kernelWorkingFolder.getWorkingDirectory(anything())).thenResolve(undefined);
 
         assert.isEmpty(await startupCodeProvider.getCode(instance(kernel)));
     });
-    test('No startup code for Remote Kernel Specs', async () => {
-        when(connectionMetadata.kind).thenReturn('startUsingRemoteKernelSpec');
-
-        assert.isEmpty(await startupCodeProvider.getCode(instance(kernel)));
-    });
-    test('No startup code for non-Python local kernelspecs', async () => {
-        let localKernelSpec = connectionMetadata as LocalKernelSpecConnectionMetadata;
-        when(localKernelSpec.kind).thenReturn('startUsingLocalKernelSpec');
-        when(localKernelSpec.kernelSpec).thenReturn(instance(kernelSpec));
-        when(kernelSpec.language).thenReturn('Java');
-
-        assert.isEmpty(await startupCodeProvider.getCode(instance(kernel)));
-    });
-    test('Has startup code for Local Python Kernelspecs', async () => {
-        let localKernelSpec = connectionMetadata as LocalKernelSpecConnectionMetadata;
-        when(localKernelSpec.kind).thenReturn('startUsingLocalKernelSpec');
-        when(localKernelSpec.kernelSpec).thenReturn(instance(kernelSpec));
-        when(kernelSpec.language).thenReturn(PYTHON_LANGUAGE);
-
-        assert.isNotEmpty(await startupCodeProvider.getCode(instance(kernel)));
-    });
-    test('Has startup code for Local Python Interpreter', async () => {
-        when(connectionMetadata.kind).thenReturn('startUsingPythonInterpreter');
+    test('Has startup code when we have a working directory', async () => {
+        when(kernelWorkingFolder.getWorkingDirectory(anything())).thenResolve(Uri.file(__dirname));
 
         assert.isNotEmpty(await startupCodeProvider.getCode(instance(kernel)));
     });
