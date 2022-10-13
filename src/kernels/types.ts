2dd6e06327a954252fedde6ad2cec5ecfd9d0a07
Don Jayamanne <don.jayamanne@outlook.com>
2022-10-13 11:56:13 +1100

    Misc

--- src/kernels/types.ts
+++ src/kernels/types.ts
@@ -611,6 +611,7 @@ export const IKernelFinder = Symbol('IKernelFinder');
 
 export interface IKernelFinder {
     onDidChangeKernels: Event<void>;
+    listKernels(cancelToken?: CancellationToken): Promise<KernelConnectionMetadata[]>;
     /*
      * For a given kernel connection metadata return what kernel finder found it
      */
