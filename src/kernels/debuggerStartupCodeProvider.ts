0111a77aca32c88fcf3022119ecc10cd60dfbe4f
Don Jayamanne <don.jayamanne@outlook.com>
2022-10-18 12:36:22 +1100

    Set PYDEVD_IPYTHON_COMPATIBLE_DEBUGGING in Python process (#11684)

--- src/kernels/debuggerStartupCodeProvider.ts
+++ src/kernels/debuggerStartupCodeProvider.ts
@@ -3,7 +3,7 @@
 
 import { injectable } from 'inversify';
 import { isPythonKernelConnection } from './helpers';
-import { IKernel, IStartupCodeProvider, StartupCodePriority } from './types';
+import { IKernel, isLocalConnection, IStartupCodeProvider, StartupCodePriority } from './types';
 
 @injectable()
 export class DebugStartupCodeProvider implements IStartupCodeProvider {
@@ -13,7 +13,11 @@ export class DebugStartupCodeProvider implements IStartupCodeProvider {
         if (!isPythonKernelConnection(kernel.kernelConnectionMetadata)) {
             return [];
         }
-
+        if (isLocalConnection(kernel.kernelConnectionMetadata)) {
+            return [];
+        }
+        // Inject the env var only for remote kernels, for local kernels,
+        // this env var will be setup when spawning the kernel process.
         return [
             'import os as __VSCODE_os',
             // Required to get pydevd to work properly in Python kernel, more info here https://github.com/microsoft/vscode-jupyter/issues/11033
