4e123082895fa29dec36613e025ee7b4d543d3b6
Don Jayamanne <don.jayamanne@outlook.com>
2022-10-20 10:37:00 +1100

    Skip debugger tests

--- src/test/datascience/debugger.vscode.test.ts
+++ src/test/datascience/debugger.vscode.test.ts
@@ -34,7 +34,7 @@ import { Commands } from '../../platform/common/constants';
 import { IVariableViewProvider } from '../../webviews/extension-side/variablesView/types';
 import { IDebuggingManager } from '../../notebooks/debugger/debuggingTypes';
 
-suite('VSCode Notebook - Run By Line', function () {
+suite.only('VSCode Notebook - Run By Line', function () {
     let api: IExtensionTestApi;
     const disposables: IDisposable[] = [];
     let commandManager: ICommandManager;
