251bcc7e60e6afd030a0fd697500fb2330ba8a01
Rob Lourens <roblourens@gmail.com>
2022-10-21 12:33:01 -0700

    Implement restart  (#11668)
    * Support debug "restart" microsoft/vscode-jupyter#7670
    
    * More debug refactoring, set up IW for restart
    
    * Hook up Restart for rbl, via a custom Restart request implementation.
    Move commands out of DebuggingManager, and expose methods on that class for use by the rbl controller
    
    * Hook up restart for "debug cell"
    
    * Clean up dumped cell files in disconnect, so it can be awaited
    
    * Fix comments in controller
    
    * formatting
    
    * Fix PR comments
    
    * Wait for debugger to be totally set up before executing cell in IW
    
    * Add test for restarting debug.
    Also fix the restart request handler to avoid cancelling the restart request on accident.

---
+++ src/notebooks/debugger/commandRegistry.ts
@@ -0,0 +1,89 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+'use strict';
+
+import { inject, injectable } from 'inversify';
+import { NotebookCell } from 'vscode';
+import { ICommandManager, IVSCodeNotebook } from '../../platform/common/application/types';
+
+import { IExtensionSingleActivationService } from '../../platform/activation/types';
+import { Commands } from '../../platform/common/constants';
+import { IDisposable, IDisposableRegistry } from '../../platform/common/types';
+import { sendTelemetryEvent } from '../../telemetry';
+import { DebuggingTelemetry } from './constants';
+import { INotebookDebuggingManager, KernelDebugMode } from './debuggingTypes';
+
+/**
+ * Class that registers command handlers for interactive window commands.
+ */
+@injectable()
+export class CommandRegistry implements IDisposable, IExtensionSingleActivationService {
+    constructor(
+        @inject(INotebookDebuggingManager) private readonly debuggingManager: INotebookDebuggingManager,
+        @inject(IVSCodeNotebook) private readonly vscNotebook: IVSCodeNotebook,
+        @inject(IDisposableRegistry) private readonly disposables: IDisposableRegistry,
+        @inject(ICommandManager) private readonly commandManager: ICommandManager
+    ) {}
+
+    public async activate(): Promise<void> {
+        this.disposables.push(this.commandManager.registerCommand(Commands.RunByLine, this.runByLine, this));
+        this.disposables.push(this.commandManager.registerCommand(Commands.RunByLineNext, this.runByLineNext, this));
+        this.disposables.push(this.commandManager.registerCommand(Commands.RunByLineStop, this.runByLineStop, this));
+        this.disposables.push(
+            this.commandManager.registerCommand(Commands.RunAndDebugCell, this.runAndDebugCell, this)
+        );
+    }
+
+    private async runByLine(cell: NotebookCell | undefined) {
+        sendTelemetryEvent(DebuggingTelemetry.clickedRunByLine);
+        cell ??= this.getCellFromActiveEditor();
+        if (!cell) {
+            return;
+        }
+
+        await this.debuggingManager.tryToStartDebugging(KernelDebugMode.RunByLine, cell);
+    }
+
+    private async runByLineNext(cell: NotebookCell | undefined) {
+        cell ??= this.getCellFromActiveEditor();
+        if (!cell) {
+            return;
+        }
+
+        this.debuggingManager.runByLineNext(cell);
+    }
+
+    private async runByLineStop(cell: NotebookCell | undefined) {
+        cell ??= this.getCellFromActiveEditor();
+        if (!cell) {
+            return;
+        }
+
+        this.debuggingManager.runByLineStop(cell);
+    }
+
+    private async runAndDebugCell(cell: NotebookCell | undefined) {
+        sendTelemetryEvent(DebuggingTelemetry.clickedRunAndDebugCell);
+        cell ??= this.getCellFromActiveEditor();
+        if (!cell) {
+            return;
+        }
+
+        await this.debuggingManager.tryToStartDebugging(KernelDebugMode.Cell, cell);
+    }
+
+    private getCellFromActiveEditor(): NotebookCell | undefined {
+        const editor = this.vscNotebook.activeNotebookEditor;
+        if (editor) {
+            const range = editor.selections[0];
+            if (range) {
+                return editor.notebook.cellAt(range.start);
+            }
+        }
+    }
+
+    public dispose() {
+        this.disposables.forEach((d) => d.dispose());
+    }
+}
