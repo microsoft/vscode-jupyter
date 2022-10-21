cfbd057b2590cad215b27473070e11cc1d21666c
Rob Lourens <roblourens@gmail.com>
2022-10-20 18:32:06 -0700

    Run tests against latest insiders

--- src/test/standardTest.node.ts
+++ src/test/standardTest.node.ts
@@ -29,7 +29,7 @@ function requiresPythonExtensionToBeInstalled() {
 }
 
 const channel = (process.env.VSC_JUPYTER_CI_TEST_VSC_CHANNEL || '').toLowerCase().includes('insiders')
-    ? '6455bf1608dfa1b955717f66295f0f9200db0f73'
+    ? 'insiders'
     : 'stable';
 
 function computePlatform() {
