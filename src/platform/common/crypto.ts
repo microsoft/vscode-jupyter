9aad5a0c5b48fec0bbdc9c9b90906e77e4c0fcde
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-11-16 12:05:43 -0800

    Explicitly state computeHash type

--- src/platform/common/crypto.ts
+++ src/platform/common/crypto.ts
@@ -22,25 +22,21 @@ let stopStoringHashes = false;
 
 /**
  * Computes a hash for a give string and returns hash as a hex value.
- *
- * @param {string} data
- * @param {('SHA512' | 'SHA256' | 'SHA1')} algorithm
- * @return {*}
  */
-export async function computeHash(data: string, algorithm: 'SHA-512' | 'SHA-256' | 'SHA-1') {
+export async function computeHash(data: string, algorithm: 'SHA-512' | 'SHA-256' | 'SHA-1'): Promise<string> {
     // Save some CPU as this is called in a number of places.
     // This will not get too large, will only grow by number of files per workspace, even if user has
     // 1000s of files, this will not grow that large to cause any memory issues.
     // Files get hashed a lot in a number of places within the extension (.interactive is the IW window Uri).
     // Even things that include file paths like kernel id, which isn't a file path, but contains python executable path.
-    const isCandidateForCashing = data.includes('/') || data.includes('\\') || data.endsWith('.interactive');
-    if (isCandidateForCashing && computedHashes[data]) {
+    const isCandidateForCaching = data.includes('/') || data.includes('\\') || data.endsWith('.interactive');
+    if (isCandidateForCaching && computedHashes[data]) {
         return computedHashes[data];
     }
 
     const hash = await computeHashLib(data, algorithm);
 
-    if (isCandidateForCashing && !stopStoringHashes) {
+    if (isCandidateForCaching && !stopStoringHashes) {
         // Just a simple fail safe, why 10_000, simple why not 10_000
         // All we want to ensure is that we don't store too many hashes.
         // The only way we can get there is if user never closes VS Code and our code
