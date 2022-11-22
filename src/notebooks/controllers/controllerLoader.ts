c6dff96b7cd090c611407e649d6e1d4633f92a7d
Don Jayamanne <don.jayamanne@outlook.com>
2022-11-22 20:23:45 +1100

    Fix old kernel picker with remotes (#12136)
    * Fix remote tests
    
    * More comments
    
    * More fixes
    
    * Comments

--- src/notebooks/controllers/controllerLoader.ts
+++ src/notebooks/controllers/controllerLoader.ts
@@ -6,7 +6,7 @@ import { inject, injectable } from 'inversify';
 import * as vscode from 'vscode';
 import { isPythonNotebook } from '../../kernels/helpers';
 import { IJupyterServerUriStorage } from '../../kernels/jupyter/types';
-import { IKernelFinder, KernelConnectionMetadata } from '../../kernels/types';
+import { IKernelFinder, isLocalConnection, isRemoteConnection, KernelConnectionMetadata } from '../../kernels/types';
 import { IExtensionSyncActivationService } from '../../platform/activation/types';
 import { IPythonExtensionChecker } from '../../platform/api/types';
 import { IVSCodeNotebook } from '../../platform/common/application/types';
@@ -85,12 +85,6 @@ export class ControllerLoader implements IControllerLoader, IExtensionSyncActiva
                     this.interpreters,
                     this.registration
                 ).catch(noop);
-                createActiveInterpreterController(
-                    InteractiveWindowView,
-                    document.uri,
-                    this.interpreters,
-                    this.registration
-                ).catch(noop);
             }
         }
     }
@@ -107,12 +101,6 @@ export class ControllerLoader implements IControllerLoader, IExtensionSyncActiva
                 if (this.serverUriStorage.isLocalLaunch && !useNewKernelPicker) {
                     await createActiveInterpreterController(
                         JupyterNotebookView,
-                        undefined,
-                        this.interpreters,
-                        this.registration
-                    );
-                    await createActiveInterpreterController(
-                        InteractiveWindowView,
                         undefined,
                         this.interpreters,
                         this.registration
@@ -140,6 +128,20 @@ export class ControllerLoader implements IControllerLoader, IExtensionSyncActiva
                     return connection.id === controller.connection.id;
                 });
 
+                if (this.featuresManager.features.kernelPickerType !== 'Insiders') {
+                    if (this.serverUriStorage.isLocalLaunch && isRemoteConnection(controller.connection)) {
+                        traceVerbose(
+                            `Remote Controller ${controller.connection.kind}:'${controller.id}' for view = '${controller.viewType}' is no longer a valid as we are not connected to a remote server.`
+                        );
+                        return true;
+                    }
+                    if (!this.serverUriStorage.isLocalLaunch && isLocalConnection(controller.connection)) {
+                        traceVerbose(
+                            `Local Controller ${controller.connection.kind}:'${controller.id}' for view = '${controller.viewType}' is no longer a valid as we are connected to a remote server.`
+                        );
+                        return true;
+                    }
+                }
                 // Never remove remote kernels that don't exist.
                 // Always leave them there for user to select, and if the connection is not available/not valid,
                 // then notify the user and remove them.
@@ -152,7 +154,7 @@ export class ControllerLoader implements IControllerLoader, IExtensionSyncActiva
                     return false;
                 }
                 if (!connectionIsStillValid) {
-                    traceInfoIfCI(
+                    traceVerbose(
                         `Controller ${controller.connection.kind}:'${controller.id}' for view = '${controller.viewType}' is no longer a valid`
                     );
                 }
@@ -161,7 +163,7 @@ export class ControllerLoader implements IControllerLoader, IExtensionSyncActiva
 
             // If we have any out of date connections, dispose of them
             disposedControllers.forEach((controller) => {
-                traceInfoIfCI(
+                traceVerbose(
                     `Disposing old controller ${controller.connection.kind}:'${controller.id}' for view = '${controller.viewType}'`
                 );
                 controller.dispose(); // This should remove it from the registered list
