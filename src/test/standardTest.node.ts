889583de831c415b05232890446e51abfecb96d3
Don Jayamanne <don.jayamanne@outlook.com>
2022-12-06 03:42:19 +1100

    misc

--- src/test/standardTest.node.ts
+++ src/test/standardTest.node.ts
@@ -110,9 +110,7 @@ async function createSettings(): Promise<string> {
         // Disable the restart ask so that restart just happens
         'jupyter.askForKernelRestart': false,
         // To get widgets working.
-        'jupyter.widgetScriptSources': ['jsdelivr.com', 'unpkg.com'],
-        // New Kernel Picker.
-        'notebook.kernelPicker.type': 'mru'
+        'jupyter.widgetScriptSources': ['jsdelivr.com', 'unpkg.com']
     };
     fs.ensureDirSync(path.dirname(settingsFile));
     fs.writeFileSync(settingsFile, JSON.stringify(defaultSettings, undefined, 4));
