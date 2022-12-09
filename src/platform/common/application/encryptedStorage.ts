cf6dd16177e59229fe904d3218b30a1ff0c232be
Don Jayamanne <don.jayamanne@outlook.com>
2022-12-09 11:03:59 +1100

    Remove usage of secret storage

--- src/platform/common/application/encryptedStorage.ts
+++ src/platform/common/application/encryptedStorage.ts
@@ -1,11 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { inject, injectable } from 'inversify';
-import { ExtensionMode } from 'vscode';
-import { isCI } from '../constants';
-import { traceError } from '../../logging';
-import { IExtensionContext } from '../types';
+import { injectable } from 'inversify';
 import { IEncryptedStorage } from './types';
 
 /**
@@ -14,44 +10,12 @@ import { IEncryptedStorage } from './types';
  */
 @injectable()
 export class EncryptedStorage implements IEncryptedStorage {
-    constructor(@inject(IExtensionContext) private readonly extensionContext: IExtensionContext) {}
-
     private readonly testingState = new Map<string, string>();
 
     public async store(service: string, key: string, value: string | undefined): Promise<void> {
-        // On CI we don't need to use keytar for testing (else it hangs).
-        if (isCI && this.extensionContext.extensionMode !== ExtensionMode.Production) {
-            this.testingState.set(`${service}#${key}`, value || '');
-            return;
-        }
-
-        if (!value) {
-            try {
-                await this.extensionContext.secrets.delete(`${service}.${key}`);
-            } catch (e) {
-                traceError(e);
-            }
-        } else {
-            await this.extensionContext.secrets.store(`${service}.${key}`, value);
-        }
+        this.testingState.set(`${service}#${key}`, value || '');
     }
     public async retrieve(service: string, key: string): Promise<string | undefined> {
-        // On CI we don't need to use keytar for testing (else it hangs).
-        if (isCI && this.extensionContext.extensionMode !== ExtensionMode.Production) {
-            return this.testingState.get(`${service}#${key}`);
-        }
-        try {
-            // eslint-disable-next-line
-            const val = await this.extensionContext.secrets.get(`${service}.${key}`);
-            return val;
-        } catch (e) {
-            // If we get an error trying to get a secret, it might be corrupted. So we delete it.
-            try {
-                await this.extensionContext.secrets.delete(`${service}.${key}`);
-                return;
-            } catch (e) {
-                traceError(e);
-            }
-        }
+        return this.testingState.get(`${service}#${key}`);
     }
 }
