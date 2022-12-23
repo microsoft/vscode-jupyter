ef1564846c7a60b07c104abb0008777d50faa9b5
Don Jayamanne <don.jayamanne@outlook.com>
2022-12-23 13:59:29 +1100

    Remove dead code

--- src/platform/common/process/internal/python.node.ts
+++ src/platform/common/process/internal/python.node.ts
@@ -13,38 +13,11 @@
 // into the corresponding object or objects.  "parse()" takes a single
 // string as the stdout text and returns the relevant data.
 
-export function execCode(code: string): string[] {
-    let args = ['-c', code];
-    // "code" isn't specific enough to know how to parse it,
-    // so we only return the args.
-    return args;
-}
-
 export function execModule(name: string, moduleArgs: string[]): string[] {
     const args = ['-m', name, ...moduleArgs];
     // "code" isn't specific enough to know how to parse it,
     // so we only return the args.
     return args;
-}
-
-export function getVersion(): [string[], (out: string) => string] {
-    const args = ['--version'];
-
-    function parse(out: string): string {
-        return out.trim();
-    }
-
-    return [args, parse];
-}
-
-export function getSysPrefix(): [string[], (out: string) => string] {
-    const args = ['-c', 'import sys;print(sys.prefix)'];
-
-    function parse(out: string): string {
-        return out.trim();
-    }
-
-    return [args, parse];
 }
 
 export function getExecutable(): [string[], (out: string) => string] {
@@ -57,55 +30,12 @@ export function getExecutable(): [string[], (out: string) => string] {
     return [args, parse];
 }
 
-export function getSitePackages(): [string[], (out: string) => string] {
-    // On windows we also need the libs path (second item will
-    // return c:\xxx\lib\site-packages).  This is returned by
-    // the following: get_python_lib
-    const args = ['-c', 'from distutils.sysconfig import get_python_lib; print(get_python_lib())'];
-
-    function parse(out: string): string {
-        return out.trim();
-    }
-
-    return [args, parse];
-}
-
-export function getUserSitePackages(): [string[], (out: string) => string] {
-    const args = ['site', '--user-site'];
-
-    function parse(out: string): string {
-        return out.trim();
-    }
-
-    return [args, parse];
-}
-
-export function isValid(): [string[], (out: string) => boolean] {
-    const args = ['-c', 'print(1234)'];
-
-    function parse(out: string): boolean {
-        return out.startsWith('1234');
-    }
-
-    return [args, parse];
-}
-
 export function isModuleInstalled(name: string): [string[], (out: string) => boolean] {
     const args = ['-c', `import ${name}`];
 
     function parse(_out: string): boolean {
         // If the command did not fail then the module is installed.
         return true;
-    }
-
-    return [args, parse];
-}
-
-export function getModuleVersion(name: string): [string[], (out: string) => string] {
-    const args = ['-c', `import ${name}; print(${name}.__version__)`];
-
-    function parse(out: string): string {
-        return out.trim();
     }
 
     return [args, parse];
