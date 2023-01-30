3e3023d60546a2acdfb32f928257c6d1c635ff2b
Don Jayamanne <don.jayamanne@outlook.com>
2023-01-31 10:18:38 +1100

    ooops

--- src/kernels/execution/cellExecution.ts
+++ src/kernels/execution/cellExecution.ts
@@ -22,7 +22,7 @@ import { IDisposable } from '../../platform/common/types';
 import { createDeferred } from '../../platform/common/utils/async';
 import { StopWatch } from '../../platform/common/utils/stopWatch';
 import { noop } from '../../platform/common/utils/misc';
-import { getDisplayNameOrNameOfKernelConnection, isPythonKernelConnection } from '../../kernels/helpers';
+import { getDisplayNameOrNameOfKernelConnection } from '../../kernels/helpers';
 import { isCancellationError } from '../../platform/common/cancellation';
 import { activeNotebookCellExecution, CellExecutionMessageHandler } from './cellExecutionMessageHandler';
 import { CellExecutionMessageHandlerService } from './cellExecutionMessageHandlerService';
