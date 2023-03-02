c697672fe19129b88aaf56579bb793ecf430308c
Don Jayamanne <don.jayamanne@outlook.com>
2023-03-02 17:12:11 +1100

    Remove unwanted packages

--- src/test/common/asyncDump.ts
+++ src/test/common/asyncDump.ts
@@ -1,10 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-/* eslint-disable @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
-const log = require('why-is-node-running');
-
 // Call this function to debug async hangs. It should print out stack traces of still running promises.
 export function asyncDump() {
-    log();
+    require('why-is-node-running')();
 }
