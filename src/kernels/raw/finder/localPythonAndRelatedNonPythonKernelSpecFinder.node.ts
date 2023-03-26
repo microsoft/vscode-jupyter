0dc730da9df9c45c50807e320346b4e644b28094
Don Jayamanne <don.jayamanne@outlook.com>
2023-03-27 06:58:44 +1100

    Fix tests

--- src/kernels/raw/finder/localPythonAndRelatedNonPythonKernelSpecFinder.node.ts
+++ src/kernels/raw/finder/localPythonAndRelatedNonPythonKernelSpecFinder.node.ts
@@ -28,6 +28,7 @@ import {
     listPythonAndRelatedNonPythonKernelSpecs,
     localPythonKernelsCacheKey
 } from './interpreterKernelSpecFinderHelper.node';
+import { getDisplayPath } from '../../../platform/common/platform/fs-paths.node';
 
 type InterpreterId = string;
 
@@ -108,7 +109,8 @@ export class LocalPythonAndRelatedNonPythonKernelSpecFinder extends LocalKernelS
                 if (deletedKernels.length) {
                     traceVerbose(
                         `Local Python connection deleted ${deletedKernels.map(
-                            (item) => `${item.kind}:'${item.id}: (interpreter id=${item.interpreter?.id})'`
+                            (item) =>
+                                `${item.kind}:'${item.id}: (interpreter id=${getDisplayPath(item.interpreter?.id)})'`
                         )}`
                     );
                     this.updateCache().catch(noop);
@@ -229,7 +231,12 @@ export class LocalPythonAndRelatedNonPythonKernelSpecFinder extends LocalKernelS
                 if (kernelsThatPointToInvalidValidInterpreters.length) {
                     traceWarning(
                         `The following kernels use interpreters that are no longer valid or not recognized by Python extension, Kernels ${kernelsThatPointToInvalidValidInterpreters
-                            .map((item) => `${item.kind}:'id=${item.id}'(interpreterId='${item.interpreter?.id}')`)
+                            .map(
+                                (item) =>
+                                    `${item.kind}:'id=${item.id}'(interpreterId='${getDisplayPath(
+                                        item.interpreter?.id
+                                    )}')`
+                            )
                             .join(',')} and valid interpreter ids include ${Array.from(validInterpreterIds).join(', ')}`
                     );
                     kernelsThatPointToInvalidValidInterpreters.forEach((item) => {
@@ -241,7 +248,8 @@ export class LocalPythonAndRelatedNonPythonKernelSpecFinder extends LocalKernelS
                 if (deletedKernels.length) {
                     traceVerbose(
                         `Local Python connection deleted ${deletedKernels.map(
-                            (item) => `${item.kind}:'${item.id}: (interpreter id=${item.interpreter?.id})'`
+                            (item) =>
+                                `${item.kind}:'${item.id}: (interpreter id=${getDisplayPath(item.interpreter?.id)})'`
                         )}`
                     );
                     await this.updateCache();
