0a1373e3380047fd27bcf8138d1bdba7495644bd
Don Jayamanne <don.jayamanne@outlook.com>
2023-03-30 10:43:39 +1100

    fix formatting

--- src/test/standardTest.node.ts
+++ src/test/standardTest.node.ts
@@ -167,7 +167,7 @@ async function start() {
             .concat(['--enable-proposed-api'])
             .concat(['--timeout', '5000'])
             .concat(['--disable-extension', 'ms-python.isort']) // We don't need this, also has a lot of errors on CI and floods CI logs unnecessarily.
-            .concat(IS_SMOKE_TEST() ? ['--disable-extension', 'ms-python.vscode-pylance']: []) // For some reason pylance crashes and takes down the entire test run. See https://github.com/microsoft/vscode-jupyter/issues/13200
+            .concat(IS_SMOKE_TEST() ? ['--disable-extension', 'ms-python.vscode-pylance'] : []) // For some reason pylance crashes and takes down the entire test run. See https://github.com/microsoft/vscode-jupyter/issues/13200
             .concat(['--extensions-dir', extensionsDir])
             .concat(['--user-data-dir', userDataDirectory]),
         // .concat(['--verbose']), // Too much logging from VS Code, enable this to see what's going on in VSC.
