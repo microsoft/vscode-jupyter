e7b3f9ceeb4120b4a2fe9667568313c43784965a
Don Jayamanne <don.jayamanne@outlook.com>
2023-05-05 12:01:22 +1000

    wip

--- src/kernels/raw/finder/contributedKerneFinder.node.unit.test.ts
+++ src/kernels/raw/finder/contributedKerneFinder.node.unit.test.ts
@@ -883,6 +883,12 @@ import { IPythonExecutionService, IPythonExecutionFactory } from '../../../platf
             suite(
                 activePythonEnv ? `With active Python (${activePythonEnv?.displayName})` : 'without active Python',
                 () => {
+                    setup(function () {
+                        // Flaky windows unit tests. https://github.com/microsoft/vscode-jupyter/issues/13462
+                        if (os.platform() === 'win32' && !activePythonEnv) {
+                            return this.skip();
+                        }
+                    });
                     /**
                      * As we're using a push model, we need to wait for the events to get triggered.
                      * How many events do we need to wait for is not deterministic (well for tests it is, but its too complex).
