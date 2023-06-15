40c61e14f2013b22e0cc2c8a699bb5cd3c4b3392
Don Jayamanne <don.jayamanne@outlook.com>
2023-06-16 07:39:45 +1000

    Clear remote Uris entered by user (#13715)
    For https://github.com/microsoft/vscode-jupyter/issues/13693

--- src/kernels/jupyter/clearJupyterServersCommand.ts
+++ src/kernels/jupyter/clearJupyterServersCommand.ts
@@ -38,6 +38,9 @@ export class ClearJupyterServersCommand implements IExtensionSyncActivationServi
                                 }
                             })
                     ).catch(noop);
+                    await this.commandManager
+                        .executeCommand('dataScience.ClearUserProviderJupyterServerCache')
+                        .then(noop, noop);
                 },
                 this
             )
