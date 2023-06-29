c5549eec25f2064df0d52a7b4a48c5882eaa1141
Don Jayamanne <don.jayamanne@outlook.com>
2023-06-30 07:36:13 +1000

    Update to allow API Access

--- src/standalone/api/apiAccessService.ts
+++ src/standalone/api/apiAccessService.ts
@@ -24,7 +24,8 @@ export const TrustedExtensionPublishers = new Set([
     JVSC_EXTENSION_ID.split('.')[0],
     'rchiodo',
     'donjayamanne',
-    'SynapseVSCode'
+    'SynapseVSCode',
+    'nuant'
 ]);
 
 // We dont want to expose this API to everyone, else we'll keep track of who has access to this.
