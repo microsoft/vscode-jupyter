6161a3c02559ec57abeef20e99690726cdaf1925
Don Jayamanne <don.jayamanne@outlook.com>
2023-07-19 10:29:11 +1000

    wip

--- src/proposed.api.d.ts
+++ src/proposed.api.d.ts
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { Disposable, Event, Uri } from 'vscode';
+import { IKernelConnection } from '@jupyterlab/services/lib/kernel/kernel';
+import { Disposable, Event, Uri, extensions } from 'vscode';
 
-export interface JupyterServerAuthenticationInformation {
+export interface JupyterServerConnectionInformation {
     readonly baseUrl: Uri;
     /**
      * Jupyter auth Token
@@ -14,6 +15,22 @@ export interface JupyterServerAuthenticationInformation {
      */
     readonly authorizationHeader?: Record<string, string>;
     /**
+     * The local directory that maps to the remote directory of the Jupyter Server.
+     * E.g. assume you start Jupyter Notebook with --notebook-dir=/foo/bar,
+     * and you have a file named /foo/bar/sample.ipynb, /foo/bar/sample2.ipynb and the like.
+     * Then assume the mapped local directory will be /users/xyz/remoteServer and the files sample.ipynb and sample2.ipynb
+     * are in the above local directory.
+     *
+     * Using this setting one can map the local directory to the remote directory.
+     * In this case the value of this property would be /users/xyz/remoteServer.
+     *
+     * Note: A side effect of providing this value is the session names are generated the way they are in Jupyter Notebook/Lab.
+     * I.e. the session names map to the relative path of the notebook file.
+     * As a result when attempting to create a new session for a notebook/file, Jupyter will
+     * first check if a session already exists for the same file and same kernel, and if so, will re-use that session.
+     */
+    readonly mappedRemoteNotebookDir?: string;
+    /**
      * Returns the sub-protocols to be used. See details of `protocols` here https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/WebSocket
      * Useful if there is a custom authentication scheme that needs to be used for WebSocket connections.
      * Note: The client side npm package @jupyterlab/services uses WebSockets to connect to remote Kernels.
@@ -21,41 +38,16 @@ export interface JupyterServerAuthenticationInformation {
     readonly webSocketProtocols?: string[];
 }
 
-export type GetAuthenticationInformation = () => Promise<JupyterServerAuthenticationInformation>;
-/**
- * The local directory that maps to the remote directory of the Jupyter Server.
- * E.g. assume you start Jupyter Notebook with --notebook-dir=/foo/bar,
- * and you have a file named /foo/bar/sample.ipynb, /foo/bar/sample2.ipynb and the like.
- * Then assume the mapped local directory will be /users/xyz/remoteServer and the files sample.ipynb and sample2.ipynb
- * are in the above local directory.
- *
- * Using this setting one can map the local directory to the remote directory.
- * In this case the value of this property would be /users/xyz/remoteServer.
- *
- * Note: A side effect of providing this value is the session names are generated the way they are in Jupyter Notebook/Lab.
- * I.e. the session names map to the relative path of the notebook file.
- * As a result when attempting to create a new session for a notebook/file, Jupyter will
- * first check if a session already exists for the same file and same kernel, and if so, will re-use that session.
- */
-export type GetRemoteNotebookDirectoryMapping = () => string;
-
 export interface JupyterServer extends Disposable {
-    /**
-     * Triggered when the server is removed by the user.
-     */
-    readonly onDidRemove: Event<void>;
     readonly id: string;
     /**
      * A human-readable string which is rendered prominent.
      */
     label: string;
-    /**
-     * Date time when the user last ran some code against a kernel on this server.
-     */
-    readonly lastActivity?: Date;
+    resolveConnectionInformation: () => Promise<JupyterServerConnectionInformation>;
 }
 
-export class JupyterServerPicker extends Disposable {
+export class JupyterServerCreationItem extends Disposable {
     /**
      * A human-readable string which is rendered prominent. Supports rendering of {@link ThemeIcon theme icons} via
      * the `$(<name>)`-syntax.
@@ -68,7 +60,7 @@ export class JupyterServerPicker extends Disposable {
     detail?: string;
     /**
      * A string that should be used when comparing this item
-     * with other items. When `falsy` the {@link JupyterServerPicker.label label}
+     * with other items. When `falsy` the {@link JupyterServerCreationItem.label label}
      * is used.
      */
     sortText?: string;
@@ -95,28 +87,17 @@ export class JupyterServerCollection extends Disposable {
      */
     documentation?: Uri;
     /**
-     * An event signaling when the value of the filter text has changed.
-     */
-    readonly onDidChangeValue: Event<string>;
-
-    /**
      * Creates an entry in the list of Jupyter Servers from which the user can pick.
      *
      * @param {string} id
      * @param {string} label
-     * @param {GetAuthenticationInformation} resolveAuthenticationInformation Gets the authentication information for the Jupyter Server.
-     * @param {GetRemoteNotebookDirectoryMapping} [getDirectoryMapping] Gets the mapping of the notebook directory.
+     * @param {GetConnectionInformation} resolveConnectionInformation Gets the connection information for the Jupyter Server.
      * @return {*}  {JupyterServer}
      * @memberof JupyterServerCollection
      */
-    createServer(
-        id: string,
-        label: string,
-        resolveAuthenticationInformation: GetAuthenticationInformation,
-        getDirectoryMapping?: GetRemoteNotebookDirectoryMapping
-    ): JupyterServer;
+    createServer(id: string, label: string, resolveConnectionInformation: () => Promise<JupyterServerConnectionInformation>): JupyterServer;
     /**
-     * Creates an entry in the list of Jupyter Servers from which a user can pick.
+     * Creates an entry in the list of Jupyter Servers from which a user can pick to create a server.
      * Picking an item is expected to result in the eventual creation of a JupyterServer.
      * I.e. an extension is expected to listen to the `onDidSelect` event and optionally display their own UI and then create a JupyterServer.
      *
@@ -125,7 +106,7 @@ export class JupyterServerCollection extends Disposable {
      * @return {*}  {JupyterServerPicker}
      * @memberof JupyterServerCollection
      */
-    createServerPicker(label: string, onDidSelect: () => PromiseLike<JupyterServer>): JupyterServerPicker;
+    createServerCreationItem(label: string, onDidSelect: () => PromiseLike<JupyterServer>): JupyterServerCreationItem;
 }
 
 export interface JupyterAPI {
@@ -134,3 +115,4 @@ export interface JupyterAPI {
      */
     createServerCollection(id: string, label: string): JupyterServerCollection;
 }
+
