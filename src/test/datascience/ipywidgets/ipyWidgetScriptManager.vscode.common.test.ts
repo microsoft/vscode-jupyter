d2569f177ad9d69a6f17b8e743596509c914650c
Don Jayamanne <don.jayamanne@outlook.com>
2023-08-28 11:38:21 +1000

    misc

--- src/test/datascience/ipywidgets/ipyWidgetScriptManager.vscode.common.test.ts
+++ src/test/datascience/ipywidgets/ipyWidgetScriptManager.vscode.common.test.ts
@@ -168,7 +168,8 @@ suite('IPyWidget Script Manager @widgets', function () {
         const newFiles = (await fs.getFiles(nbExtensionsFolder)).map((item) => item.toString());
         assert.deepEqual(newFiles, files);
     });
-    test('Get a list of Widgets and script paths', async () => {
+    // eslint-disable-next-line no-only-tests/no-only-tests
+    test.only('Get a list of Widgets and script paths', async () => {
         const baseUrl = await scriptManager.getBaseUrl!()!;
         const moduleMappings = await scriptManager.getWidgetModuleMappings();
 
