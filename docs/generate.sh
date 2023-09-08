8ba4c596f07b379068e654da5814d8c592f34b45
Don Jayamanne <don.jayamanne@outlook.com>
2023-09-08 14:02:12 +1000

    Remove unwanted files

--- docs/generate.sh
+++
@@ -1,6 +0,0 @@
-for dir in src/client/datascience/*/
-do
-  dirSub=${dir:23:-1}
-  echo "Running dependency analysis on $dirSub"
-  npx dependency-cruise -X "^node_modules" -x "^(src/test|node_modules|src/datascience-ui)" --prefix "https://github.com/Microsoft/vscode-jupyter/blob/main/" -p -I "^src/client/datascience/$dirSub" -T dot src | dot -T svg | depcruise-wrap-stream-in-html > docs/$dirSub.html
-done
