6445a5032497808edc73a015c6ac5c17032780c9
Don Jayamanne <don.jayamanne@outlook.com>
2023-09-11 12:20:55 +1000

    Ability to publish npm package

---
+++ api/build.js
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const fs = require('fs');
+const path = require('path');
+console.error('copying api.d.ts');
+fs.copyFileSync(path.join(__dirname, '../src/api.d.ts'), path.join(__dirname, 'api.d.ts'));
+
+if (process.env.npm_config_tag === 'proposed') {
+    fs.readdirSync(path.join(__dirname, '../src')).forEach((file) => {
+        if (file.startsWith('api.proposed.') && file.endsWith('.d.ts')) {
+            fs.copyFileSync(path.join(__dirname, '../src', file), path.join(__dirname, `.${file}`));
+        }
+    });
+}
--- api/stable.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-const fs = require('fs');
-const path = require('path');
-
-fs.copyFileSync(path.join(__dirname, '../src/api.d.ts'), path.join(__dirname, '.api.d.ts'));
