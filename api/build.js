b8f31abc1c6a056d7e23020eac00ca37005f2958
Don Jayamanne <don.jayamanne@outlook.com>
2023-09-11 13:53:57 +1000

    Misc

--- api/build.js
+++ api/build.js
@@ -1,15 +1,40 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+const { EOL } = require('os');
 const fs = require('fs');
 const path = require('path');
-console.error('copying api.d.ts');
+
 fs.copyFileSync(path.join(__dirname, '../src/api.d.ts'), path.join(__dirname, 'api.d.ts'));
 
 if (process.env.npm_config_tag === 'proposed') {
     fs.readdirSync(path.join(__dirname, '../src')).forEach((file) => {
         if (file.startsWith('api.proposed.') && file.endsWith('.d.ts')) {
-            fs.copyFileSync(path.join(__dirname, '../src', file), path.join(__dirname, file));
+            const source = fs.readFileSync(path.join(__dirname, '../src', file), 'utf8').toString();
+            fs.writeFileSync(
+                path.join(__dirname, file),
+                source.replace(`declare module './api' {`, `declare module '@vscode/jupyter-extension' {`)
+            );
         }
     });
+
+    // Add module namespace to the main api.d.ts file.
+    // Required for module augmentation to work.
+    const source = fs.readFileSync(path.join(__dirname, 'api.d.ts'), 'utf8').toString();
+    let foundFirstExport = false;
+    const tab = '    '; // 4 spaces used as tab size in source formatting.
+    const newSource = source
+        .split(/\r?\n/g)
+        .map((line) => {
+            if (line.startsWith('export ') && !foundFirstExport) {
+                foundFirstExport = true;
+                return [`declare module '@vscode/jupyter-extension' {`, `${tab}${line}`].join(EOL);
+            }
+            if (foundFirstExport) {
+                `${tab}${line}`;
+            }
+            return line;
+        })
+        .join(EOL);
+    fs.writeFileSync(path.join(__dirname, 'api.d.ts'), newSource.trim() + EOL + '}' + EOL);
 }
