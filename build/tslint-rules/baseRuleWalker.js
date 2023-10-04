5202958d89de53048726d89c1545a1185a14399f
Don Jayamanne <don.jayamanne@outlook.com>
2023-10-04 22:22:16 +1100

    Revert "Remove unwanted files (#14416)"
    This reverts commit bb016dfbcdbed5e3310b0834057efda4f482da6b.

---
+++ build/tslint-rules/baseRuleWalker.js
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+'use strict';
+
+const path = require('path');
+const Lint = require('tslint');
+const util = require('../util');
+class BaseRuleWalker extends Lint.RuleWalker {
+    shouldIgnoreCurrentFile(node, filesToIgnore) {
+        const sourceFile = node.getSourceFile();
+        if (sourceFile && sourceFile.fileName) {
+            const filename = path.resolve(util.ExtensionRootDir, sourceFile.fileName);
+            if (filesToIgnore.indexOf(filename.replace(/\//g, path.sep)) >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+exports.BaseRuleWalker = BaseRuleWalker;
