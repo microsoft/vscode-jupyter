87b3902a11db2a30ff6df643ee9cf172d1860446
Don Jayamanne <don.jayamanne@outlook.com>
2023-10-06 17:08:47 +1100

    Fix data explorer to use esbuild as well

---
+++ build/esbuild/jquery.js
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// From old webpack, requried in data-explorer.
+const jquery = require('slickgrid/lib/jquery-1.11.2.min');
+window.jQuery = jquery;
+window.$ = jquery;
--- build/webpack/webpack.datascience-ui-viewers.config.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-'use strict';
-
-const builder = require('./webpack.datascience-ui.config.builder');
-module.exports = [builder.viewers];
