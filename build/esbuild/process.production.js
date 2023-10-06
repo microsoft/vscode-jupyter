fcc54550d2dd10aac086c51af266d76480ae193a
Don Jayamanne <don.jayamanne@outlook.com>
2023-10-06 13:46:09 +1100

    Use esbuild for bundling widget scripts

---
+++ build/esbuild/process.production.js
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// Dummy global process variable for use in webviews, such as IPyWidgets kernel and renderers
+// We cannot use the esbuild CLI to inject these, because some code checks for the existence of process.env.XYZ
+// And its not possible in CLI to have an undefined define value for these.
+export var process = {
+    env: {
+        NODE_ENV: 'production'
+    }
+};
--- build/webpack/webpack.datascience-ui.config.builder.js
+++ build/webpack/webpack.datascience-ui.config.builder.js
@@ -3,8 +3,6 @@
 
 'use strict';
 
-// Note to editors, if you change this file you have to restart compile-webviews.
-// It doesn't reload the config otherwise.
 const common = require('./common');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -15,6 +13,7 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 // Any build on the CI is considered production mode.
 const isProdBuild = constants.isCI || process.argv.includes('--mode');
+const isWatchMode = process.argv.includes('--watch');
 const fasterCompiler = !!process.env.CI_JUPYTER_FAST_COMPILATION;
 
 function getEntry(bundle) {
@@ -24,19 +23,6 @@ function getEntry(bundle) {
                 plotViewer: ['babel-polyfill', `./src/webviews/webview-side/plot/index.tsx`],
                 dataExplorer: ['babel-polyfill', `./src/webviews/webview-side/data-explorer/index.tsx`],
                 variableView: ['babel-polyfill', `./src/webviews/webview-side/variable-view/index.tsx`]
-            };
-        case 'ipywidgetsKernel':
-            return {
-                ipywidgetsKernel: [`./src/webviews/webview-side/ipywidgets/kernel/index.ts`]
-            };
-        case 'ipywidgetsRenderer':
-            // This is only used in tests (not shipped with extension).
-            return {
-                ipywidgetsRenderer: [`./src/webviews/webview-side/ipywidgets/renderer/index.ts`]
-            };
-        case 'widgetTester':
-            return {
-                widgetTester: [`./src/test/datascience/widgets/rendererUtils.ts`]
             };
         default:
             throw new Error(`Bundle not supported ${bundle}`);
@@ -93,15 +79,6 @@ function getPlugins(bundle) {
                     })
                 ]
             );
-            break;
-        }
-        case 'widgetTester': {
-            plugins.push(definePlugin);
-            break;
-        }
-        case 'ipywidgetsKernel':
-        case 'ipywidgetsRenderer': {
-            plugins.push(definePlugin);
             break;
         }
         default:
@@ -164,7 +141,7 @@ function buildConfiguration(bundle) {
                                 // there should be 1 cpu for the fork-ts-checker-webpack-plugin
                                 workers: require('os').cpus().length - 1,
                                 workerNodeArgs: ['--max-old-space-size=9096'],
-                                poolTimeout: isProdBuild ? 1000 : Infinity // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
+                                poolTimeout: isWatchMode ? Infinity : 1000 // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
                             }
                         },
                         {
@@ -248,6 +225,3 @@ function buildConfiguration(bundle) {
 }
 
 exports.viewers = buildConfiguration('viewers');
-exports.ipywidgetsKernel = buildConfiguration('ipywidgetsKernel');
-exports.ipywidgetsRenderer = buildConfiguration('ipywidgetsRenderer');
-exports.widgetTester = buildConfiguration('widgetTester');
