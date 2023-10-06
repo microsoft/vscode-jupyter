b671fc9db0ee6deecda8400e65018d65700ca698
Don Jayamanne <don.jayamanne@outlook.com>
2023-10-06 11:21:29 +1100

    Remove chaining execution

--- src/kernels/common/chainingExecuteRequester.ts
+++ src/kernels/common/chainingExecuteRequester.ts
@@ -3,15 +3,15 @@
 
 import type { JSONObject } from '@lumino/coreutils';
 import type { Kernel, KernelMessage } from '@jupyterlab/services';
-import { DelayedFutureExecute } from './delayedFutureExecute';
+// import { DelayedFutureExecute } from './delayedFutureExecute';
 
 // Class that makes sure when doing a requestExecute on an IKernelConnection, that only one request happens
 // at a time.
 export class ChainingExecuteRequester {
-    private previousExecute:
-        | Kernel.IShellFuture<KernelMessage.IExecuteRequestMsg, KernelMessage.IExecuteReplyMsg>
-        | undefined;
-    private previousKernel: Kernel.IKernelConnection | undefined;
+    // private previousExecute:
+    //     | Kernel.IShellFuture<KernelMessage.IExecuteRequestMsg, KernelMessage.IExecuteReplyMsg>
+    //     | undefined;
+    // private previousKernel: Kernel.IKernelConnection | undefined;
     public requestExecute(
         kernel: Kernel.IKernelConnection,
         content: KernelMessage.IExecuteRequestMsg['content'],
@@ -25,27 +25,28 @@ export class ChainingExecuteRequester {
         if (!content.store_history) {
             return kernel.requestExecute(content, disposeOnDone, metadata);
         }
-        // Wrap execute in a delay so we don't queue up more than one of these at a time.
-        // Make sure for same kernel though. Otherwise the previous execute may never return.
-        const nextExecute =
-            this.previousExecute && this.previousKernel?.id === kernel.id
-                ? new DelayedFutureExecute(kernel, this.previousExecute, content, disposeOnDone, metadata)
-                : kernel.requestExecute(content, disposeOnDone, metadata);
-        this.previousExecute = nextExecute;
-        this.previousKernel = kernel;
-        nextExecute.done
-            .then(() => {
-                if (this.previousExecute == nextExecute) {
-                    this.previousExecute = undefined;
-                    this.previousKernel = undefined;
-                }
-            })
-            .catch(() => {
-                if (this.previousExecute == nextExecute) {
-                    this.previousExecute = undefined;
-                    this.previousKernel = undefined;
-                }
-            });
-        return nextExecute;
+        return kernel.requestExecute(content, disposeOnDone, metadata);
+        // // Wrap execute in a delay so we don't queue up more than one of these at a time.
+        // // Make sure for same kernel though. Otherwise the previous execute may never return.
+        // const nextExecute =
+        //     this.previousExecute && this.previousKernel?.id === kernel.id
+        //         ? new DelayedFutureExecute(kernel, this.previousExecute, content, disposeOnDone, metadata)
+        //         : kernel.requestExecute(content, disposeOnDone, metadata);
+        // this.previousExecute = nextExecute;
+        // this.previousKernel = kernel;
+        // nextExecute.done
+        //     .then(() => {
+        //         if (this.previousExecute == nextExecute) {
+        //             this.previousExecute = undefined;
+        //             this.previousKernel = undefined;
+        //         }
+        //     })
+        //     .catch(() => {
+        //         if (this.previousExecute == nextExecute) {
+        //             this.previousExecute = undefined;
+        //             this.previousKernel = undefined;
+        //         }
+        //     });
+        // return nextExecute;
     }
 }
