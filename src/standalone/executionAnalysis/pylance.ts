b5a9010d3af8d09c76e1a85e5d0b7143e14ddb86
Peng Lyu <penn.lv@gmail.com>
2023-10-13 13:04:22 -0700

    Experimental cell execution analysis. (#14507)

---
+++ src/standalone/executionAnalysis/pylance.ts
@@ -0,0 +1,87 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as vscode from 'vscode';
+import { BaseLanguageClient } from 'vscode-languageclient';
+import { LocationWithReferenceKind, PylanceExtension, noop } from './common';
+
+export interface ILanguageServerFolder {
+    path: string;
+    version: string; // SemVer, in string form to avoid cross-extension type issues.
+}
+
+export interface INotebookLanguageClient {
+    registerJupyterPythonPathFunction(func: (uri: vscode.Uri) => Promise<string | undefined>): void;
+    registerGetNotebookUriForTextDocumentUriFunction(
+        func: (textDocumentUri: vscode.Uri) => vscode.Uri | undefined
+    ): void;
+    getCompletionItems(
+        document: vscode.TextDocument,
+        position: vscode.Position,
+        context: vscode.CompletionContext,
+        token: vscode.CancellationToken
+    ): Promise<vscode.CompletionItem[] | vscode.CompletionList | undefined>;
+    getReferences(
+        textDocument: vscode.TextDocument,
+        position: vscode.Position,
+        options: {
+            includeDeclaration: boolean;
+        },
+        token: vscode.CancellationToken
+    ): Promise<LocationWithReferenceKind[] | null | undefined>;
+}
+
+export interface LSExtensionApi {
+    languageServerFolder?(): Promise<ILanguageServerFolder>;
+    client?: {
+        isEnabled(): boolean;
+        start(): Promise<void>;
+        stop(): Promise<void>;
+    };
+    notebook?: INotebookLanguageClient;
+}
+
+export interface PythonApi {
+    readonly pylance?: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        createClient(...args: any[]): BaseLanguageClient;
+        start(client: BaseLanguageClient): Promise<void>;
+        stop(client: BaseLanguageClient): Promise<void>;
+    };
+}
+
+export async function runPylance(pylanceExtension: vscode.Extension<LSExtensionApi>) {
+    const pylanceApi = await pylanceExtension.activate();
+    return pylanceApi;
+}
+
+let _client: INotebookLanguageClient | undefined;
+export async function activatePylance(): Promise<INotebookLanguageClient | undefined> {
+    const pylanceExtension = vscode.extensions.getExtension(PylanceExtension);
+    if (!pylanceExtension) {
+        return undefined;
+    }
+
+    if (_client) {
+        return _client;
+    }
+
+    return new Promise((resolve, reject) => {
+        runPylance(pylanceExtension)
+            .then(async (client) => {
+                if (!client) {
+                    vscode.window.showErrorMessage('Could not start Pylance').then(noop, noop);
+                    reject();
+                    return;
+                }
+
+                if (client.client) {
+                    await client.client.start();
+                }
+
+                _client = client.notebook;
+                resolve(client.notebook);
+            })
+            .then(noop, noop);
+    });
+}
