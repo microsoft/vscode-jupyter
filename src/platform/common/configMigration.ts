a5bf9e1c6bbc188fa22bf79b0d1b74d97ae94361
Aaron Munger <aamunger@microsoft.com>
2023-11-03 15:45:26 -0700

    fix setting name

--- src/platform/common/configMigration.ts
+++ src/platform/common/configMigration.ts
@@ -10,6 +10,7 @@ export class ConfigMigration {
     public static readonly migratedSettings: Record<string, string> = {
         interactiveWindowMode: 'interactiveWindow.creationMode',
         interactiveWindowViewColumn: 'interactiveWindow.viewColumn',
+        splitRunFileIntoCells: 'interactiveWindow.splitRunFileIntoCells',
 
         sendSelectionToInteractiveWindow: 'interactiveWindow.textEditor.executeSelection',
         normalizeSelectionForInteractiveWindow: 'interactiveWindow.textEditor.normalizeSelection',
@@ -17,7 +18,6 @@ export class ConfigMigration {
         enableAutoMoveToNextCell: 'interactiveWindow.textEditor.autoMoveToNextCell',
         newCellOnRunLast: 'interactiveWindow.textEditor.autoAddNewCell',
         pythonCellFolding: 'interactiveWindow.textEditor.cellFolding',
-        splitRunFileIntoCells: 'jupyter.interactiveWindow.splitRunFileIntoCells',
 
         enableCellCodeLens: 'interactiveWindow.codeLens.enable',
         addGotoCodeLenses: 'interactiveWindow.codeLens.enableGotoCell',
