c4e56fd462af6bbdfef03b8d0df25fdfbdd5700e
Aaron Munger <aamunger@microsoft.com>
2023-11-06 11:54:13 -0800

    fix migrated setting check

--- src/platform/common/configSettings.ts
+++ src/platform/common/configSettings.ts
@@ -217,7 +217,7 @@ export class JupyterSettings implements IWatchableJupyterSettings {
             // Configuration migration is asyncronous, so check the old configuration key if the new one isn't set
             const configValue = newKey && config.get(newKey) !== undefined ? config.get(newKey) : config.get(k);
             const val = systemVariables.resolveAny(configValue);
-            if (val) {
+            if (val !== undefined) {
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 (<any>this)[k] = val;
             }
