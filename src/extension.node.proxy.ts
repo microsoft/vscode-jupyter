47537f2bbde7b9375b70d512cc28a69466be871e
Don Jayamanne <don.jayamanne@outlook.com>
2023-11-13 14:10:31 +1100

    Address codeql issue

--- src/extension.node.proxy.ts
+++ src/extension.node.proxy.ts
@@ -12,12 +12,12 @@ let realEntryPoint: {
 export async function activate(context: IExtensionContext): Promise<IExtensionApi> {
     const entryPoint = context.extensionMode === ExtensionMode.Test ? '../out/extension.node' : './extension.node';
     try {
-        realEntryPoint = eval('require')(entryPoint);
+        realEntryPoint = eval('require')(entryPoint); // CodeQL [SM04509] Usage of eval in this context is safe (we do not want bundlers to import code when it sees `require`).
         return realEntryPoint.activate(context);
     } catch (ex) {
         console.error('Failed to activate extension, falling back to `./extension.node`', ex);
         // In smoke tests, we do not want to load the out/extension.node.
-        realEntryPoint = eval('require')('./extension.node');
+        realEntryPoint = eval('require')('./extension.node'); // CodeQL [SM04509] Usage of eval in this context is safe (we do not want bundlers to import code when it sees `require`)
         return realEntryPoint.activate(context);
     }
 }
