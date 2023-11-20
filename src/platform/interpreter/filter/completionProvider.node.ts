ef602a0480c57e4509e2b0a1c71a34b07c4b1089
Don Jayamanne <don.jayamanne@outlook.com>
2023-11-20 13:40:23 +1100

    Do not display envs already excluded via settings

--- src/platform/interpreter/filter/completionProvider.node.ts
+++ src/platform/interpreter/filter/completionProvider.node.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import { inject, injectable } from 'inversify';
-import { getLocation } from 'jsonc-parser';
+import { findNodeAtLocation, getLocation, getNodeValue, parseTree } from 'jsonc-parser';
 import { CancellationToken, CompletionItem, CompletionItemProvider, Position, TextDocument, languages } from 'vscode';
 import { IExtensionSyncActivationService } from '../../activation/types';
 import { IDisposableRegistry } from '../../common/types';
@@ -11,6 +11,8 @@ import { IInterpreterService } from '../contracts';
 import { IPythonExtensionChecker } from '../../api/types';
 import { getDisplayPath } from '../../common/platform/fs-paths';
 import { getPythonEnvDisplayName } from '../helpers';
+import { isPythonEnvInListOfHiddenEnvs } from './filterService';
+import { traceWarning } from '../../logging';
 
 @injectable()
 export class PythonEnvFilterCompletionProvider implements CompletionItemProvider, IExtensionSyncActivationService {
@@ -41,16 +43,24 @@ export class PythonEnvFilterCompletionProvider implements CompletionItemProvider
             return [];
         }
 
-        return this.interpreters.resolvedEnvironments.map((env) => {
-            const label = getPythonEnvDisplayName(env);
-            const envPath = getDisplayPath(env.uri);
-            return {
-                label,
-                detail: envPath,
-                insertText: `"${envPath.replace(/\\/g, '\\\\')}"`,
-                filterText: `${label} ${envPath}`
-            };
-        });
+        // If an environment has already been added into the settings,
+        // Then no point displaying that env again in the list of completions.
+        const currentItems = this.getCurrentItemsInList(document, position);
+
+        return this.interpreters.resolvedEnvironments
+            .filter((env) => {
+                return !isPythonEnvInListOfHiddenEnvs(env, currentItems);
+            })
+            .map((env) => {
+                const label = getPythonEnvDisplayName(env);
+                const envPath = getDisplayPath(env.uri);
+                return {
+                    label,
+                    detail: envPath,
+                    insertText: `"${envPath.replace(/\\/g, '\\\\')}"`,
+                    filterText: `${label} ${envPath}`
+                };
+            });
     }
 
     public static canProvideCompletions(document: TextDocument, position: Position): boolean {
@@ -61,5 +71,21 @@ export class PythonEnvFilterCompletionProvider implements CompletionItemProvider
         // Cursor must be inside the configurations array and not in any nested items.
         // Hence path[0] = array, path[1] = array element index.
         return location.path[0] === 'jupyter.kernels.excludePythonEnvironments' && location.path.length === 2;
+    }
+    private getCurrentItemsInList(document: TextDocument, position: Position): string[] {
+        try {
+            const settings = document.getText();
+            const location = getLocation(settings, document.offsetAt(position));
+            const root = parseTree(settings);
+            const settingsNode = findNodeAtLocation(root, [location.path[0]]);
+            if (!settingsNode) {
+                return [];
+            }
+            const items = getNodeValue(settingsNode) as string[];
+            return Array.isArray(items) ? items : [];
+        } catch (ex) {
+            traceWarning(`Failed to provide completions for python env filter`, ex);
+            return [];
+        }
     }
 }
