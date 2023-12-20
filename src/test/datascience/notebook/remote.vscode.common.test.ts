efa9b74c9bdcf26b030410756bde1e3253e09fe3
Don Jayamanne <don.jayamanne@outlook.com>
2023-12-20 16:53:15 +1100

    Remove

--- src/test/datascience/notebook/remote.vscode.common.test.ts
+++ src/test/datascience/notebook/remote.vscode.common.test.ts
@@ -25,7 +25,6 @@ import {
     waitForExecutionCompletedSuccessfully,
     waitForTextOutput
 } from '../notebook/helper';
-import { IS_REMOTE_NATIVE_TEST } from '../../constants';
 import { isWeb } from '../../../platform/common/utils/misc';
 
 suiteMandatory('Remote Tests', function () {
@@ -35,7 +34,7 @@ suiteMandatory('Remote Tests', function () {
     this.retries(1);
     let editor: NotebookEditor;
     suiteSetup(async function () {
-        if (!IS_REMOTE_NATIVE_TEST() && !isWeb()) {
+        if (!isWeb()) {
             return this.skip();
         }
         traceInfo('Suite Setup Remote Tests');
