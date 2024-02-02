197e4de88b1f68ee9c68537b9841be5c63e91eb4
Don Jayamanne <don.jayamanne@outlook.com>
2024-02-02 13:39:22 +1100

    More fixes

--- src/notebooks/debugger/debugLocationTrackerFactory.ts
+++ src/notebooks/debugger/debugLocationTrackerFactory.ts
@@ -15,6 +15,7 @@ export class DebugLocationTrackerFactory
     implements IDebugLocationTracker, IDebugLocationTrackerFactory, DebugAdapterTrackerFactory
 {
     private activeTrackers = new WeakMap<DebugSession, DebugLocationTracker>();
+    private activeTrackersById = new Map<string, DebugLocationTracker>();
     private updatedEmitter: EventEmitter<void> = new EventEmitter<void>();
 
     constructor(
@@ -27,7 +28,15 @@ export class DebugLocationTrackerFactory
     public createDebugAdapterTracker(session: DebugSession): DebugAdapterTracker {
         const result = new DebugLocationTracker(session.id);
         this.activeTrackers.set(session, result);
-        result.sessionEnded(() => this.activeTrackers.delete(session), this, this.disposableRegistry);
+        this.activeTrackersById.set(session.id, result);
+        result.sessionEnded(
+            () => {
+                this.activeTrackers.delete(session);
+                this.activeTrackersById.delete(session.id);
+            },
+            this,
+            this.disposableRegistry
+        );
         result.debugLocationUpdated(this.onLocationUpdated, this, this.disposableRegistry);
         this.onLocationUpdated();
         return result;
@@ -38,7 +47,7 @@ export class DebugLocationTrackerFactory
     }
 
     public getLocation(session: DebugSession) {
-        const tracker = this.activeTrackers.get(session);
+        const tracker = this.activeTrackers.get(session) || this.activeTrackersById.get(session.id);
         if (tracker) {
             return tracker.debugLocation;
         }
