0687274020b2282668e49eeae82e125c02966fbb
Don Jayamanne <don.jayamanne@outlook.com>
2024-02-12 13:04:35 +1100

    more changes

--- src/test/datascience/setupTestEnvs.sh
+++ src/test/datascience/setupTestEnvs.sh
@@ -5,7 +5,7 @@ python -m venv .venvkernel
 source .venvkernel/bin/activate
 python --version
 python -c "import sys;print(sys.executable)"
-python -m pip install ipykernel
+python -m pip install 'ipykernel==6.29.0'
 python -m ipykernel install --user --name .venvkernel --display-name .venvkernel
 python -m pip uninstall jedi --yes
 python -m pip install jedi==0.17.2
@@ -14,7 +14,7 @@ python -m pip install ipywidgets==7.7.2
 source .venvnokernel/bin/activate
 python --version
 python -c "import sys;print(sys.executable)"
-python -m pip install ipykernel
+python -m pip install 'ipykernel==6.29.0'
 python -m ipykernel install --user --name .venvnokernel --display-name .venvnokernel
 python -m pip uninstall jedi --yes
 python -m pip install jedi==0.17.2
