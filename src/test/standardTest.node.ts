43812a062805fd494a88ff54c5ddd340fac1a90e
Don Jayamanne <don.jayamanne@outlook.com>
2024-02-12 13:51:36 +1100

    Stable vscode

--- src/test/standardTest.node.ts
+++ src/test/standardTest.node.ts
@@ -34,10 +34,9 @@ function requiresPythonExtensionToBeInstalled() {
     return isRunningVSCodeTests || IS_SMOKE_TEST() || IS_PERF_TEST();
 }
 
-// const channel = (process.env.VSC_JUPYTER_CI_TEST_VSC_CHANNEL || '').toLowerCase().includes('insiders')
-//     ? 'insiders'
-//     : 'stable';
-const channel = '1.85.0';
+const channel = (process.env.VSC_JUPYTER_CI_TEST_VSC_CHANNEL || '').toLowerCase().includes('insiders')
+    ? 'insiders'
+    : 'stable';
 
 function computePlatform() {
     switch (process.platform) {
