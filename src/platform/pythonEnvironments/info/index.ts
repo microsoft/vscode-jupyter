237c153829239dc8b31a2d7f13d9a7dcaf8b41ea
Don Jayamanne <don.jayamanne@outlook.com>
2024-02-27 21:46:31 +1100

    Oops

--- src/platform/pythonEnvironments/info/index.ts
+++ src/platform/pythonEnvironments/info/index.ts
@@ -19,22 +19,13 @@ export enum EnvironmentType {
 }
 
 export type InterpreterId = string;
-/**
- * Details about a Python runtime.
- *
- * @prop path - the location of the executable file
- * @prop version - the runtime version
- * @prop sysPrefix - the environment's install root (`sys.prefix`)
- */
-export type InterpreterInformation = {
-    id: InterpreterId;
-    uri: Uri;
-};
 
 /**
  * Details about a Python environment.
  */
-export interface PythonEnvironment extends InterpreterInformation  {
+export interface PythonEnvironment {
+    id: InterpreterId;
+    uri: Uri;
     displayName?: string;
     /**
      * This contains the path to the environment.
