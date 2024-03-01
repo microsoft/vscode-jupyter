214b69e33cfd78e1ab4c96dd531dc806a529b8c3
Aaron Munger <aamunger@microsoft.com>
2024-03-01 15:09:50 -0800

    remove fluentUI filter box

--- src/webviews/webview-side/data-explorer/reactSlickGridFilterBox.tsx
+++ src/webviews/webview-side/data-explorer/reactSlickGridFilterBox.tsx
@@ -2,35 +2,10 @@
 // Licensed under the MIT License.
 
 import * as React from 'react';
-import { IIconProps, SearchBox } from '@fluentui/react';
 
 import './reactSlickGridFilterBox.css';
 
-const filterIcon: IIconProps = {
-    iconName: 'Filter',
-    styles: {
-        root: {
-            fontSize: 'var(--vscode-font-size)',
-            width: 'var(--vscode-font-size)',
-            color: 'var(--vscode-settings-textInputForeground)'
-        }
-    }
-};
 
-const styles = {
-    iconContainer: {
-        opacity: 0.4
-    },
-    root: {
-        '::after': {
-            borderRadius: '0px',
-            border: '1px solid var(--vscode-focusBorder)'
-        }
-    },
-    field: {
-        color: 'var(--vscode-settings-textInputForeground)'
-    }
-};
 
 interface IFilterProps {
     column: Slick.Column<Slick.SlickData>;
@@ -42,27 +17,16 @@ interface IFilterProps {
 export class ReactSlickGridFilterBox extends React.Component<IFilterProps> {
     public override render() {
         return (
-            <SearchBox
-                iconProps={filterIcon}
+            <input
                 onChange={this.updateInputValue}
-                onClear={this.clearInputValue}
                 tabIndex={0}
-                ariaLabel={this.props.column.name}
+                aria-label={this.props.column.name}
                 className="filter-box"
-                styles={styles}
-                value={this.props.filter}
             />
         );
     }
 
-    private clearInputValue = () => {
-        this.props.onChange('', this.props.column);
-    };
-
-    private updateInputValue = (
-        _event?: React.ChangeEvent<HTMLInputElement> | undefined,
-        newValue?: string | undefined
-    ) => {
-        this.props.onChange(newValue ?? '', this.props.column);
+    private updateInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.props.onChange(event.target.value ?? '', this.props.column);
     };
 }
