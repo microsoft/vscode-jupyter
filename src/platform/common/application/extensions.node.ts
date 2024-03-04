39d92f7859143330b0a542030986a214b0e1f30b
Don Jayamanne <don.jayamanne@outlook.com>
2024-03-04 17:13:23 +1100

    More logging

--- src/platform/common/application/extensions.node.ts
+++ src/platform/common/application/extensions.node.ts
@@ -7,7 +7,7 @@ import { IExtensions } from '../types';
 import { DataScience } from '../utils/localize';
 import { parseStack } from '../../errors';
 import { JVSC_EXTENSION_ID, unknownExtensionId } from '../constants';
-import { traceError } from '../../logging';
+import { traceError, traceWarning } from '../../logging';
 
 /**
  * Provides functions for tracking the list of extensions that VS code has installed (besides our own)
@@ -17,17 +17,17 @@ export class Extensions implements IExtensions {
     public determineExtensionFromCallStack(stack?: string): { extensionId: string; displayName: string } {
         stack = stack || new Error().stack;
         try {
+            let frames: string[] = [];
+            const jupyterExtRoot = extensions.getExtension(JVSC_EXTENSION_ID)!.extensionUri.toString().toLowerCase();
             if (stack) {
-                const jupyterExtRoot = extensions
-                    .getExtension(JVSC_EXTENSION_ID)!
-                    .extensionUri.toString()
-                    .toLowerCase();
-                const frames = stack
+                frames = stack
                     .split('\n')
                     .map((f) => {
                         const result = /\((.*)\)/.exec(f);
                         if (result) {
                             return result[1];
+                        } else {
+                            traceWarning(`Regex failed to parse stack frame ${f}`);
                         }
                     })
                     .filter((item) => item && !item.toLowerCase().startsWith(jupyterExtRoot)) as string[];
@@ -35,6 +35,9 @@ export class Extensions implements IExtensions {
                     const fileName = item.getFileName();
                     if (fileName && !fileName.toLowerCase().startsWith(jupyterExtRoot)) {
                         frames.push(fileName);
+                        traceWarning(`Parsed file name in stack ${fileName}`);
+                    } else {
+                        traceWarning(`Unable to parse file name in stack`);
                     }
                 });
                 for (const frame of frames) {
@@ -50,6 +53,8 @@ export class Extensions implements IExtensions {
                 }
             }
             traceError(`Unable to determine the caller of the extension API for trace stack.`, stack);
+            traceError(`Frames.`, frames.join(', '));
+            traceError(`Jupyter Extension Root.`, jupyterExtRoot);
             return { extensionId: unknownExtensionId, displayName: DataScience.unknownPackage };
         } catch (ex) {
             traceError(`Unable to determine the caller of the extension API for trace stack.`, stack);
