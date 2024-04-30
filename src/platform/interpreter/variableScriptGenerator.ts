b2dff64fb28ad8a701dbeee9ae606c4b948deba1
Aaron Munger <aamunger@microsoft.com>
2024-04-30 13:33:45 -0700

    cache in correct variable (#15636)

--- src/platform/interpreter/variableScriptGenerator.ts
+++ src/platform/interpreter/variableScriptGenerator.ts
@@ -140,7 +140,7 @@ export class VariableScriptGenerator implements IVariableScriptGenerator {
             'vscodeGetVariablesForProvider.py'
         );
         const contents = await this.fs.readFile(scriptPath);
-        VariableScriptGenerator.contentsOfScript = contents;
+        VariableScriptGenerator.contentsOfVariablesScript = contents;
         return contents;
     }
 }
