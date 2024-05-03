1ab6bfe7465cc29e915b5088d997c469448f26e4
Don Jayamanne <don.jayamanne@outlook.com>
2024-05-04 01:32:40 +1000

    OOps

--- src/standalone/api/kernels/api.vscode.common.test.ts
+++ src/standalone/api/kernels/api.vscode.common.test.ts
@@ -219,7 +219,7 @@ suiteMandatory('Kernel API Tests @typescript', function () {
 });
 
 // eslint-disable-next-line no-only-tests/no-only-tests
-suite.only('Kernel API Tests @typescript/@python', function () {
+suite('Kernel API Tests @typescript/@python', function () {
     const disposables: IDisposable[] = [];
     this.timeout(120_000);
     let notebook: NotebookDocument;
