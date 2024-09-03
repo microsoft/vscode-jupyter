c0e19a92d8ad52fb5969f7b27fda041b1f73e5dd
Don Jayamanne <don.jayamanne@outlook.com>
2024-09-04 06:26:12 +1000

    Re-enable intellisense tests

--- src/test/datascience/notebook/intellisense/completionProvider.vscode.common.test.ts
+++ src/test/datascience/notebook/intellisense/completionProvider.vscode.common.test.ts
@@ -183,7 +183,7 @@ import { IKernelProvider } from '../../../../kernels/types';
                     )
                 );
             }
-            test.skip('Dataframe completions', async () => {
+            test('Dataframe completions', async () => {
                 const fileName = path.basename(window.activeNotebookEditor!.notebook.uri.fsPath);
                 await testCompletions('df.', '.', fileName, 'Age', 'S', 'Sex');
             });
@@ -192,7 +192,7 @@ import { IKernelProvider } from '../../../../kernels/types';
                 const fileName = path.basename(window.activeNotebookEditor!.notebook.uri.fsPath);
                 await testCompletions('df.Name.', '.', fileName, 'add_prefix', 'add_s', 'add_suffix');
             });
-            test.skip('Dataframe assignment completions', async () => {
+            test('Dataframe assignment completions', async () => {
                 const fileName = path.basename(window.activeNotebookEditor!.notebook.uri.fsPath);
                 await testCompletions('var_name = df.', '.', fileName, 'Age', 'S', 'Sex');
             });
