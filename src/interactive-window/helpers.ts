79a3b7cfeafea97008d663814584bc965f5af85e
amunger <aamunger@microsoft.com>
2024-10-28 11:06:48 -0700

    recover notebook backed IWs to be re-used

--- src/interactive-window/helpers.ts
+++ src/interactive-window/helpers.ts
@@ -5,11 +5,9 @@ import { NotebookCell } from 'vscode';
 import { dedentCode, splitLines } from '../platform/common/helpers';
 import { IJupyterSettings } from '../platform/common/types';
 import { appendLineFeed, removeLinesFromFrontAndBackNoConcat } from '../platform/common/utils';
-import { isUri } from '../platform/common/utils/misc';
 import { uncommentMagicCommands } from './editor-integration/cellFactory';
 import { CellMatcher } from './editor-integration/cellMatcher';
 import { InteractiveCellMetadata } from './editor-integration/types';
-import { InteractiveTab } from './types';
 
 export function getInteractiveCellMetadata(cell: NotebookCell): InteractiveCellMetadata | undefined {
     if (cell.metadata.interactive !== undefined) {
@@ -37,14 +35,4 @@ export function generateInteractiveCode(code: string, settings: IJupyterSettings
     );
 
     return dedentCode(withMagicsAndLinefeeds.join(''));
-}
-
-export function isInteractiveInputTab(tab: unknown): tab is InteractiveTab {
-    let interactiveTab = tab as InteractiveTab;
-    return (
-        interactiveTab &&
-        interactiveTab.input &&
-        isUri(interactiveTab.input.uri) &&
-        isUri(interactiveTab.input.inputBoxUri)
-    );
 }
--- src/interactive-window/interactiveWindow.ts
+++ src/interactive-window/interactiveWindow.ts
@@ -42,10 +42,9 @@ import { CellMatcher } from './editor-integration/cellMatcher';
 import {
     IInteractiveWindow,
     IInteractiveWindowDebugger,
-    IInteractiveWindowDebuggingManager,
-    InteractiveTab
+    IInteractiveWindowDebuggingManager
 } from './types';
-import { generateInteractiveCode, isInteractiveInputTab } from './helpers';
+import { generateInteractiveCode } from './helpers';
 import { getInteractiveCellMetadata } from './helpers';
 import { getFilePath } from '../platform/common/platform/fs-paths';
 import {
@@ -120,7 +119,7 @@ export class InteractiveWindow implements IInteractiveWindow {
         private readonly serviceContainer: IServiceContainer,
         private _owner: Resource,
         private readonly controllerFactory: InteractiveControllerFactory,
-        notebookEditorOrTab: NotebookEditor | InteractiveTab,
+        notebookEditorOrTabInput: NotebookEditor | TabInputNotebook | TabInputInteractiveWindow,
         public readonly inputUri: Uri
     ) {
         this.fs = this.serviceContainer.get<IFileSystem>(IFileSystem);
@@ -135,9 +134,9 @@ export class InteractiveWindow implements IInteractiveWindow {
         this.debuggingManager = this.serviceContainer.get<IInteractiveWindowDebuggingManager>(
             IInteractiveWindowDebuggingManager
         );
-        this.notebookUri = isInteractiveInputTab(notebookEditorOrTab)
-            ? notebookEditorOrTab.input.uri
-            : notebookEditorOrTab.notebook.uri;
+        this.notebookUri = notebookEditorOrTabInput instanceof TabInputInteractiveWindow || notebookEditorOrTabInput instanceof TabInputNotebook
+            ? notebookEditorOrTabInput.uri
+            : notebookEditorOrTabInput.notebook.uri;
 
         // Set our owner and first submitter
         if (this._owner) {
