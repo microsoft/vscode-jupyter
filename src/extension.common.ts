91d3f58014b70b968d091ab9b0653aabf27aebf6
Don Jayamanne <don.jayamanne@outlook.com>
2024-10-28 12:10:41 +1100

    Check global storage dir perm & fallback to ext dir

--- src/extension.common.ts
+++ src/extension.common.ts
@@ -49,7 +49,7 @@ import { IExtensionActivationManager } from './platform/activation/types';
 import { getVSCodeChannel } from './platform/common/application/applicationEnvironment';
 import { getExtensionTempDir } from './platform/common/temp';
 
-export function initializeLoggers(
+export async function initializeLoggers(
     context: IExtensionContext,
     options: {
         addConsoleLogger: boolean;
@@ -83,7 +83,7 @@ export function initializeLoggers(
     if (options?.platform) {
         standardOutputChannel.appendLine(`Platform: ${options.platform} (${options.arch}).`);
     }
-    standardOutputChannel.appendLine(`Temp Storage folder ${getDisplayPath(getExtensionTempDir(context))}`);
+    standardOutputChannel.appendLine(`Temp Storage folder ${getDisplayPath(await getExtensionTempDir(context))}`);
     if (!workspace.workspaceFolders || workspace.workspaceFolders.length === 0) {
         standardOutputChannel.appendLine(`No workspace folder opened.`);
     } else if (workspace.workspaceFolders.length === 1) {
