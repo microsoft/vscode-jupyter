175f1792626c53e360bbc7d2932565d1bea017c6
Paul Wang <paul_wang347@hotmail.com>
2025-01-14 16:29:58 -0800

    Fix tests

--- src/kernels/kernelProvider.node.ts
+++ src/kernels/kernelProvider.node.ts
@@ -98,8 +98,8 @@ export class KernelProvider extends BaseCoreKernelProvider {
             this.disposables
         );
         kernel.onPostInitialized(
-            ({ token, waitUntil }) => {
-                this._onDidPostInitializeKernel.fire({ kernel, token, waitUntil });
+            (e) => {
+                this._onDidPostInitializeKernel.fire({ kernel, ...e });
             },
             this,
             this.disposables
@@ -160,8 +160,8 @@ export class ThirdPartyKernelProvider extends BaseThirdPartyKernelProvider {
             this.disposables
         );
         kernel.onPostInitialized(
-            ({ token, waitUntil }) => {
-                this._onDidPostInitializeKernel.fire({ kernel, token, waitUntil });
+            (e) => {
+                this._onDidPostInitializeKernel.fire({ kernel, ...e });
             },
             this,
             this.disposables
--- src/kernels/kernelProvider.web.ts
+++ src/kernels/kernelProvider.web.ts
@@ -80,11 +80,7 @@ export class KernelProvider extends BaseCoreKernelProvider {
             this.workspaceStorage
         ) as IKernel;
         kernel.onRestarted(() => this._onDidRestartKernel.fire(kernel), this, this.disposables);
-        kernel.onPostInitialized(
-            ({ token, waitUntil }) => this._onDidPostInitializeKernel.fire({ kernel, token, waitUntil }),
-            this,
-            this.disposables
-        );
+        kernel.onPostInitialized((e) => this._onDidPostInitializeKernel.fire({ kernel, ...e }), this, this.disposables);
         kernel.onDisposed(() => this._onDidDisposeKernel.fire(kernel), this, this.disposables);
         kernel.onStarted(() => this._onDidStartKernel.fire(kernel), this, this.disposables);
         kernel.onStatusChanged(
@@ -137,11 +133,7 @@ export class ThirdPartyKernelProvider extends BaseThirdPartyKernelProvider {
             this.workspaceStorage
         );
         kernel.onRestarted(() => this._onDidRestartKernel.fire(kernel), this, this.disposables);
-        kernel.onPostInitialized(
-            ({ token, waitUntil }) => this._onDidPostInitializeKernel.fire({ kernel, token, waitUntil }),
-            this,
-            this.disposables
-        );
+        kernel.onPostInitialized((e) => this._onDidPostInitializeKernel.fire({ kernel, ...e }), this, this.disposables);
         kernel.onDisposed(() => this._onDidDisposeKernel.fire(kernel), this, this.disposables);
         kernel.onStarted(() => this._onDidStartKernel.fire(kernel), this, this.disposables);
         kernel.onStatusChanged(
