d162ae083c3f97a04d40d8dfc334a2ee857b38ea
Don Jayamanne <don.jayamanne@outlook.com>
2025-02-03 14:06:12 +1100

    Fix failing, re-enable skipped and move tests

--- src/kernels/raw/finder/contributedKerneFinder.node.unit.test.ts
+++ src/kernels/raw/finder/contributedKerneFinder.node.unit.test.ts
@@ -834,10 +834,10 @@ import { setPythonApi } from '../../../platform/interpreter/helpers';
             condaEnv1
         ].forEach((activePythonEnv) => {
             suite(activePythonEnv ? `With active Python (${activePythonEnv.id})` : 'without active Python', () => {
-                setup(function () {
-                    // Flaky windows unit tests. https://github.com/microsoft/vscode-jupyter/issues/13462
-                    return this.skip();
-                });
+                // setup(function () {
+                //     // Flaky windows unit tests. https://github.com/microsoft/vscode-jupyter/issues/13462
+                //     return this.skip();
+                // });
                 /**
                  * As we're using a push model, we need to wait for the events to get triggered.
                  * How many events do we need to wait for is not deterministic (well for tests it is, but its too complex).
@@ -915,9 +915,9 @@ import { setPythonApi } from '../../../platform/interpreter/helpers';
                 });
                 test('If two kernelspecs share the same interpreter, but have different env variables, then both should be listed', async function () {
                     // https://github.com/microsoft/vscode-jupyter/issues/13236
-                    if (os.platform() === 'win32') {
-                        return this.skip();
-                    }
+                    // if (os.platform() === 'win32') {
+                    //     return this.skip();
+                    // }
                     const testData: TestData = {
                         interpreters: [
                             {
