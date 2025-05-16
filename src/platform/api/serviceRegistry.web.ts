f2f4cbca22d9ad347a3d210f25aa03ff4f513a4c
Don Jayamanne <don.jayamanne@outlook.com>
2025-05-16 16:28:57 +1000

    Prefer kernel last used by Python Tools

--- src/platform/api/serviceRegistry.web.ts
+++ src/platform/api/serviceRegistry.web.ts
@@ -2,10 +2,11 @@
 // Licensed under the MIT License.
 
 import { IServiceManager } from '../ioc/types';
-import { OldPythonApiProvider, PythonExtensionChecker } from './pythonApi';
+import { IPythonChatTools, OldPythonApiProvider, PythonChatTools, PythonExtensionChecker } from './pythonApi';
 import { IPythonApiProvider, IPythonExtensionChecker } from './types';
 
 export function registerTypes(serviceManager: IServiceManager): void {
     serviceManager.addSingleton<IPythonApiProvider>(IPythonApiProvider, OldPythonApiProvider);
+    serviceManager.addSingleton<IPythonChatTools>(IPythonChatTools, PythonChatTools);
     serviceManager.addSingleton<IPythonExtensionChecker>(IPythonExtensionChecker, PythonExtensionChecker);
 }
