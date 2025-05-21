0f311bc6fee6c05ca05d643760d5a9c215387f32
Don Jayamanne <don.jayamanne@outlook.com>
2025-05-21 12:42:03 +1000

    Further changes to pre-selecting kernels in dev containers (#16663)
    * Update comments and additional logging
    
    * updates
    
    * Updates
    
    * Updates
    
    * Upates

--- src/notebooks/controllers/preferredKernelConnectionService.node.ts
+++ src/notebooks/controllers/preferredKernelConnectionService.node.ts
@@ -6,10 +6,12 @@ import * as path from '../../platform/vscode-path/resources';
 import { isParentPath } from '../../platform/common/platform/fileUtils';
 import { EnvironmentType } from '../../platform/pythonEnvironments/info';
 import { getEnvironmentType } from '../../platform/interpreter/helpers';
-import { Environment, PythonExtension, ResolvedEnvironment } from '@vscode/python-extension';
 import type { PythonEnvironmentFilter } from '../../platform/interpreter/filter/filterService';
 import type { INotebookPythonEnvironmentService } from '../types';
 import { raceTimeout } from '../../platform/common/utils/async';
+import { logger } from '../../platform/logging';
+import { getDisplayPath } from '../../platform/common/platform/fs-paths';
+import { Environment, PythonExtension, ResolvedEnvironment } from '@vscode/python-extension';
 
 export async function findPreferredPythonEnvironment(
     notebook: NotebookDocument,
@@ -23,6 +25,9 @@ export async function findPreferredPythonEnvironment(
         pythonApi.environments.known.filter((e) => !filter.isPythonEnvironmentExcluded(e))
     );
     if (localEnv) {
+        logger.trace(
+            `For ${getDisplayPath(notebook.uri)} found local Python environment: ${getDisplayPath(localEnv.path)}`
+        );
         return localEnv;
     }
 
@@ -30,11 +35,16 @@ export async function findPreferredPythonEnvironment(
     // Its possible the active interpreter is global and could cause other issues.
     const pythonEnv = notebookEnvironment.getPythonEnvironment(notebook.uri);
     if (pythonEnv) {
+        logger.trace(`For ${getDisplayPath(notebook.uri)} found Python environment: ${getDisplayPath(pythonEnv.path)}`);
         return pythonApi.environments.resolveEnvironment(pythonEnv.id);
     }
 
     // 2. Fall back to the workspace env selected by the user.
-    return getRecommendedPythonEnvironment(notebook.uri);
+    const recommeded = await getRecommendedPythonEnvironment(notebook.uri);
+    logger.trace(
+        `For ${getDisplayPath(notebook.uri)} found recommeded Python environment: ${getDisplayPath(recommeded?.path)}`
+    );
+    return recommeded;
 }
 
 function findPythonEnvironmentClosestToNotebook(notebook: NotebookDocument, envs: readonly Environment[]) {
@@ -90,8 +100,14 @@ export async function getRecommendedPythonEnvironment(uri: Uri): Promise<Resolve
             | RecommededEnvironment
             | undefined;
         if (!result) {
+            logger.trace(`No recommended Python environment found for ${getDisplayPath(uri)}`);
             return;
         }
+        logger.trace(
+            `Got a recommended Python environment for ${getDisplayPath(uri)}, ${result.reason} and ${getDisplayPath(
+                result.environment?.path
+            )}`
+        );
         if (result.reason === 'workspaceUserSelected') {
             return result.environment;
         }
@@ -99,11 +115,36 @@ export async function getRecommendedPythonEnvironment(uri: Uri): Promise<Resolve
         // In dev containers give preference to the global user selected environment.
         // This is because the global user selected environment is the one that is most likely pre-configured in the dev container.
         // Generally the images have the settings already pre-configured with this global env pre-selected.
+        // However if there are no workspace folders, then we should use the global user selected environment.
         if (
             result.reason === 'globalUserSelected' &&
             (!workspace.workspaceFolders?.length || env.remoteName === 'dev-container')
         ) {
             return result.environment;
+        }
+
+        // Its possible we're in a dev container and the workspace & global env is not selected.
+        // However the python.defaultInterprerterPath is set to a path in the dev container.
+        // If thats the same as the one we get, then we should use that.
+        const defaultInterpreterPath = workspace
+            .getConfiguration('python')
+            .get<string | undefined>('defaultInterpreterPath', undefined);
+        if (env.remoteName === 'dev-container' && defaultInterpreterPath) {
+            try {
+                let env: ResolvedEnvironment | undefined = result.environment;
+                if (env.path !== defaultInterpreterPath) {
+                    const api = await PythonExtension.api();
+                    env = await api.environments.resolveEnvironment(defaultInterpreterPath);
+                }
+                if (env && env.path === defaultInterpreterPath) {
+                    logger.trace(
+                        `Using the default interpreter path ${defaultInterpreterPath} as the recommended Python environment`
+                    );
+                    return env;
+                }
+            } catch {
+                //
+            }
         }
     } catch (ex) {
         return;
