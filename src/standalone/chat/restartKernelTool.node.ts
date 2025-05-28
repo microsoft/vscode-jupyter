2dad8f2edd4b2e49967547ecc362cfbdaa3c428a
Aaron Munger <2019016+amunger@users.noreply.github.com>
2025-05-27 20:50:44 -0700

    chat tool for restarting kernel (#16678)
    * chat tool for restarting kernel
    
    * lint

---
+++ src/standalone/chat/restartKernelTool.node.ts
@@ -0,0 +1,55 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as vscode from 'vscode';
+import { IKernelProvider } from '../../kernels/types';
+import { IBaseToolParams, resolveNotebookFromFilePath } from './helper';
+import { IControllerRegistration } from '../../notebooks/controllers/types';
+
+export class RestartKernelTool implements vscode.LanguageModelTool<IBaseToolParams> {
+    public static toolName = 'restart_notebook_kernel';
+
+    public get name() {
+        return RestartKernelTool.toolName;
+    }
+    public get description() {
+        return 'Restarts the active kernel of a notebook.';
+    }
+
+    constructor(
+        private readonly kernelProvider: IKernelProvider,
+        private readonly controllerRegistration: IControllerRegistration
+    ) {}
+
+    async invoke(
+        options: vscode.LanguageModelToolInvocationOptions<IBaseToolParams>,
+        _token: vscode.CancellationToken
+    ) {
+        await vscode.commands.executeCommand('jupyter.restartkernel', vscode.Uri.file(options.input.filePath));
+        const finalMessageString = `The kernel for the notebook at ${options.input.filePath} has been restarted.`;
+        return new vscode.LanguageModelToolResult([new vscode.LanguageModelTextPart(finalMessageString)]);
+    }
+
+    async prepareInvocation(
+        options: vscode.LanguageModelToolInvocationPrepareOptions<IBaseToolParams>,
+        _token: vscode.CancellationToken
+    ): Promise<vscode.PreparedToolInvocation> {
+        const notebook = await resolveNotebookFromFilePath(options.input.filePath);
+
+        const controller = this.controllerRegistration.getSelected(notebook);
+        const kernel = this.kernelProvider.get(notebook);
+        if (!controller || !kernel || !kernel.startedAtLeastOnce) {
+            throw new Error(
+                `No active kernel for notebook ${options.input.filePath}, the configure_notebook tool can be used to help the user select a kernel.`
+            );
+        }
+
+        return {
+            confirmationMessages: {
+                title: vscode.l10n.t(`Restart Kernel`),
+                message: vscode.l10n.t('Restart the notebook kernel?')
+            },
+            invocationMessage: vscode.l10n.t('Restarting kernel')
+        };
+    }
+}
