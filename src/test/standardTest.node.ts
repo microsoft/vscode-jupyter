b4c32f1ae7cfa2dc69261039d52499e46c3cc988
Don Jayamanne <don.jayamanne@outlook.com>
2025-08-06 15:03:44 +1000

    Remove unwanted logs

--- src/test/standardTest.node.ts
+++ src/test/standardTest.node.ts
@@ -93,7 +93,6 @@ async function installPythonExtension(vscodeExecutablePath: string, extensionsDi
         console.info('Python Extension not required');
         return;
     }
-    console.info(`Installing Python Extension ${PythonExtension} to ${extensionsDir}`);
     const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath, platform);
     await installExtension(PythonExtension, cliPath, extensionsDir, ['--pre-release']);
 
@@ -106,7 +105,6 @@ async function installPythonExtension(vscodeExecutablePath: string, extensionsDi
 
 // Make sure renderers is there too as we'll use it for widget tests
 async function installExtension(extension: string, cliPath: string, extensionsDir: string, args: string[] = []) {
-    console.info(`Installing ${extension} Extension to ${extensionsDir}`);
     args = ['--install-extension', extension, ...args, '--extensions-dir', extensionsDir, '--disable-telemetry'];
     const output =
         process.platform === 'win32'
