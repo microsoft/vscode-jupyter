507a29930abd26ad916420f0e192741c0edf4dda
Don Jayamanne <don.jayamanne@outlook.com>
2026-06-01 16:37:00 +1000

    Bump version to 2026.06.0 and add Synapse extension IDs

--- src/platform/common/constants.ts
+++ src/platform/common/constants.ts
@@ -26,6 +26,8 @@ export const DATA_WRANGLER_EXTENSION_ID = 'ms-toolsai.datawrangler';
 export const PROPOSED_API_ALLOWED_PUBLISHERS = ['donjayamanne'];
 export const POWER_TOYS_EXTENSION_ID = 'ms-toolsai.vscode-jupyter-powertoys';
 export const SENTINEL_EXTENSION_ID = 'ms-security.ms-sentinel';
+export const SYNAPSE_EXTENSION_ID = 'synapsevscode.synapse';
+export const SYNAPSE_REMOTE_EXTENSION_ID = 'synapsevscode.vscode-synapse-remote';
 export const JUPYTER_HUB_EXTENSION_ID = 'ms-toolsai.jupyter-hub';
 export const AppinsightsKey = '0c6ae279ed8443289764825290e4f9e2-1a736e7c-1324-4338-be46-fc2a58ae4d14-7255';
 
